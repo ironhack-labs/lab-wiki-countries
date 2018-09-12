260b7ab2d7e24b6f28fa8c775e60ecd7eba9336d
Andrei Fitai <fitai.andrei@gmail.com>
2018-09-12 16:54:11 +0200

    initial setup

---
+++ src/CountryDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountryDetail = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default CountryDetail;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+class App extends React.Component {
+  render() {
+    return <div>{/* Your application code */}</div>;
+  }
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
