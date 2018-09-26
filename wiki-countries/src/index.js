2c833283768a1d7393b55b422076e469c5f64dbc
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-26 16:08:56 -0400

    Created basic structure

---
+++ wiki-countries/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './style.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+            {/* Your application code */}
+            </div>
+    );
+    }
+}
+
+ReactDOM.render((
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+),
+    document.getElementById('root')
+);
