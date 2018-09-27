06482557afb2531dd9350a358a69b83d7fd9ccfc
Doug DeJong <dougdejong@gmail.com>
2018-09-27 16:35:52 -0400

    fully functional

--- src/components/CountryDetail/CountryDetail.js
+++ src/components/CountryDetail/CountryDetail.js
@@ -1,10 +1,58 @@
 import React from 'react';
-import './style.css';
+import './CountryDetail.css'
+
+import { withRouter } from 'react-router'
+import { Link, Route } from "react-router-dom";
 
 class CountryDetail extends React.Component{
-  render(){
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+
+  borderingCountries(){
+    const theCountry = this.props.countries.find((propCountry)=>
+    propCountry.cca3 === this.props.match.params.id
+    )
+    let bordering = this.props.countries.filter(nation => nation.borders.includes(theCountry.cca3) )
+    console.log(bordering)
     return(
-      <h1>Country Detail Working!</h1>
+      bordering.map((nation, index) => (
+        <li key={index}>
+          <Link to={`/${nation.cca3}`}>{nation.name.common}</Link>
+        </li>
+      ))
     )
   }
-}
+  
+  showCountry(){
+
+    const theCountry = this.props.countries.find((propCountry)=>
+    propCountry.cca3 === this.props.match.params.id
+    )
+    console.log(this.props.match.params.id)
+
+    return(
+      <div className="col-6 fixeddiv">
+      <h1>{theCountry.flag}{theCountry.name.common}</h1>
+      <h2>Capital: {theCountry.capital}</h2>
+      <h3>Area: {theCountry.area} km</h3>
+      <h4>Bordering Countries:</h4>
+      <ul>
+     {this.borderingCountries()}
+
+      </ul>
+      </div>
+    )
+  }
+  
+  
+  render() {
+    return(
+      <div>{this.showCountry()}</div>
+      )
+    }
+  }
+    
+  export default withRouter(CountryDetail);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { Link, Route } from "react-router-dom";
 import countries from './countries.json'
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import CountryDetail from './components/CountryDetail/CountryDetail'
 
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      countries: countries
+      countryList: countries
     }
+  }
+
+
+  showCountries() {
+    return  this.state.countryList.map((country, index) => {
+      return(
+        <Link to={`/${country.cca3}`} key={index} className="list-group-item list-group-item-action">{country.flag} {country.name.common}</Link>
+      )
+    })
+    
   }
   render() {
     return (
-      <div>
-        <h1>Working!</h1>
+      <div className="container border">
+        <h1>WikiCountries</h1>
+        <div className="row">
+        <div className="col-3">
+        <div className="list-group">
+       {this.showCountries()}
+        </div>
+        </div>
+        <Route
+            path="/:id"
+            render={() => {
+              return  (
+                
+                <CountryDetail countries={this.state.countryList}/>
+                )
+              }}
+              />
+              </div>
       </div>
     );
   }
