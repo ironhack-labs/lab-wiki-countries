9e95bffc17743d7e420cf78466c90f39c9599a44
Mapno <mariorvinas@gmail.com>
2018-10-03 21:04:46 +0200

    cool buttons

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,34 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, NavLink, Link, Route } from 'react-router-dom';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import countries from './countries.json';
 
 class Countrydetail extends React.Component {
+
+
     render() {
         const { cca3 } = this.props;
-        return(
+        return (
             <div>
-                
+                <h2>{{}}</h2>
+            </div>
+        )
+    }
+}
+
+class CountryCard extends React.Component {
+    render() {
+        let { cca3, flag, name: { common } } = this.props.country;
+        let route = `/${cca3}`
+        return (
+            <div>
+                <NavLink className='btn my-1 col-12 d-flex justify-content-start' to={route} activeClassName="btn-info" key={cca3}>{flag} {common}</NavLink>
             </div>
         )
     }
 }
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        {/* Your application code */}
-      </div>
-    );
-  }
+    render() {
+        const countriesList = countries;
+        return (
+            <div className='App container'>
+                <div className='row my-5'>
+                    {countriesList.map(e => <Route exact path='/:cca3' Component='' key={e.cca3}></Route>)}
+                    <div className="col-md-6 countries">
+                        {countriesList.map(e => <CountryCard country={e} key={e.cca3}></CountryCard>)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  ),
-  document.getElementById('root')
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+),
+    document.getElementById('root')
 );
