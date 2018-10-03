9ae1e3c64c5db75c2ece021a5466e956dd9e0476
Evenstarw1 <lauracorrallopez@gmail.com>
2018-10-04 01:32:37 +0200

    Intentando sacar detalles de los paises

--- my-app/src/components/CountryDetail.js
+++ my-app/src/components/CountryDetail.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import countries from './countries.json';
+import countriesList from './countries.json';
 import {Link} from 'react-router-dom';
 
 
-export default class CountryDetail extends Component {
-//   render() {
+class CountryDetail extends Component {
+  render() {
+    let cca3 = this.props.match.params.cca3
+
+    let country = countriesList.filter(e => e.cca3.includes(cca3));
+    
+    let name = country.name.official
+    let capital = country.capital
+    let area = country.area
+    let borders = country.borders
+
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>{capital}</p>
+            <p>{area}</p>
+            <p>{borders}<Link to={'/country/' + country.cca3}>{country.name.common}</Link></p>
+
+        </div>
+    )
       
 
-//   }
+  }
 }
+
+export default CountryDetail;
 
