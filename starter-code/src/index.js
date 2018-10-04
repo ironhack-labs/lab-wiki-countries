802b281efcb409d46b6dad30d6f970e6497cb789
omar-nav <omar.navarro@g.austincc.edu>
2018-10-03 19:08:21 -0500

    agregando los enlaces para cada pais

--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -4,15 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './style.css';
 import 'antd/dist/antd.css';
 import App from './App';
-// class App extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 {/* Your application code */}
-//             </div>
-//         );
-//     }
-// }
+
 
 ReactDOM.render((
     <BrowserRouter>
