8fc0a72202dd7718feec4b5242dcf7e16af48c8e
Mishel <mishel_gonzalez@hotmail.com>
2018-10-03 21:13:34 -0500

    yeah

---
+++ countries/src/Routes.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+
+import CountryDetail from './components/CountryDetail';
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route exact path="/detail/:id" component={CountryDetail}/>
+        </Switch>
+    )
+}
+
+export default Routes
