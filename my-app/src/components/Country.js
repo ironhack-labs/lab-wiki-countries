393c3d27d3f00ee403f82a7910fbe69f16803853
Evenstarw1 <lauracorrallopez@gmail.com>
2018-10-05 17:30:49 +0200

    Creo que ya esta todo

--- my-app/src/components/Country.js
+++ my-app/src/components/Country.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import countriesList from './countries.json';
+import countries from './countries.json';
 import CountryDetail from './CountryDetail.js'
 import { Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 export class Country extends Component {
 
  render() {
     return (
-        <div className="list-group col-5 pre-scrollable">
-            {countriesList.map((country, index) => {
+        <div>
+        <Router>
+          <div>
+            <div className="row">
+            <div className="list-group col-5 pre-scrollable">
+              {countries.map((country, index) => {
                 return (
-                    <div key={index} className="list-group-item list-group-item-action">
-                  <Link to={'/country/' + country.cca3}><div><spam>{country.flag}</spam>{country.name.common}</div></Link>
-                </div>
-                    )
-                 })}
-                 <div>
-              <Route path="/country/:cca3" component={CountryDetail} />
+                  <div key={index} className="list-group-item list-group-item-action">
+                    <Link to={"/country/" + country.cca3}>
+                      <div> 
+                      {country.flag} {country.name.common}
+                      </div>
+                    </Link>
+                  </div>
+                );
+              })}
             </div>
+            <div className="col-7">
+            <Route path="/country/:cca3" countries={countries} component={CountryDetail} />
+            </div>
+          </div>
         </div>
+        </Router>
+      </div>
         );
     }
 }
--- my-app/src/components/CountryDetail.js
+++ my-app/src/components/CountryDetail.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import countriesList from './countries.json';
+import React from 'react';
+import countries from './countries.json';
 import {Link} from 'react-router-dom';
 
+export default class CountryDetail extends React.Component {
+  
+    render() {
+    let country = countries.find(e => e.cca3 === this.props.match.params.cca3);
 
-class CountryDetail extends Component {
-    // constructor(props){
-    //     super(props)
-
-  render() {
-    let cca3 = this.params.cca3
-
-    let country = countriesList.filter(e => e.cca3.includes(cca3));
+    let bordersArray = country.borders.slice();
+    let borders = countries.filter(c => bordersArray.includes(c.cca3));
 
     return (
         <div>
-            <h2>{country.name.official}</h2>
-            <p>{country.capital}</p>
-            <p>{country.area}</p>
-            <p>{country.borders}<Link to={'/country/' + country.cca3}>{country.name.common}</Link></p>
+            <h2>{country.name.common}</h2>
+            <spam>{country.flag}</spam>
+            <hr/>
+            <h6>Capital: {country.capital}</h6>
+            <hr/>
+            <h6>Area: {country.area} m<sup>2</sup></h6>
+            <hr/>
+            <h6>
+            {''}
+                Borders: {''}
+                <span>
+                    <ul>
+                        {borders.map((country, index) => 
+                            <li key={index}>
+                            <Link to={'/country/' + country.cca3}>{country.name.common}</Link>
+                            </li>)}
+                    </ul>
+
+                </span>
+            </h6>
 
         </div>
     )  
   }
-}
-
-export default CountryDetail;
-
+    }
