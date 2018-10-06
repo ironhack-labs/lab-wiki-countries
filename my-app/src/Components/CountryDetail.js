e07cbc3f649748c8a8ea74ad9f06facbcf97c4c5
David Sirit <davidsirit@yahoo.com>
2018-10-06 17:05:22 +0200

    Lab Done

--- my-app/src/Components/CountryDetail.js
+++ my-app/src/Components/CountryDetail.js
@@ -1,40 +1,42 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-// import countries from "../countries.json";
+import { Link, NavLink } from "react-router-dom";
 
 export class CountryDetail extends Component {
   render() {
-    console.log('el this enviado',this.props.data);
-    console.log('las props',this.props.id );
+    const totalCountry = this.props.data;
 
-    const country = this.props.data.filter((c)=> c.cca3 === this.props.id);
-    console.log(country);
-    // let neighbors =this.props.data.filter((ele)=> country[0].countryBorders.includes(ele.cca3));
-    // console.log('arry vecinos', neighbors);
+    const country = totalCountry.filter(ele => ele.cca3 === this.props.id);
 
+    const neighborsList = country[0].borders;
 
+    let neighbors = [];
+    neighborsList.forEach(element => {
+      neighbors.push(
+        totalCountry.filter(px => {
+          if (px.cca3 === element) {
+            console.log("veamos esto ", px.name.official);
+            return px.name.official;
+          }
+        })
+      );
+    });
 
-
+    let nei2 = neighbors.map(ele => ele[0]);
 
     return (
       <div>
-        <h1>Venezuela mi tierra</h1>
         <h2>{country[0].name.official}</h2>
-        <span>Capital: {country[0].capital}</span>
-        {/* <span>Neighbors: {neighbors[0]}</span> */}
+        <h3>Capital: {country[0].capital}</h3>
+        <h4>Area: {country[0].area} km^2</h4>
+        <hr />
+        <ul className="col-8">
+          {nei2.map(e => (
+            <NavLink to={e.cca3}>
+              <li key={e.cca3}>{e.name.common}</li>
+            </NavLink>
+          ))}
+        </ul>
       </div>
-
     );
-
-
-
-
-
-
-
   }
-
-
-
-
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,11 +5,7 @@ import "./style.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { WikiCountry } from "./WikiCountry";
 
-class App extends React.Component {
-  render() {
-    return <div>{/* Your application code */}</div>;
-  }
-}
+
 
 ReactDOM.render(
   <div>
