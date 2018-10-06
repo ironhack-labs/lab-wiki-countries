4abc94ed3da5ea7792a0de769ada76b3e8a85738
agdwm <almudenaguerraswm@gmail.com>
2018-10-06 11:01:52 +0200

    Finished refactoring the function to render the country

--- wiki-countries/src/components/CountryDetail.js
+++ wiki-countries/src/components/CountryDetail.js
@@ -3,9 +3,16 @@ import './countryDetail.css';
 
 
 export class CountryDetail extends Component {
+	//{country.borders.map((border,i) => <li key={i}><a href={id}>{border}</a></li>)}: ''}
 	
-	renderCountry = (countryProp, country) => {
-		
+	renderCountryBorders = (country, id) => {
+		let bordersToHtml = country.borders.map((border,i) => {
+			return (<li key={i}><a href={id}>{border}</a></li>)
+		})
+		return (<div><strong>Borders:</strong><ul>{bordersToHtml}</ul></div>)
+	}
+
+	renderCountry = (countryProp, country, id) => {
 		switch (countryProp) {
 			case "name":
 				return (<div><h2>{country.name}</h2></div>)
@@ -13,10 +20,11 @@ export class CountryDetail extends Component {
 				return (<div><strong>Capital:</strong><span>{country.capital}</span></div>)
 			case "area":
 				return	(<div><strong>Area:</strong><span>{`${country.area} Km`}<sup>2</sup></span></div>)
+			case "borders":
+				return (this.renderCountryBorders(country, id));
 			default:
 				break;
 		}
-
   	};
 
 
@@ -25,13 +33,14 @@ export class CountryDetail extends Component {
 
 		return (
 			<div className="country-detail">
-				{Object.keys(country).map((countryProp) => this.renderCountry(countryProp, country))}
+				{Object.keys(country).map((countryProp) => this.renderCountry(countryProp, country, id))}
 			</div>
 		)
 	}
 }
 
-				// {country.name && country.name !=='' ? <div><h2>{country.name}</h2></div> : false}
-				// {country.capital && country.capital !=='' ? <div><strong>Capital:</strong><span>{country.capital}</span></div> : false}
-				// {country.area && country.area !=='' ? <div><strong>Area:</strong><span>{`${country.area} Km`}</span></div> : false}
-				// {country.borders && country.borders.length !== 0 ? <div><strong>Borders:</strong><ul>{country.borders.map((border,i) => <li key={i}><a href={id}>{border}</a></li>)}</ul></div> : ''}
+// {country.name && country.name !=='' ? <div><h2>{country.name}</h2></div> : false}
+// {country.capital && country.capital !=='' ? <div><strong>Capital:</strong><span>{country.capital}</span></div> : false}
+// {country.area && country.area !=='' ? <div><strong>Area:</strong><span>{`${country.area} Km`}</span></div> : false}
+// {country.borders && country.borders.length !== 0 ? 
+//<div><strong>Borders:</strong><ul>{country.borders.map((border,i) => <li key={i}><a href={id}>{border}</a></li>)}</ul></div> : ''}
