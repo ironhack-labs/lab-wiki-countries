a35b6565851aa63d9ca244d9d83a4dd0338fe341
David Diez <ddiezr@gmail.com>
2018-12-05 19:33:00 +0100

    Exercise Finished

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -25,7 +25,7 @@ export default class App extends Component {
         <div>
           <nav className="navbar navbar-dark bg-primary mb-3">
             <div className="container">
-              <Link className="navbar-brand" to="/CountryDetail">
+              <Link className="navbar-brand" to="/">
                 WikiCountries
               </Link>
             </div>
