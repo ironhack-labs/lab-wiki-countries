e7e5aef43a373c14af9e7bb593dfc9cded4e34f1
jrecio <recio.juan@gmail.com>
2018-12-05 16:00:05 +0100

    Iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import countries from './countries.json'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <nav className="navbar navbar-dark bg-primary mb-3">
+            <div className="container">
+              <a className="navbar-brand" href="/">WikiCountries</a>
+            </div>
+          </nav>
+          <div className="container">
+            <div className="row">
+              <div className="col-5" style={{ "maxHeight": "90vh", "overflow": "scroll" }}>
+                <div className="list-group">
+                  {countries.map((country, index) => {
+                    console.log(country.name.common);
+                    return (<a className="list-group-item list-group-item-action" key={index} href={"/"+country.cca3}> {country.name.common}</a>)
+                  })}
+                </div>
+              </div>
+              <div className="col-7">
+
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
