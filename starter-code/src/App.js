e256257a2ad542648e735e83d8937cd09d6cd61a
sitobusnan <sito@citytonight.es>
2018-12-05 19:36:07 +0100

    completado

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import countries from "./countries.json";
+import { Link, Switch, Route } from "react-router-dom";
+import CountryDetail from "./Components/CountryDetail";
 
 class App extends Component {
   constructor() {
@@ -16,28 +18,34 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark bg-primary mb-3">
+        <div>
+          <nav className="navbar navbar-dark bg-primary mb-3">
+            <div className="container">
+              <a className="navbar-brand" href="/">
+                WikiCountries
+              </a>
+            </div>
+          </nav>
           <div className="container">
-            <a className="navbar-brand" href="/">
-              WikiCountries
-            </a>
-          </div>
-        </nav>
-        <div className="container">
-          <div className="row">
-            <div className="col-5" style={{maxHeight : '90vh', overflow: 'scroll'}}>
-              <div className="list-group">
-                {this.state.countries.map(country => {
-                  return (
-                    <a
-                      className="list-group-item list-group-item-action"
-                      href={country.cca3}
-                    >
-                      {country.flag} {country.name.common}
-                    </a>
-                  );
-                })}
+            <div className="row">
+              <div
+                className="col-5"
+                style={{ maxHeight: "90vh", overflow: "scroll" }}
+              >
+                <div className="list-group">
+                  {this.state.countries.map(country => {
+                    return (
+                      <Link
+                        className="list-group-item list-group-item-action"
+                        to={`/${country.cca3}`}
+                      >
+                        {country.flag} {country.name.common}
+                      </Link>
+                    );
+                  })}
+                </div>
               </div>
+              <Route exact path="/:cca3" component={CountryDetail} />
             </div>
           </div>
         </div>
--- starter-code/src/Components/CountryDetail.js
+++ starter-code/src/Components/CountryDetail.js
@@ -1,11 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import countries from "../countries.json";
+import { Link, Switch, Route } from "react-router-dom";
 
-export default class CountryDetail extends Component {
+class CountryDetail extends Component {
   render() {
+    const myCountry = countries.find(
+      myCountry => myCountry.cca3 === this.props.match.params.cca3
+    );
+
     return (
       <div>
-        
+        <div className="col-7">
+          <h1>{myCountry.name.common}</h1>
+          <table className="table">
+            <thead />
+            <tbody>
+              <tr>
+                <td style={{ width: "30%" }}>Capital</td>
+                <td>{myCountry.capital}</td>
+              </tr>
+              <tr>
+                <td>Area</td>
+                <td>
+                  {myCountry.area} km
+                  <sup>2</sup>
+                </td>
+              </tr>
+              <tr>
+                <td>borders </td>
+                <td>
+                  <ul>
+                    {myCountry.borders.map(e => {
+                      let completCountry = countries.find(myCountry => myCountry.cca3 === e);
+                      return (
+                        <li>
+                          <Link to={e}>{completCountry.name.common}</Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    )
+    );
   }
 }
+
+export default CountryDetail;
