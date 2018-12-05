5311f3c2da0ff572afa102580801b323d8a2ff22
Sonia Ruiz <yune.vk@gmail.com>
2018-12-05 18:22:26 +0100

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import countries from './countries.json'
+
+import { Switch, Route, Link } from 'react-router-dom';
+import CountryDetail from "./components/CountryDetail/CountryDetail";
 
 class App extends Component {
+  findCountry = id => countries.filter(country => country.cca3 === id)[0]
+
   render() {
+    let options = countries.map(country => 
+      <Link to={`/${country.cca3}`} className="list-group-item list-group-item-action">{country.flag} {country.name.common}</Link>
+    )
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav className="navbar navbar-dark bg-primary mb-3">
+          <div className="container">
+            <a className="navbar-brand" href="/">WikiCountries</a>
+          </div>
+        </nav>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-5 select-countries">
+              <div className="list-group">
+                {options}
+              </div>
+            </div>
+            <div className="col-7">
+            <Switch>
+              <Route exact path='/:id' render={routerProps => {
+                const id = routerProps.location.pathname.slice(1,routerProps.location.pathname.length);
+                return <CountryDetail teo="holaTeo" country={this.findCountry(id)} findFnc={this.findCountry}/>}}  />
+                }
+            </Switch>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ starter-code/src/components/CountryDetail/CountryDetail.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class CountryDetail extends Component {
+  render() {
+    const borders = this.props.country.borders.map(border => {
+      const borderInfo = this.props.findFnc(border);
+      return <li><Link to={`/${borderInfo.cca3}`}>{borderInfo.name.common}</Link></li>
+    })
+    return (
+      <React.Fragment>
+        <h1>{this.props.country.name.common}</h1>
+        <table className="table">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td className="capital">Capital</td>
+              <td>{this.props.country.capital[0]}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>{this.props.country.area} km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                <ul>
+                  {borders}
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </React.Fragment>
+    )
+  }
+}
