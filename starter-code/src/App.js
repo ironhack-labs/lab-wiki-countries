08be99244e2426237e0bda8871f19e9c9de23bdb
Jorge Devesa <jrgdevesa@gmail.com>
2018-12-05 21:23:08 +0100

    Iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './App.css';
+import { Link, Switch, Route } from 'react-router-dom';
+import "./App.css";
 import countries from "./countries.json";
-import Menu from "./Components/Menu/Menu"
+import Menu from "./Components/Menu/Menu";
+import CountryDetail from "./Components/CountryDetail/CountryDetail";
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = { countries: countries }
+  constructor() {
+    super();
+    this.state = { countries: countries };
   }
 
-
-
   render() {
+
+
     return (
       <div className="App">
-      <nav class="navbar navbar-dark bg-primary mb-3">
-        <div class="container">
-          <a class="navbar-brand" href="/">WikiCountries</a>
+        <nav class="navbar navbar-dark bg-primary mb-3">
+          <div class="container">
+            <a class="navbar-brand" href="/">
+              WikiCountries
+            </a>
+          </div>
+        </nav>
+        <div className="flex">
+          <div
+            className="col-5"
+            style={{ maxHeight: "90vh", overflow: "scroll" }}
+          >
+            <Menu
+              className="list-group-item list-group-item-action"
+              countries={this.state.countries}
+            />
+          </div>
+          <div className="col-7">
+            <Switch>
+              <Route exact path="/:id" component={CountryDetail} />
+            </Switch>
+          </div>
         </div>
-      </nav>
-      <Menu className="list-group-item list-group-item-action" countries={this.state.countries}></Menu>
       </div>
     );
   }
