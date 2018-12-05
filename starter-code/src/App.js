2e718c8b276a03cab33fe7a512429c2ef31accb2
cocobilbao <cocobilbao@gmail.com>
2018-12-05 18:32:36 +0100

    country detail structure

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -31,12 +31,66 @@ class App extends Component {
                 <div class="list-group">
                   {this.state.country.map(country => (
                     <a class="list-group-item list-group-item-action">
-                     <Link to='/'><Countries {...country} /></Link> 
+                      <Link to="/country-detail">
+                        <Countries {...country} />
+                      </Link>
                     </a>
                   ))}
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div class="countryDetail">
+          <div class="col-7">
+            <h1>France</h1>
+            <table class="table">
+              <thead />
+              <tbody>
+                <tr>
+                  <td style={{ width: "30%" }}>Capital</td>
+                  <td>Paris</td>
+                </tr>
+                <tr>
+                  <td>Area</td>
+                  <td>
+                    551695 km
+                    <sup>2</sup>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Borders</td>
+                  <td>
+                    <ul>
+                      <li>
+                        <a href="/AND">Andorra</a>
+                      </li>
+                      <li>
+                        <a href="/BEL">Belgium</a>
+                      </li>
+                      <li>
+                        <a href="/DEU">Germany</a>
+                      </li>
+                      <li>
+                        <a href="/ITA">Italy</a>
+                      </li>
+                      <li>
+                        <a href="/LUX">Luxembourg</a>
+                      </li>
+                      <li>
+                        <a href="/MCO">Monaco</a>
+                      </li>
+                      <li>
+                        <a href="/ESP">Spain</a>
+                      </li>
+                      <li>
+                        <a href="/CHE">Switzerland</a>
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
--- starter-code/src/components/Countries/Countries.js
+++ starter-code/src/components/Countries/Countries.js
@@ -4,6 +4,7 @@ export default class Countries extends Component {
   render() {
     return (
       <div>
+          {this.props.flag}
      {this.props.name.common}
       </div>
     );
