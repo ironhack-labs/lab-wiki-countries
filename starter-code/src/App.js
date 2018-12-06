90eac65b6865280d26bdd988eb67379006efba6e
Simon G Negrete <simongnegrete@MacBook-Pro-de-Simon.local>
2018-12-06 09:17:02 +0100

    interacción 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Link} from "react-router-dom"
+import country from "./countries.json"
 
 class App extends Component {
+  
+ state = {country: country} 
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav className="navbar navbar-dark bg-primary mb-3">
+          <div className="container">
+          <Link className="navbar-brand" to="/">WikiCountries</Link>
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-5" style={{maxHeight: "90vh", overflow: "scroll"}}>
+            <div className="list-group">
+            
+              { this.state.country.map((country,index) => 
+
+                <Link className="list-group-item list-group-item-action" to="/ABW"> {country.flag}{country.name.common}</Link>) 
+      }
+            </div>
+          </div>
+          </div>
+          </div>
+
       </div>
     );
   }
---
+++ starter-code/src/Component/CountryDetail.js
@@ -0,0 +1,18 @@
+
+    <div>
+        <nav class="navbar navbar-dark bg-primary mb-3">
+            <div class="container">
+                <a class="navbar-brand" href="/">WikiCountries</a>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <div class="col-5" style="max-height: 90vh; overflow: scroll;">
+                    <div class="list-group">
+                        <a class="list-group-item list-group-item-action" href="/ABW">🇦🇼 Aruba</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
