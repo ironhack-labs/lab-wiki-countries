2f5a852c9bd479196c3fee145c4135fc5ff382e8
jaqusa <dev@javierquinte.com>
2018-12-12 20:36:19 -0600

    iteration 1 and 2.1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Routes from './Routes'
+import Axios from 'axios'
+import {Link,NavLink} from 'react-router-dom'
+
 
 class App extends Component {
+  state = {
+    countries: []
+
+  };
+
+  componentDidMount(){
+    Axios.get('https://raw.githubusercontent.com/mledoze/countries/master/countries.json')
+      .then(res=>{
+          const countries = res.data
+          this.setState({countries})
+        
+      })
+    
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    <div id="root">
+
+    <div>
+      <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">WikiCountries</a>
+        </div>
+      </nav>
+    <div className="container">
+        <div className="row">
+          <div className="col-5" style={{maxHeight: "90vh", overflow: "scroll"}}>
+            <div className="list-group">
+           { this.state.countries.map((country,index) => {
+             return <NavLink className="list-group-item list-group-item-action" to={`/${country.cca3}`}> {country.flag} {country.name.official} </NavLink>
+            }) }
+            </div>
+          </div>
+          
+          <Routes/> 
+
+        </div>
       </div>
+    </div>
+  </div>
+
     );
   }
 }
