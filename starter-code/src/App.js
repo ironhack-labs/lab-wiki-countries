70d9747cb75e3ed1a039182af7bb951952ba619e
Zenchy <alvaro.corral.soilan@gmail.com>
2019-02-20 19:55:21 +0100

    Cambio de ruta mostrando detalles de paises

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,29 +1,31 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import CountryDetail from "./components/Countrydetail";
 import { Link } from "react-router-dom";
 import countries from "./countries.json";
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <nav className="navbar navbar-dark bg-primary mb-3">
-         <div className="container">
-           <a className="navbar-brand" href="/">
-             WikiCountries
-           </a>
-         </div>
-       </nav>
+        <nav className="navbar navbar-dark bg-primary mb-3">
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              WikiCountries
+            </a>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/countries/:id" component={CountryDetail} />
+        </Switch>
         {countries.map((country, idx) => {
           return (
             <div key={idx}>
               <h3>
                 <Link to={`/countries/${country.cca3}`}>
-                  {country.name.official}{country.flag}
+                  {country.name.official}
+                  {country.flag}
                 </Link>
               </h3>
             </div>
--- starter-code/src/components/Countrydetail.jsx
+++ starter-code/src/components/Countrydetail.jsx
@@ -1,14 +1,26 @@
-import React, { Component } from "react";
+import React,{Component} from "react"
+import Countries from "../countries.json";
+
+const Details = (props) =>{
+  console.log(props);
 
 
-class Details extends Component {
-  render() {
-    return (
-       <div className="Details">
-       <span>Hey hey</span>
-       </div>
-    );
-  }
+  return(
+      <div>
+          {Countries.map((country,idx)=>{
+              if(country.cca3 == props.match.params.id){
+
+                  return(
+                      <div key={idx}>
+                          <p>{country.capital[0]}</p>
+                          <p>{country.area}</p>
+                          <p>{country.borders}</p>
+                      </div>
+                  )
+              }
+          })}
+      </div>
+  )
 }
 
 export default Details;
