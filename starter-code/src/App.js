e3e827bc5dfca908626ecd2612e65e84d8505b18
Deie <chantalsauvann@gmail.com>
2019-02-21 17:25:12 +0100

    Paris - Poeli & Delphine

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, NavLink } from 'react-router-dom';
+import allCountries from './countries.json';
+import CountryDetail from './CountryDetail';
 
+
+function countryUrl(country) {
+  return `/country/${country.cca3}`;
+}
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { countriesArray: allCountries };
+  }
+
   render() {
+
+    const { countriesArray } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Wiki Countries</h1>
         </header>
-      </div>
+
+        <div className="container-fluid">
+          <div className='row'>
+            
+            <aside className='col-5'>
+              <h2>Countries List</h2>
+              <nav className="list-group">
+                {countriesArray.map(oneCountry => {
+                  return (
+                    <NavLink key={oneCountry.cca3} to={countryUrl(oneCountry)}>{oneCountry.name.common}</NavLink>                 
+                  )
+                })}              
+              </nav>
+            </aside>
+
+            <main className='col-7'>
+              <h2>Countries Detail</h2>
+              <Switch>
+                <Route path='/country/:countryId' component={CountryDetail} />
+                <Route/>
+              </Switch>
+            </main>
+
+          </div> {/*  end row */}
+        </div> {/*  end container-fluid */}
+
+      </div> //  end app 
     );
   }
 }
---
+++ starter-code/src/CountryDetail.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './CountryDetail.css';
+import allCountries from './countries.json';
+
+
+class CountryDetail extends Component {
+    render() {
+
+        const { params } = this.props.match;
+
+        const countryItem = allCountries.find(oneCountry => {
+            return oneCountry.cca3 === params.countryId;
+        })
+
+        return (
+            <div className='CountryDetail'>
+                {countryItem
+                    ?
+                    (<table>
+                        <thead>
+                            <tr>
+                                <th><h2>{countryItem.name.common}</h2></th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td><p>Capital</p></td>
+                                <td>{countryItem.capital}</td>
+                            </tr>
+
+                            <tr>
+                                <td><p>Area</p></td>
+                                <td>{countryItem.area}</td>
+                            </tr>
+
+                            <tr>
+                                <td><p>Borders</p></td>
+                                <td>
+                                    {countryItem.borders.map(countryBorder => {
+                                        return (
+                                            <li key={countryBorder}>{countryBorder}</li>
+                                        )
+                                    })}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>)
+                        :
+                            ( <p>coucou</p> )}
+             </div>
+        );
+    }
+}
+
+
+
+// {
+//     projectItem
+//         ?
+//         (<div>
+//             <h2>Project Detail</h2>
+
+//             <h3>{projectItem.name} ({projectItem.year})</h3>
+//             <p>{projectItem.description}</p>
+
+//             <h3>Technos :</h3>
+//             <p>{projectItem.technologies}</p>
+//         </div>)
+//         :
+//         (<h2>Project Detail</h2>)
+// } 
+
+
+
+export default CountryDetail;
