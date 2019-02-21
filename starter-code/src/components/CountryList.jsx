243f4de33ac0f180aea2be386119cb4e25410777
Nira <niralonso@gmail.com>
2019-02-21 09:31:09 +0100

    .

---
+++ starter-code/src/components/CountryList.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import countries from '../countries.json'
+
+export default class CountryList extends Component {
+
+  state = {
+    countries: [...countries]
+  }
+
+  render() {
+    return (
+      <div className="list-group">
+        {this.state.countries.map(country => {
+          return <Link
+            className="list-group-item list-group-item-action"
+            to={`/${country.cca3}`}
+            key={country.cca3}>
+            {country.flag} {country.name.official}
+          </Link>
+        })}
+      </div>
+
+    )
+  }
+}
