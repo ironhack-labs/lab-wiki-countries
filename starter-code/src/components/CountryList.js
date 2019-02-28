18d05990481f77c26df46b1dbee840722aaa55d5
Andres <andreshm94@gmail.com>
2019-02-27 21:53:06 -0600

    Completed⚽️

--- starter-code/src/components/CountryList.js
+++ starter-code/src/components/CountryList.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default class ListMenu extends React.Component {
+    
     state = {
         countries: [...this.props.countries]
     }
+
+
     render() {
         return (
             <div className="list-group">
                 {this.state.countries.map((country, index) => {
                     return (
-                        <NavLink className="list-group-items list-group-item-action"
-                        to={"/" + country.cca3}
-                        key={index}>
-                        {country.flag} {country.name.official}
+                        <NavLink className="list-group-item list-group-item-action"
+                            to={"/" + country.cca3}
+                            key={index}>
+                            {country.flag} {country.name.official}
                         </NavLink>
                     )
                 })}
--- starter-code/src/components/CountryView.js
+++ starter-code/src/components/CountryView.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 
@@ -8,41 +8,41 @@ export default class CountryDetail extends Component {
         countries: [...this.props.countries]
     }
 
-    
+
     borderFinder = (country) => {
         return this.state.countries.find(elem => elem.cca3 === country).name.common
     }
 
-    render () {
-    let country = this.state.countries.filter(country => country.cca3 === this.props.match.params.cca3)[0]
-    return (
-        <div className="container" style={{textAlign: "left"}}>
-            <h3>{country.name.common}</h3>
-            <table className="table">
-                <tbody>
-                    <tr>
-                        <td>Capital </td>
-                        <td>{country.capital} </td>
-                    </tr>
-                    <tr>
-                        <td>Area </td>
-                        <td>{country.area} km2</td>
-                    </tr>
-                    <tr>
-                    <td>Borders</td>
-                        <td>
-                            <ul>
-                                {country.borders.map((elem, index) => <li key={index}> <Link to={"/" + elem}>{this.borderFinder(elem)}</Link></li> )
-                                }
-                            </ul>
-                        </td>
-                    </tr>
+    render() {
+        let country = this.state.countries.filter(country => country.cca3 === this.props.match.params.cca3)[0]
+        return (
+            <div className="container" style={{ textAlign: "left" }}>
+                <h3>{country.name.common}</h3>
+                <table className="table">
+                    <tbody>
+                        <tr>
+                            <td>Capital </td>
+                            <td>{country.capital} </td>
+                        </tr>
+                        <tr>
+                            <td>Area </td>
+                            <td>{country.area} km2</td>
+                        </tr>
+                        <tr>
+                            <td>Borders</td>
+                            <td>
+                                <ul>
+                                    {country.borders.map((elem, index) => <li key={index}> <Link to={"/" + elem}>{this.borderFinder(elem)}</Link></li>)
+                                    }
+                                </ul>
+                            </td>
+                        </tr>
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
 
-        </div>
-    )
+            </div>
+        )
     }
 
 }
