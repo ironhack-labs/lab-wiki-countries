2255b0d1a084c55778d63f0096968a5835d5f926
miguelangelosoriocruz <ing.mosorio19@gmail.com>
2019-02-28 19:12:29 -0600

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import countries from './countries.json'
+import { Link, Switch, Route } from 'react-router-dom'
+import CountryDetails from './components/CountryDetail'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="row">
+          <div className="col-5">
+            <div className="list-group">
+              {countries.map((valor, key ) => {
+                return(
+                <div key={key}>
+                    <Link to={`/countries/${valor.cca3}`} className="list-group-item list-group-item-action"><span>{valor.flag}</span>{valor.name.common}</Link>
+                </div>)
+              })}
+            </div>
+          </div>
+          <div className="col-7">
+              <Switch>
+                <Route exact path='/countries/:cca3' component={CountryDetails} />
+              </Switch>
+          </div>
+        </div>
       </div>
     );
   }
