1d88abe18e23a761cd7a5f7c8b45f81892bd64f1
CGuillotG <c.guillot@outlook.com>
2019-03-05 09:54:39 -0600

    Removed empty placeholder

--- starter-code/src/Routes.jsx
+++ starter-code/src/Routes.jsx
@@ -6,7 +6,7 @@ import Page404 from './components/Page404';
 export default function (){
     return (
         <Switch>
-            <Route exact path="/" component={()=><div>Empty</div>} />
+            <Route exact path="/" component={()=><div></div>} />
             <Route exact path="/:id" component={Detail} />
             <Route component={Page404} />
         </Switch>
