2212a642806a60fa68907d63b071594ac71d9da9
justinezahiri <justinezahiri@gmail.com>
2019-03-06 22:11:33 +0100

    implemented project base + components

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import CountryDetail from './components/CountryDetail.js'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+        <Switch>
+          <Route exact path="/countries/:id" component={CountryDetail} />
+        </Switch>
+        
       </div>
     );
   }
---
+++ starter-code/src/components/CountryDetail.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import countries from 'countries.json';
+
+import { Link } from 'react-router-dom';
+
+//TODO: set the cca3 of each country as his id for the url
