b87b32f0341d81d7c4ba058319fe43348b3d44b9
justinezahiri <justinezahiri@gmail.com>
2019-03-07 17:13:36 +0100

    iteration1 trying to set country id in the url

--- starter-code/src/components/CountryDetail.js
+++ starter-code/src/components/CountryDetail.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import countries from 'countries.json';
+import countries from '../countries.json';
 
 import { Link } from 'react-router-dom';
 
-//TODO: set the cca3 of each country as his id for the url
+const CountryDetail = () => {
+  //TODO: set the cca3 of each country as his id for the url
+let countriesId = countries.map(country => ({
+  id: country.cca3
+}));
+console.log(countriesId);
+
+return (
+  <div>
+
+  </div>
+)
+}
+
+export default CountryDetail;
+
+
