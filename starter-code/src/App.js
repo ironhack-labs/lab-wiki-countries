d0b298c27e317b710955dbffd201ad12f71c2534
EloK6 <ecassignol@gmail.com>
2019-03-07 20:09:41 +0100

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CountryDetail from "./components/CountryDetail";
+import allCountries from "./countries.json";
+import { NavLink, Switch, Route } from "react-router-dom";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allCountriesArray: allCountries
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <nav className="navbar navbar-dark bg-primary mb-3">
+          <div className="container">
+            <div className="navbar-brand">WikiCountries</div>
+          </div>
+        </nav>
+        <div className="row">
+          <div
+            className="col-5"
+            style={{ maxHeight: "90vh", overflow: "scroll" }}
           >
-            Learn React
-          </a>
-        </header>
+            <div className="list-group">
+              {this.state.allCountriesArray.map(eachCountry => {
+                return (
+                  // <div className="scroll">
+                  <NavLink
+                    to={`/${eachCountry.cca3}`}
+                    // activeClassName="active"
+                    key={eachCountry.cca3}
+                    className="list-group-item list-group-item-action"
+                  >
+                    {eachCountry.flag}
+                    {eachCountry.name.common}
+                  </NavLink>
+                  // </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="col-sm-7">
+            <Switch>
+              <Route exact path="/:id" component={CountryDetail} />
+            </Switch>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ starter-code/src/components/CountryDetail.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import countries from "../countries.json";
+import { Link } from "react-router-dom";
+
+class CountryDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { params } = this.props.match;
+
+    function getCountry(id) {
+      function byCca3(oneCountry) {
+        return oneCountry.cca3 === id;
+      }
+      return countries.find(byCca3);
+    }
+
+    const foundCountry = getCountry(params.id);
+
+    return (
+      <section className="CountryDetail">
+        <h2>{foundCountry.name.common}</h2>
+        <table className="table">
+          <thead />
+          <tbody>
+            <tr>
+              <td style={{ width: "30%" }}>Capital</td>
+              <td>{foundCountry.capital}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>
+                {foundCountry.area} km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                <ul>
+                  {/* {console.log(foundCountry.borders)} */}
+                  {foundCountry.borders.map(oneBorder => {
+                    // return <li>{oneBorder}</li>;
+                    return (
+                      <li>
+                        <Link to={`/${getCountry(oneBorder).cca3}`}>
+                          {getCountry(oneBorder).name.common}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    );
+  }
+}
+
+export default CountryDetail;
