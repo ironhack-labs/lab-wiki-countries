d5c8709822f066932cb933c37d3759cb6eceabf6
justinezahiri <justinezahiri@gmail.com>
2019-03-07 21:35:44 +0100

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,58 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import CountryDetail from './components/CountryDetail.js'
-
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route, NavLink } from "react-router-dom";
+import countries from "./countries.json";
+import CountryDetail from "./components/CountryDetail.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
-        <Switch>
-          <Route exact path="/countries/:cca3" component={CountryDetail} />
-        </Switch>
-        
+        <div>
+          <nav className="navbar navbar-dark bg-primary mb-3">
+            <div className="container">
+              <a className="navbar-brand" href="/">
+                WikiCountries
+              </a>
+            </div>
+          </nav>
+
+          <div className="container">
+            <div className="row">
+              <div
+                className="col-5"
+                style={{ maxHeight: "90vh", overFlow: "scroll" }}
+              >
+                <div className="list-group">
+                  {countries.map((country, index) => {
+                    return (
+                      <NavLink
+                        to={`/countries/${country.cca3}`}
+                        className="list-group-item list-group-item-action"
+                      >
+                        {country.flag}
+                        {country.name.common}
+                      </NavLink>
+                    );
+                  })}
+                </div>
+              </div>
+
+              <div className="col-7">
+                <Switch>
+                  <Route
+                    exact
+                    path="/"
+                    render={() => {
+                      return <p>Bonjour, clique sur un pays a gauche</p>;
+                    }}
+                  />
+                  <Route path="/countries/:id" component={CountryDetail} />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- starter-code/src/components/CountryDetail.js
+++ starter-code/src/components/CountryDetail.js
@@ -1,22 +1,65 @@
-import React from 'react';
-import countries from '../countries.json';
+import React from "react";
+import countries from "../countries.json";
+import { Link } from "react-router-dom";
 
-import { Link } from 'react-router-dom';
+// import { Link } from "react-router-dom";
 
-const CountryDetail = () => {
-//   //TODO: set the cca3 of each country as his id for the url?
-// let id = countries.map(country => ({
-//   id: country.cca3
-// }));
-// console.log(id);
+const CountryDetail = props => {
+  console.log("props");
+  console.log(props);
 
-return (
-  <div>
+  const getCountry = cca3 => {
+    return countries.find(oneCountry => oneCountry.cca3 === cca3);
+  };
 
-  </div>
-)
-}
+  const getCountryName = (cca3) => {
+    const object = getCountry(cca3);
+    return object.name.common;
+  }
+
+  const { params } = props.match;
+  const foundCountry = getCountry(params.id);
+  console.log("country", foundCountry);
+
+  return (
+    <div>
+      <h1> {foundCountry.name.common} </h1>{" "}
+      <table className="table">
+        <thead> </thead>{" "}
+        <tbody>
+          <tr>
+            <td style={{ width: "30%" }}>
+              {" "}
+              Capital
+              {" "}
+            </td>{" "}
+            <td>{foundCountry.capital}</td>{" "}
+          </tr>{" "}
+          <tr>
+            <td> Area </td>{" "}
+            <td>
+              {" "}
+              {foundCountry.area}<sup> 2 </sup>{" "}
+            </td>{" "}
+          </tr>{" "}
+          <tr>
+            <td> Borders </td>{" "}
+            <td>
+              <ul>
+                {foundCountry.borders.map((cca3, index) => {
+
+                  return (<li key={index}>
+                    <Link to={`/countries/${cca3}`}>
+                      {getCountryName(cca3)}
+                    </Link></li>)
+                })}
+              </ul>{" "}
+            </td>{" "}
+          </tr>{" "}
+        </tbody>{" "}
+      </table>{" "}
+    </div>
+  );
+};
 
 export default CountryDetail;
-
-
