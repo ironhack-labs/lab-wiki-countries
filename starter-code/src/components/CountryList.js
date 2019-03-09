29bb1a065f228ec2e2fe092c3faa9e1a6740b131
Rita Mernissi <mernissi.rita@gmail.com>
2019-03-09 10:17:47 +0100

    added a comment for template strings

--- starter-code/src/components/CountryList.js
+++ starter-code/src/components/CountryList.js
@@ -10,6 +10,8 @@ class CountryList extends Component {
           return (
             <NavLink
               key={index}
+              /* si on ne met pas `/$ on aura <a href='FRA'> et non pas <a href='/FRA'> */
+              /* le $ permet de dire que ce qui vient après ce $ est une variable et non pas une string (cf ES6 Template Strings) */
               to={`/${country.cca3}`}
               className="list-group-item list-group-item-action"
             >
