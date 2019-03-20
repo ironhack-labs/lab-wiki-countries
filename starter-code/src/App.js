9634cd80adf3f346bb3c47ca663eae171ed9690a
GomezOrtiz <david.gomez.mail@gmail.com>
2019-03-20 23:25:52 +0100

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import countries from "./countries.json"
+import CountryDetail from "./components/CountryDetail.js"
+
+import './App.css'
 
 class App extends Component {
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      
+      <main>
+
+        <nav className="navbar navbar-dark bg-primary mb-3">
+
+          <div className="container">
+            <Link className="navbar-brand" to="/">WikiCountries</Link>
+          </div>
+      
+        </nav>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-5 scrollable">
+              <div className="list-group">
+                {
+                  countries.map((country, idx) => {
+                    return <Link key={idx} className="list-group-item list-group-item-action" to={`/countries/${country.cca3}`}>{country.flag} {country.name.common}</Link>
+                  })
+                }
+                </div>
+              </div>
+        <Switch>
+      <Route path="/countries/:cca3" component={CountryDetail} />
+        </Switch>
+        </div>
+
+        </div>
+      </main>
+    )
   }
 }
 
-export default App;
+export default App
