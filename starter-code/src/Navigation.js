aefd3c9aa0e83fa6f87b14d77cd796cbd39f894b
Raúl Contreras <raulc98@gmail.com>
2019-05-01 21:08:13 +0200

    Initial commit

---
+++ starter-code/src/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Navigation extends Component {
+  render() {
+    return (
+    <nav class="navbar navbar-dark bg-primary mb-3">
+      <div class="container">
+        <a class="navbar-brand" href="/">WikiCountries</a>
+      </div>
+    </nav>
+    )
+  }
+}
