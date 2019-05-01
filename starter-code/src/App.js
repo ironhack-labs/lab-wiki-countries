123c34dec2c6cd8c66e09d72e15016df46cce597
raveBolognesa <rafael.arrieta.gar@gmail.com>
2019-05-01 16:06:57 +0200

    commit

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CountryDetail from "./components/CountryDetail";
+import countries from "./countries.json"
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allCountries: countries,
+      currentCountrie: undefined
+    };
+  }
   render() {
     return (
-      <div className="App">
+      <React.Fragment className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
-      </div>
+        <div className="row mt-3">
+          <div className="col-5 ml-3">
+          {this.state.allCountries.map(yaEsViernes=> <CountryDetail name={yaEsViernes.name.common} />)}
+            
+          </div>
+          <div className="col-6">
+            <CountryDetail />
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
---
+++ starter-code/src/components/CountryDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CountryDetail extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+}
