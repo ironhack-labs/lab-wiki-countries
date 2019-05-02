5e966ca50907b135914bab2809ef35ff772ed255
raveBolognesa <rafael.arrieta.gar@gmail.com>
2019-05-02 09:52:38 +0200

    commit

--- starter-code/src/About.js
+++ starter-code/src/About.js
@@ -12,33 +12,19 @@ class About extends React.Component {
   }
 
   render() {
-    let selectedMovie = this.state.movies.filter(movie => {
-      return movie.name.common === this.props.match.params.id;
+    let miPais = this.state.movies.filter(patata => {
+      return patata.name.common === this.props.match.params.id;
     });
 
-    const sectionHeader = (
-      <React.Fragment>
-        <h3>About</h3>
-        <p>Welcome to the about section</p>
-      </React.Fragment>
-    );
 
-    if (selectedMovie.length === 0) {
       return (
         <div>
-          {sectionHeader}
+          
+        <h3>Wiki paises</h3>
 
-          <h1>Not found</h1>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          {sectionHeader}
-
-          <p>Your chosen movie is '{selectedMovie[0].name.common}'</p>
+          <p>Tu pais es '{miPais[0].name.common}'</p>
           <ul>
-            {selectedMovie[0].borders.map(element => {
+            {miPais[0].borders.map(element => {
                 var findedurl = countries.filter(country=> country.cca3 === element)
                 var cubs = "/about/" + findedurl[0].name.common;
               return(  <React.Fragment>
@@ -51,7 +37,7 @@ class About extends React.Component {
           </ul>
         </div>
       );
-    }
+    
   }
 }
 
