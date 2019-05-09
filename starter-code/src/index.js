698468fc9f08313aa73d8d26bbe2428cf702ac0f
betoiron1988 <galactico10@live.com.mx>
2019-05-08 23:24:08 -0500

    DE MEX - HUMBERTO

--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from "react-router-dom";
+import Routes from './Routes';
+import CountryList from './components/CountryList';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+class App extends React.Component {
+    render() {
+      return (
+        <div className="app">
+            <CountryList/>
+            <Routes/>
+        </div>
+      );
+    }
+  }
+ 
+
+ReactDOM.render(<BrowserRouter>
+    <App />
+</BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
