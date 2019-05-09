26b70a1229cbcf777e375cf1fb7a061758de19e8
Marypztr <nutrimary.77@gmail.com>
2019-05-09 09:24:35 -0500

    DE MEX MARYJOSE

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route } from 'react-router-dom'
+import countries from './countries.json'
+import countriesDetails from './components/CountriesDetails'
+import { Layout } from 'antd';
+
+const { Header } = Layout;
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Layout>
+          <Header>
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="1">Wiki Countries</Menu.Item>
+            </Menu>
+          </Header>
+        </Layout>
+      <h2>Countries</h2>
+      {countries.map((oneCountrie, index)=>
+      <div key= {oneCountrie.id}/>
+      <h3><Link to ={`/${oneCountrie.cca3}`}>{oneCountrie.name.common}</Link></h3>
+        )}
+
       </div>
     );
   }
