6a9f00bbadf3be1afefec0467221a7e0a94985ae
eskalona17 <eskalona_17@hotmail.com>
2019-05-30 19:57:57 +0200

    first

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import countries from './data/countries.json'
+import CountryList from './components/CountryList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div className="col-5" style={{'height': '90vh', overflow: 'scroll'}}>
+                <CountryList countries = {countries}/>
+              </div>
+            <Switch>
+                <Route exact path='/:id' component={CountryList}/>
+              </Switch>
+     
       </div>
     );
   }
