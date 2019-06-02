34e09ec1cb9f1c35abd644da587b150760ae5b2b
MartaVaqgut <martavaquerogutierrez@gmail.com>
2019-06-02 20:30:33 +0200

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './scenes/Home'
+import { Switch, Route } from 'react-router-dom';
+import CountryDetails from './components/CountryDetails'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Home />
+        <Switch>
+          <Route exact path="/:cca3" component={CountryDetails} />
+        </Switch>
       </div>
     );
   }
---
+++ starter-code/src/components/CountryDetails.js
@@ -0,0 +1,51 @@
+import React, {Fragment} from 'react'
+import countries from '../countries.json'
+import {Link} from 'react-router-dom'
+
+class CountryDetails extends React.Component {
+    state = {
+        country: {}
+    }
+
+    render() {
+        const cca3 = this.props.match.params.cca3;
+        const country = countries.find(country => country.cca3 === cca3);
+        const list = country.borders.map((item,i) => {
+            const borderCountry = countries.find(country => country.cca3 === item)
+            const nameCountry = borderCountry.name
+            return <li key={i}><Link to={`/${item}`}>{item}</Link></li>
+        })
+        return (
+            <Fragment>
+                <div className="col-7">
+                    <h1>{country.name.official}</h1>
+                    <table className="table">
+                        <thead></thead>
+                        <tbody>
+                            <tr>
+                                <td style={{width: '30%'}}>Capital</td>
+                                <td>{country.capital}</td>
+                            </tr>
+                            <tr>
+                                <td>Area</td>
+                                <td>{country.area} km
+                                    <sup>2</sup>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Borders</td>
+                                <td>
+                                    <ul>
+                                        {list}
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default CountryDetails
