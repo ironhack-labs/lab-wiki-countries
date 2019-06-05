b69c577974d9fb03606baa7206c90c7d0d6135b1
Marnelas <marnelasbenito@gmail.com>
2019-06-05 19:47:41 +0200

    first interation complete

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Link } from "react-router-dom";
+
+import CountryDetail from "./components/CountryDetail"
+import countries from "./countries.json"
+
 
 class App extends Component {
+    constructor() {
+      super()
+    this.state =  {
+      countries
+      
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h1>Daily con Rutas</h1>
         </header>
+        <main>
+          <ul>
+            {this.state.countries.map(elm => <li><Link to={`/${elm.cca3}`}>{elm.name.common}</Link></li>)}
+          </ul>
+        </main>
+
+        <Switch>
+          <Route path="/:id" exact component={CountryDetail} />
+        </Switch>
       </div>
     );
   }
