c5d53aa5e08af8326c650916f567a1a0c84a3257
pdiv55 <paul.divet5@gmail.com>
2019-06-12 00:23:23 +0000

    ajuste

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import CountryDetail from '../src/CountryDetail'
+import CountryList from '../src/CountryList'
+
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar />
+        <CountryList />
+        <Switch>
+          <Route exact path='/:countryId' component={CountryDetail} />
+        </Switch>
       </div>
     );
   }
