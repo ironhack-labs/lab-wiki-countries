d5fd66d76045f752ebacb95ea6300ee261d8bf9d
rubander <ralexanderv89@gmail.com>
2019-07-17 21:09:30 +0200

    hecho

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route, Redirect, Link } from "react-router-dom";
+import CountryDetail from "./CountryDetail";
+import countries from "./countries.json";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.countries = {
+      list: [...countries]
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* <Switch>
+          <Route
+            exact
+            path="/movies"
+            render={() => {
+              return <Home movies={this.state.movies} />;
+            }}
+          />
+          <Route
+            exact
+            path="/viewMovie/:chosenMovie"
+            render={props => {
+              var chosenMovie = props.match.params.chosenMovie;
+              return <MovieDetail movie={this.state.movies[chosenMovie]} />;
+            }}
+          />
+        </Switch> */}
+
+        <div className="container">
+          <div className="row">
+            <div
+              className="col-5"
+              style={{ maxHeight: "90vh", overflow: "scroll", width:"1000px" }}
+            >
+              <div className="list-group">
+                {this.countries.list.map((country, idx) => {
+                  return (
+                    <div
+                      className="list-group-item list-group-item-action"
+                      key={idx}
+                    >
+                      {" "}
+                      <Link to={"/country/" + country.cca3}>
+                      <span role="img" aria-label={country.name.common}>
+                        {country.flag}
+                      </span>{" "}
+                      {country.name.common}
+                      </Link>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <Switch>
+                <Route exact path="/country/:chosenCountry"
+                  render={
+                    props => {
+                    let chosenCountry = props.match.params.chosenCountry;
+                    let filteredArray = this.countries.list.filter(country => {
+                      return (
+                        country.cca3
+                          .toLowerCase()
+                          .indexOf(chosenCountry.toLowerCase()) >= 0
+                      );
+                    });
+                    return <CountryDetail country={filteredArray[0]} allCountries={this.countries.list}  />;
+                  }}
+                />
+                {/* <Route component={notFound404} /> */}
+              </Switch>
+          
+          </div>
+        </div>
       </div>
     );
   }
--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
