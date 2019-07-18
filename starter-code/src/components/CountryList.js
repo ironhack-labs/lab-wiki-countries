abc5c52642d9607ba0d3c4730574a3c2502cf10b
mariana-gamino <mariianagamino@gmail.com>
2019-07-18 15:14:39 -0500

    Ya funciona el scroll

--- starter-code/src/components/CountryList.js
+++ starter-code/src/components/CountryList.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import data from '../countries.json'
+import countryDetail from './CountryDetail.js';
+import {Route} from 'react-router-dom'
 
 class countryList extends Component{
   state={
@@ -13,15 +15,16 @@ class countryList extends Component{
       <>
       <div class="container">
       <div class="row">
-      <div class="col-5"></div>
-      <div class="list-group">
-      {countries.map(country => (
-          <div key={country.cca3}>
-            <Link to={`/country/${country.cca3}`}>
-            <button class="list-group-item list-group-item-action"><span>{country.flag}</span>{country.name.common}</button>
-            </Link>
-          </div>
-        ))}
+      <div class="col-10" style={{height:'90vh', width: '30vw', overflow: 'scroll'}}>
+        <div class="list-group">
+        {countries.map(country => (
+            <div key={country.cca3}>
+              <Link to={`/country/${country.cca3}`}>
+              <button class="list-group-item list-group-item-action"><span>{country.flag}</span>{country.name.common}</button>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
       </div>
       </div>
