0479d9f21a754818d736f9198b74c4f4e9088707
Daniel Orio <danielislas3@live.com>
2019-07-18 00:51:12 -0500

    Ya mustra lista con banderas

---
+++ starter-code/src/Router.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import CountryDetail from './components/CountryDetail'
+import CountryContainer from './components/CountryContainer'
+
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={CountryContainer}/>
+        <Route path="/country/:id" component={CountryDetail}/>
+
+      </Switch>
+
+      </BrowserRouter>
+  )
+}
+
+export default Router
---
+++ starter-code/src/components/CountryContainer.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import Data from '../countries.json'
+
+
+export default class CountryContainer extends Component {
+
+  state={
+    countries: Data
+  }
+  render() {
+    const {countries} = this.state
+
+    return (
+      <>
+        <div class="container">
+          <div class="row">
+            <div class="col-5" style={{maxHeight:' 90vh', overflow: 'scroll'}}>
+              
+              {/* Div de pais
+               <a class="list-group-item list-group-item-action" href="/ABW">🇦🇼 Aruba</a>
+              */}
+              {
+                countries.map((country,id)=>(
+                  <div class="list-group" style={{textAlign:'initial'}}key={id}>
+                    <Link className="list-group-item list-group-item-action" to={`/country/${country.cca3}`}>
+                    <span >{country.flag} {country.name.common}</span>
+                    </Link>
+                  </div>
+                ))
+              }
+              
+            </div>
+          </div>
+        </div>
+          <div class="col-7"></div>
+        
+      </>
+    )
+  }
+}
