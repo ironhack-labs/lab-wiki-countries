d959a406ba0032fdb519b28706367ed46b246c5c
bryanmej <bryanmej@gmail.com>
2019-07-21 19:48:19 -0500

    daily done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
+import {Switch, Route, Link} from 'react-router-dom'
+import CountryList from './components/CountryList'
+import CountryDetail from './components/CountryDetail'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-primary mb-3">
+      <div className="container">
+        <Link to='/'>
+          <span className="navbar-brand">WikiCountries</span>
+        </Link>
+      </div>
+    </nav>
+  )
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Navbar/>
+
+        <div className="container">
+          <div className="row">
+
+            <div className="col-5">
+              <CountryList/>
+            </div>  
+
+            <div className="col-7">   
+              <Switch>
+                <Route path="/details/:id" component={CountryDetail} />
+              </Switch>
+            </div>
+
+          </div>  
+        </div>
+     </>  
     );
   }
 }
---
+++ starter-code/src/components/CountryList.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import countries from '../countries.json'
+import {Link} from 'react-router-dom'
+
+class CountryList extends Component {
+
+  state = {
+    countries
+  }
+
+  render() {
+    return(
+          <ul className="list-group text-left" style={{height: '90vh', overflow: 'scroll'}}>
+            {this.state.countries.map((country, i)=> 
+              <li key={i} className="list-group-item list-group-item-action">
+                <Link to={`/details/${country.cca3}`}>
+                <span> {country.flag} {country.name.common}</span>
+                </Link>
+              </li>)}
+          </ul>
+      
+      
+    )
+  }
+
+}
+
+export default CountryList
