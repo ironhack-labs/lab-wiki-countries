756e732c24ea777ed8dfc9f2b38813313dbe5503
mario salvatierra <msalv006@fiu.edu>
2019-07-25 13:18:41 -0400

    first commit some boostrap

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,24 +5,76 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div>
+      
+<nav className="navbar navbar-light bg-primary">
+  <a className="navbar-brand" href="/">
+    <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""/>
+  </a>
+ <ul className="navbar-nav mr-auto">
+   <li className="text-white">WikiCountries</li>
+ </ul>
+</nav>
+        </div>
+        <div className="row">
+        <div className="col-5">
+
+        <div className="media my-1">
+          <img src="..." class="mr-3" alt="..."/>
+          <div className="media-body">
+          <h5 >Country Media heading</h5>
+          </div>
+        </div>
+
+        <div className="media my-1">
+          <img src="..." class="mr-3" alt="..."/>
+          <div className="media-body">
+          <h5 >Country Media heading</h5>
+          </div>
+        </div>
+
+        </div>
+          <div className="col-7">
+
+
+          <table class="table">
+            <thead>
+              <tr>
+                
+                <th colspan="2" className="">First</th>
+               
+               
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+               
+                <td>Mark</td>
+                <td>Otto</td>
+               
+              </tr>
+              <tr>
+                
+                <td>Jacob</td>
+                <td>Thornton</td>
+               
+              </tr>
+              <tr>
+               
+                <td>Larry</td>
+                <td>the Bird</td>
+              
+              </tr>
+            </tbody>
+          </table>
+
+
+          </div>
+        </div>
       </div>
     );
   }
 }
-
+ 
 export default App;
