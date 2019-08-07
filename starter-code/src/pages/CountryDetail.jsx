9f0b5bac54b4a603abfab634b3c25f48c1064480
Mark Trevail <trevail.m@gmail.com>
2019-08-07 15:33:00 +0200

    Initial

---
+++ starter-code/src/pages/CountryDetail.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CountryDetail extends Component {
+  render() {
+    return (
+      <div className="CountryDetail">
+      </div>
+    );
+  }
+}
+
+export default CountryDetail;
