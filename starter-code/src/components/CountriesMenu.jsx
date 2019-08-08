3a1905daa82f7ef5519ded9b281a04e218a96010
Mark Trevail <trevail.m@gmail.com>
2019-08-08 17:12:10 +0200

    Using axios and api

--- starter-code/src/components/CountriesMenu.jsx
+++ starter-code/src/components/CountriesMenu.jsx
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import "./CountriesMenu.css";
-import countriesData from "../countries.json";
-
-
-// Create array of "country link" objects for the menu:
-
-let CountryLinks = countriesData.map((country) => {
-  return (
-    <Link to={`/country/${country.cca3}`}>
-      <a className="list-group-item list-group-item-action">
-        {country.flag.normalize()} {country.name.common}
-      </a>
-    </Link>
-  );
-});
-
+import axios from 'axios';
+// import countriesData from "../countries.json";
 
 class CountriesMenu extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+        countriesData: []
+    };
+  };
+
+  componentDidMount() {
+    axios.get("https://countries.tech-savvy.tech/countries")
+    .then(response => {
+        this.setState({countriesData: response.data})
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+  
   render() {
+
+    // Create array of "country link" objects for the menu:
+    let CountryLinks = this.state.countriesData.map((country) => {
+      debugger
+      return (
+        <Link to={`/country/${country.cca3}`}>
+          <a className="list-group-item list-group-item-action">
+            {country.flag.normalize()} {country.name.common}
+          </a>
+        </Link>
+      );
+    });
+
+    
     return (
       <div className="col-5">
         <div className="list-group">
--- starter-code/src/components/CountryDetail.jsx
+++ starter-code/src/components/CountryDetail.jsx
@@ -1,63 +1,82 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
 import Button from 'react-bootstrap/Button';
-
-import countriesData from "../countries.json";
+import axios from 'axios';
+// import countriesData from "../countries.json";
 
 
 class CountryDetail extends Component {
 
-  constructor(props) {
-    super(props);
+  constructor(props){
+    super(props)
+    this.state = {
+        countriesData: []
+    };
+  };
+
+  componentDidMount() {
+    debugger
+    axios.get("https://countries.tech-savvy.tech/countries")
+    .then(response => {
+        this.setState({countriesData: response.data})
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   };
 
   render() {
 
-    let countryObj = countriesData.find((country) => {
+    var countryObj = this.state.countriesData.find((country) => {
       return country.cca3 === this.props.match.params.cca3;
-    })
+    });  
 
-    return (
-      <div className="col-7">
-
-        <h1>{countryObj.name.common}</h1>
-
-        <table className="table">
-
-        <thead></thead>
-          <tbody>
-            <tr>
-              <td style={{width: '30%'}}>Capital</td>
-              <td>{countryObj.capital}</td>
-            </tr>
-
-            <tr>
-              <td>Area</td>
-              <td>{countryObj.area} km
-                <sup>2</sup>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Borders</td>
-              <td>
-                <ul>
-                  {countryObj.borders.map((element, index) => {
-                    return (
-                      <li key={index}><Link to={'/country/' + element}>{element}</Link></li>
-                      )
-                  })}
-                </ul>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-
-        <Link to='/'><Button>Home</Button></Link>
-
-      </div>
-    );
+    // Check if countryObj was created successfully (won't be on first run!)
+    if(!countryObj) {  // Is this good practice? Or should countryObj be part of state too. Otherwise the below throws errors if countryObj is undefined
+      return null;
+    } else {
+      return (
+        <div className="col-7">
+  
+          <h1>{countryObj.name.common}</h1>
+  
+          <table className="table">
+  
+          <thead></thead>
+            <tbody>
+              <tr>
+                <td style={{width: '30%'}}>Capital</td>
+                <td>{countryObj.capital}</td>
+              </tr>
+  
+              <tr>
+                <td>Area</td>
+                <td>{countryObj.area} km
+                  <sup>2</sup>
+                </td>
+              </tr>
+  
+              <tr>
+                <td>Borders</td>
+                <td>
+                  <ul>
+                    {countryObj.borders.map((element, index) => {
+                      return (
+                        <li key={index}><Link to={'/country/' + element}>{element}</Link></li>
+                        )
+                    })}
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+  
+          <Link to='/'><Button>Home</Button></Link>
+  
+        </div>
+      );  
+    };
+    
   }
 }
 
