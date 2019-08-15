bf304353b6f0c1043e720cbbc1b35a2dbcd6218b
Carlos Kruger <carloskr@yahoo.com>
2019-08-15 09:21:52 +0200

    [MAD DE]

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CountryDetail from './CountryDetail'
+import countries from './countries.json'
+import { Link } from 'react-router-dom'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      countries: {countries},
+    };
+  }
+
+
   render() {
+    const { hits } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="container">
+      <div className="title"><h1>Wiki Countries</h1></div>
+      
+      <ul>
+        {countries.map(hit =>
+       
+          <li>
+            <div className="row">
+            <div className="col-sm-4">{hit.flag}{hit.name.common}  <Link to={`/CountryDetail/${hit.cca3}`}></Link></div>
+            </div>
+          </li>
+        )}
+      </ul>
       </div>
     );
   }
 }
+
+  
 
 export default App;
