e7a3288a936b7815683b879e3706610eac016590
Elisa <elisam.esquivel@gmail.com>
2019-09-26 01:12:58 -0500

    Exercide completed

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import countries from './countries.json';
 import './App.css';
+import {Link, Switch, Route} from 'react-router-dom';
+import CountryDetail from './components/CountryDetail.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <span className="navbar-brand mb-0 h1">Wiki Countries</span>
+      </nav>
+      <div className="container">
+        <div className="row">
+          <div className="list col-5  overflow-auto">
+              {countries.map(country => (
+                <div key={country.cca3} className="country-box">
+                  <Link className="link" to={`/country/${country.cca3}`}>
+                  <span className="flag">{country.flag}</span>
+                  {country.name.common}
+                  </Link>
+                </div>
+              ))}
+          </div>
+          <div className="col-7 detail">
+            <Switch>
+                <Route exact path="/country/:id" component={CountryDetail} />
+            </Switch>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
