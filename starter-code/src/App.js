29ba14f309537b934a9aaca8c2e3c0f5bc6e07c5
Pepe Sainz <psainz2593@gmail.com>
2019-10-02 23:49:59 +0200

    [MAD DE] PEPE SAINZ

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import countries from './countries';
+import Country from './Country';
+import CountryDetail from './CountryDetail';
+import { BrowserRouter } from "react-router-dom";
+console.log(countries)
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      countries: countries
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+  }
+  render() {
+    return (
+      <div className="App">
+
+        <div className="col-5">
+          <Country countries={this.state.countries}></Country>
+        </div>
+        <div className="col-7"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+          <BrowserRouter exact path='/:country' render={props => {
+            console.log("holi")
+            let selectedCountry = this.state.countries.filter(country => {
+              return country.cca3 === props.match.params.country;
+            })
+
+            return (
+              <CountryDetail {...selectedCountry[0]}></CountryDetail>
+            )
+          }}></BrowserRouter>
+
+        </div>
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ starter-code/src/Country.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import CountryDetail from './CountryDetail';
+
+
+
+class Country extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      countries: props.countries
+    }
+  }
+  render() {
+    return (
+      <div className="list-group">
+        <ul>
+          {this.props.countries.map((country, idx) => {
+            return (
+              <li className="list-group-item list-group-item-action" key={idx}>
+                <Link to={country.cca3}>{country.name.common}</Link>
+              </li>
+            )
+          })}
+        </ul>
+
+      </div>
+    );
+  }
+}
+
+export default Country;
