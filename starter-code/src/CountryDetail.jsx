268fd7f4663b162f14cb091205acd130bc98a98d
Nanotorre <fernandodelatorre4@gmail.com>
2019-10-03 09:06:45 +0200

    [MAD-DE]Fernando de la Torre

--- starter-code/src/CountryDetail.jsx
+++ starter-code/src/CountryDetail.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default class CountryDetail extends Component {
   render() {
     return (
-      <Link to={this.props.href} className="list-group-item list-group-item-action">
+      <NavLink to={this.props.href}
+      activeClassName="active"
+      className="list-group-item list-group-item-action">
         {this.props.flag}
         {this.props.children}
-      </Link>
+      </NavLink>
     )
   }
 }
