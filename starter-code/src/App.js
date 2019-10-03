83973eae0962d60d271c590d14514d54996ff80d
Hoshimotao <jthomas302@gmail.com>
2019-10-03 18:16:22 -0400

    finally finished!

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,12 +5,15 @@ import { Link, Switch, Route } from "react-router-dom";
 import countries from "./countries.json";
 
 function App() {
-  function iterate() {
+  // function iterate() {
+    let iterate = () =>{
     let countryArr = countries.map(eachCountry => {
-      
       return (
         <li className="card">
-          <Link to={`/CountryDetail/${eachCountry.cca3}`}> {eachCountry.name.common}</Link>
+          <Link to={`/CountryDetail/${eachCountry.cca3}`}>
+            {" "}
+            {eachCountry.name.common}
+          </Link>
         </li>
       );
     });
@@ -18,27 +21,21 @@ function App() {
   }
 
   return (
-
-
-    <div className="App">      
+    <div className="App">
       <div>
-        <nav className="navBar">
-          Wiki Countries
-        </nav>      
+        <nav className="navBar">Wiki Countries</nav>
         <div className="flexed">
           <div className="col-6">
             <ul>{iterate()}</ul>
           </div>
-          <div className="col-6"> 
-          <Switch>
-          <Route path="/CountryDetail/:id" component={CountryDetail}/>
-          </Switch>
-          </div>
+          <div className="col-6">
+            <Switch>
+              <Route path="/CountryDetail/:id" component={CountryDetail} />
+            </Switch>
           </div>
         </div>
       </div>
-    
-    
+    </div>
   );
 }
 
--- starter-code/src/CountryDetail.js
+++ starter-code/src/CountryDetail.js
@@ -3,23 +3,60 @@ import { Link, Switch, Route } from "react-router-dom";
 import countries from "./countries"
 
 export default class CountryDetail extends Component {
+  
   render() {
 
-    let id = this.props.match.params.id
+  let id = this.props.match.params.id
 
-   let countryId = countries.find(theCountry =>{
+  let countryId = countries.find(theCountry =>{
       return theCountry.cca3 === id
     })
+    console.log(countryId.name.common)
+    console.log(countryId.borders)
 
-   console.log(id)
-    return (
+
+
+
+    // countryId.borders will be called borders below
+    let borderMap = (borders) =>{
+      console.log(borders)
+      let borderCounts = [];
+      if(borders.length === 0){
+        return <div> No border countries </div> 
+      }
+       else{borders.forEach(border=>{
+          countries.find(count=>{
+            if(count.cca3 == border){
+              borderCounts.push(count)
+            }
+
+          })
+        })
+        let a = borderCounts.map(eachOne=>{
+          console.log(eachOne.name.common)
+          return <li> {eachOne.name.common}</li>
+        })
+        return a
+       }
+    }
+
+    
+
+
+
+   
+    
+
+
+
+    return (      
       <div>
-
-        <h1> {countryId.name.official}</h1>
+        <h1> {countryId.name.common}</h1>
+        <h2> Capital: {countryId.capital} </h2 >
+        <h2> Area: {countryId.area}</h2>
+        <h2> Borders: <ul>{borderMap(countryId.borders)} </ul></h2>     
         
-        
-      
       </div>
     );
+    }
   }
-}
