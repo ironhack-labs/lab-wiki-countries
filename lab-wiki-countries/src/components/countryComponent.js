282012495340f677f51c5ac565f8ae59180e541e
AlinaZakirod <gabie1989@gmail.com>
2019-10-06 19:00:43 -0400

    countries done

--- lab-wiki-countries/src/components/countryComponent.js
+++ lab-wiki-countries/src/components/countryComponent.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CountryComponent(props) {
-  console.log(props);
+  const theCode = props.match.params.code;
+  const theActualCountry = props.listOfCountries.find(eachCountry => {
+    return eachCountry.cca3 === theCode;
+  });
 
-  return <div>This is country details</div>;
+  const showBorders = () => {
+    return theActualCountry.borders.map((eachBorder, i) => {
+      const borderCountry = props.listOfCountries.find(eachCountry => {
+        return eachCountry.cca3 === eachBorder;
+      });
+      return (
+        <div key={i}>
+          <Link to={"/" + eachBorder}>{borderCountry.name.common}</Link>
+        </div>
+      );
+    });
+  };
+
+  console.log(theActualCountry);
+  return (
+    <div>
+      <h2>{theActualCountry.name.common}</h2>
+      <p>
+        Capital: <b>{theActualCountry.capital}</b>
+      </p>
+      <p>
+        Area:{" "}
+        <b>
+          {theActualCountry.area} km<sup>2</sup>
+        </b>
+      </p>
+      {showBorders()}
+    </div>
+  );
 }
 
 export default CountryComponent;
