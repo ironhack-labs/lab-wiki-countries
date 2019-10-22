6c3a75df55bd3283a3ec846ec444a1d84c0a3bb9
marcellemcm <marcellemoreira@gmail.com>
2019-10-22 10:04:49 -0300

    done2

--- starter-code/src/CountryDetail.js
+++ starter-code/src/CountryDetail.js
@@ -3,48 +3,48 @@ import countries from './countries.json';
 import { Link } from "react-router-dom";
 
 const CountryDetail = (props) => {
-  const { params } = props.match;
+  const { id } = props.match.params;
 
   const getCountry = (id) => {
     return countries.find(country => country.cca3 === id)
   }
 
-  const countryOption = getCountry(params.id)
+  const countryOption = getCountry(id)
 
   return (
     <Fragment>
-    <h1 className="text-left">{countryOption.name.common}</h1>
-     <table className="table">
-        <thead></thead>
-        <tbody>
-            <tr>
-              <td style={{width: "30%"}}>Capital</td>
-              <td>{countryOption.capital}</td>
-            </tr>
-            <tr>
-              <td>Area</td>
-              <td>
-                {countryOption.area} km
-                <sup>2</sup>
-              </td>
-            </tr>
-            <tr>
-              <td>Borders</td>
-              <td>
-                <ul>
-                    {
+      <h1 className="text-left">{countryOption.name.common}</h1>
+      <table className="table">
+          <thead></thead>
+          <tbody>
+              <tr>
+                <td style={{width: "30%"}}>Capital</td>
+                <td>{countryOption.capital}</td>
+              </tr>
+              <tr>
+                <td>Area</td>
+                <td>
+                  {countryOption.area} km
+                  <sup>2</sup>
+                </td>
+              </tr>
+              <tr>
+                <td>Borders</td>
+                <td>
+                  <ul>
+                      {
                       countryOption.borders.map((element,idx) => (
-                    <li key={idx}>
-                        <Link to={`/${element}`}>{getCountry(element).name.common}</Link>
-                  </li>
+                        <li key={idx}>
+                            <Link to={`/${element}`}>{getCountry(element).name.common}</Link>
+                        </li>
                       ))
-                    }
-                </ul>
-              </td>
-            </tr>
-          </tbody>
-    </table>
-  </Fragment>
+                      }
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+        </table>
+    </Fragment>
   )
 };
 
