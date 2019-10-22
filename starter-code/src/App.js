c3fc354101c9c7a053259033b832b1e0304e5a0c
marcellemcm <marcellemoreira@gmail.com>
2019-10-22 08:47:53 -0300

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import countries from './countries.json';
 import './App.css';
+import { Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import Countries from './Countries';
+import CountryDetail from './CountryDetail';
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      countries: countries,
+    };
+
+  }
+
+  render() {
+    const { countries } = this.state;
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <Link className="navbar-brand" to="/"> WikiCountries</Link>
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className="row">
+          <Countries countries={countries}/>
+          <div className="col-7">
+            <Switch>
+              <Route exact path="/:id" component={CountryDetail} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    
     </div>
   );
+}
 }
 
 export default App;
---
+++ starter-code/src/CountryDetail.js
@@ -0,0 +1,51 @@
+import React, { Fragment } from "react";
+import countries from './countries.json';
+import { Link } from "react-router-dom";
+
+const CountryDetail = (props) => {
+  const { params } = props.match;
+
+  const getCountry = (id) => {
+    return countries.find(country => country.cca3 === id)
+  }
+
+  const countryOption = getCountry(params.id)
+
+  return (
+    <Fragment>
+    <h1 className="text-left">{countryOption.name.common}</h1>
+     <table className="table">
+        <thead></thead>
+        <tbody>
+            <tr>
+              <td style={{width: "30%"}}>Capital</td>
+              <td>{countryOption.capital}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>
+                {countryOption.area} km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                <ul>
+                    {
+                      countryOption.borders.map((element,idx) => (
+                    <li key={idx}>
+                        <Link to={`/${element}`}>{getCountry(element).name.common}</Link>
+                  </li>
+                      ))
+                    }
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+    </table>
+  </Fragment>
+  )
+};
+
+export default CountryDetail;
