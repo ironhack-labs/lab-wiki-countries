b20681045f82f1e1887dba162408a9d52b2dc50e
fairouz <fairouzkoehlerwest@gmail.com>
2019-11-05 19:03:55 +0100

    completes bonus without styling

--- starter-code/src/CountryDetail.js
+++ starter-code/src/CountryDetail.js
@@ -7,29 +7,31 @@ let getCountryByccas3 = cca3 => {
 };
 
 const CountryDetail = props => {
-  console.log(props);
   const country = getCountryByccas3(props.match.params.cca3);
 
   return (
     <div>
       <h3>{country.name.official}</h3>
-
       <hr />
-
       <h4>Capital:{country.capital}</h4>
       <hr />
-
-      <h5>Area: {country.area}</h5>
+      <h5>Area: {country.area}km square</h5>
       <hr />
-
       <h5>
+        {" "}
         Borders:{" "}
-        {country.borders
-          .map(cca3 => getCountryByccas3(cca3))
-          .map(c => c.name.official)}
+        <div>
+          <ol>
+            {country.borders
+              .map(cca3 => getCountryByccas3(cca3))
+              .map(c => (
+                <li>
+                  <Link to={`/${c.cca3}`}> {c.name.official}</Link>
+                </li>
+              ))}
+          </ol>
+        </div>
       </h5>
-
-      {/* <Link to="/projects">Back</Link> */}
     </div>
   );
 };
--- starter-code/src/Navbar.js
+++ starter-code/src/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-// import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
