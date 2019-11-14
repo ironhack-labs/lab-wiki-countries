488503a674f60509ba2a87d8d92462c3d21cf0cc
CodesGM <54691076+CodesGM@users.noreply.github.com>
2019-11-14 14:48:55 -0500

    fixed border mapping thru json file

--- starter-code/src/comps/CountryCard.jsx
+++ starter-code/src/comps/CountryCard.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
+import Countries from "../countries.json";
+
 export default function CountryCard(props) {
+  //   console.log(props.theCountry.borders);
+
+  //   let final = props.theCountry.borders.map(b => {
+  //     return Countries.find(co => co.cca3 === b).name.common;
+  //   });
+  //   console.log(final);
+
   return (
     <React.Fragment>
       <div>
         <h1>{props.theCountry.name.common}</h1>
+        {props.theCountry.name.official}
       </div>
       <div>
         <b>Capital:</b> {props.theCountry.capital}
@@ -15,7 +25,12 @@ export default function CountryCard(props) {
       </div>
       <br />
       <div>
-        <b>Borders:</b> {props.theCountry.borders}
+        <b>Borders:</b>
+        <ul>
+          {props.theCountry.borders.map(b => (
+            <li>{Countries.find(co => co.cca3 === b).name.common}</li>
+          ))}
+        </ul>
       </div>
     </React.Fragment>
   );
