54da33d854d946a0bdb66ec364a41ed4a781a894
jnguyen3436 <jnguyen3436@gmail.com>
2019-11-14 10:31:55 -0500

    started

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="row">
+  <div className="col-5">Column 5/12</div>
+  <div className="col-7">Column 7/12</div>
+</div>
+
+
+<div className="list-group">
+  <a href="#" className="list-group-item list-group-item-action active">Cras justo odio (active)</a>
+  <a href="#" className="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+  <a href="#" className="list-group-item list-group-item-action">Morbi leo risus</a>
+  <a href="#" className="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+  <a href="#" className="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
+</div>
+     
     </div>
   );
 }
