3da05d4045bb2d0694795dfb738ffe63431fc323
Michael Garnreiter <mgarnreiter@aol.com>
2019-11-28 18:45:04 +0100

    cleanup

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import ListGroup from "./components/ListGroup";
 
+const NotFound = () => {
+  return <div>404 not found</div>;
+};
+
 function App() {
   return (
     <div>
