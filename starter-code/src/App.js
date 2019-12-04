2b2d540e813d89150d10cb335834623f26bae3e2
Javier Agudo <javieragudo3d@gmail.com>
2019-12-04 20:25:10 +0100

    Patriota

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
+import CountryDetail from "./CountryDetail/CountryDetail";
+import Countries from "./countries.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.countries = [...Countries];
+  }
+  searchName(chosenCountry){
+    let countryPayload = [...this.countries];
+    countryPayload = countryPayload.filter(country => country.cca3 === chosenCountry)[0]
+
+    return countryPayload.name.common
+
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark bg-primary mb-3">
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              WikiCountries
+            </a>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col-5" style={{maxHeight: "90vh", overflow: "scroll"}}>
+              <div className="list-group">
+                {this.countries.map((country, idx) => {
+                  return (
+                    <Link
+                      key={idx}
+                      to={"/countryDetail/" + country.cca3}
+                      className="list-group-item list-group-item-action"
+                    >
+                      {country.flag}
+                      {country.name.common}
+                    </Link>
+                  );
+                })}
+              </div>
+            </div>
+            <Switch>
+              <Route exact path="/countryDetail/:chosenCountry" render={(props)=>{
+                let chosenCountry = props.match.params.chosenCountry;
+                let countryPayload = [...this.countries];
+                countryPayload = countryPayload.filter(country => country.cca3 === chosenCountry)[0]
+                return(
+                  <CountryDetail country={countryPayload} searchName={(cn) => this.searchName(cn)}></CountryDetail>
+                )
+              }} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ starter-code/src/CountryDetail/CountryDetail.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export default class CountryDetail extends Component {
+  render() {
+    return (
+      <div class="col-7">
+        <h1>{this.props.country.name.common}</h1>
+        <table class="table">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td style={{width: "30%"}}>Capital</td>
+              <td>{this.props.country.capital}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>
+              {this.props.country.area} km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                <ul>
+                    {this.props.country.borders.map((border,idx) => (<li key={idx}> <a href={border}>{this.props.searchName(border)}</a></li>
+                    ))}
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
