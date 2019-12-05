4c711e118c29cc6fbf6fcc5c5c6c920cc000db91
Imanol Bernardino Alonso <imanolbernardino92@gmail.com>
2019-12-05 09:31:59 +0100

    . . .

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom'
+import countries from './countries.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="root">
+      <div>
+        <nav class="navbar navbar-dark bg-primary mb-3">
+          <div class="container">
+            <a class="navbar-brand" href="/">WikiCountries</a>
+          </div>
+        </nav>
+        <div class="container">
+          <div class="row">
+            <div class="col-5" >
+              <div class="list-group">
+               
+                {countries.map((elm, idx) => (
+                  <Link
+                    className="list-group-item list-group-item-action"
+                    to={elm.cca3}
+                  >
+                     {elm.name.official}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+     </div> 
   );
 }
 
---
+++ starter-code/src/components/Contactcard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 
+
+
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-5" style="max-height: 90vh; overflow: scroll;">
+            <div class="list-group">
+                
+                <a class="list-group-item list-group-item-action" href="/ARM">🇦🇲 Armenia</a>
+                
+            </div>
+     
+        </div>
+    </div>  
+    
+</div>    
