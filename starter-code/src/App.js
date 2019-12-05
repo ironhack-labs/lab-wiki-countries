24fc3e81d86f31bf2c423e1e567f1ccf4c4f037c
Eduardo <ceciliorubes@gmail.com>
2019-12-05 09:02:09 +0100

    countries

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import countries from "./countries.json";
+import CountryDetail from "./CountryDetail/CountryDetail";
+import { Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.clonedCountries = [...countries];
+
+    this.state = {
+      countries: this.clonedCountries
+    };
+  }
+
+  selectOneCountry(selected) {
+    let countryList = [...this.state.countries];
+    countryList = countryList.filter(element => {
+      return selected === element.cca3;
+    })[0];
+    return countryList.name.common;
+  }
+
+  render() {
+    console.log(countries);
+    return (
+      <div className="App">
+        <div id="root">
+          <div>
+            <nav className="navbar navbar-dark bg-primary mb-3">
+              <div className="container">
+                <a className="navbar-brand" href="/">
+                  WikiCountries
+                </a>
+              </div>
+            </nav>
+            <div className="container">
+              <div className="row">
+                <div className="col-5">
+                  <div className="list-group">
+                    {this.state.countries.map((element, idx) => {
+                      return (
+                        <div className="container">
+                          <div className="row">
+                            <div className="col-5">
+                              <div className="list-group">
+                                <Link
+                                  className="list-group-item list-group-item-action"
+                                  to={element.cca3}
+                                >
+                                  {element.flag} {element.name.common}
+                                </Link>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                <Switch>
+                  <Route
+                    path="/:country"
+                    render={props => {
+                      console.log(props.match.params.country)
+                      let selectedCountry = props.match.params.country;
+                      let countries = [...this.state.countries];
+                      countries = countries.filter(element =>
+                        selectedCountry === element.cca3
+                      )
+                      console.log(countries);
+                      return (
+                        <CountryDetail
+                          country={countries[0]}
+                          borders={selected=> this.selectOneCountry(selected)}
+                        ></CountryDetail>
+                      );
+                    }}
+                  />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ starter-code/src/CountryDetail/CountryDetail.js
@@ -0,0 +1,40 @@
+ 
+import React, { Component } from "react";
+
+export default class CountryDetail extends Component {
+  render() {
+    return (
+      <div className="col-7">
+        <h1>{this.props.country.name.common}</h1>
+        <table className="table">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td>Capital</td>
+              <td>{this.props.country.capital}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>
+                {this.props.country.area} km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                <ul>
+                  {this.props.country.borders.map((element, idx) => (
+                    <li key={idx}>
+                      <a href={element}>{this.props.borders(element)}</a>
+                    </li>
+                  ))}
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
