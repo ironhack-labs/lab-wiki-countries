548dcedb314a65f2de34b3487c97c474a50d70b6
merrymustard <merryymustard@gmail.com>
2019-12-05 09:14:08 -0600

    aiura

--- starter-code/src/components/Details.js
+++ starter-code/src/components/Details.js
@@ -1,41 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import countries from '../countries.json'
-import styled from "styled-components"
+import Styled from "styled-components"
 
-const CountryDetails = styled.div`
-color: "blue"
+const CountryDetails = Styled.div`
+backgroundColor: "blue"
 `
-	
+
 export default class Detail extends Component {
 	state = {
 		country: {}
 	}
 
-componentDidUpdate(prevProps) {
-	if(this.props.location !== prevProps.location){
-		this.onRouteChanged()
+	componentDidUpdate(prevProps) {
+		if (this.props.location !== prevProps.location) {
+			this.onRouteChanged()
+		}
 	}
-}
-onRouteChanged = () => {
-	const country = countries.find(
-		country => country.cca3 === this.props.match.params.cca3
-	)
-	this.setState({ country })
-}
+	onRouteChanged = () => {
+		const country = countries.find(
+			country => country.cca3 === this.props.match.params.cca3
+		)
+		this.setState({ country })
+	}
 
 
-	render(){
-		const {country} = this.state
-		return(
+	render() {
+		const { country } = this.state
+		return (
 			<CountryDetails>
 
 				<p>Capital: {country.capital}</p>
 				<p>Area: {country.area}</p>
+				<p> {country.flag}{country.name.common}</p>
 
 				<p>Border: {country.border}</p>
 
 			</CountryDetails>
-		) 
+		)
 	}
 }
 
