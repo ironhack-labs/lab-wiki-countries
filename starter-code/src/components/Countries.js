6e556f3694e7325b4e8bbdd937556b571ab9808a
roberto araya gil <naggas@Naggas.local>
2020-01-16 11:25:18 +0100

    config ok

---
+++ starter-code/src/components/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return ( 
+        <div className="col-5">
+            
+        </div>
+     );
+}
+ 
+export default Countries;
---
+++ starter-code/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nabvar = () => {
+    return ( 
+        <nav className="navbar navbar-dark bg-primary mb-3">
+            <a className="navbar-brand" href="/">WikiCountries</a>
+        </nav>
+     );
+}
+ 
+export default Nabvar;
