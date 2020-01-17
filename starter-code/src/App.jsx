668878d7dc66fa54beb00d16ad3227099b446eed
Monika <mswidzinska87@gmail.com>
2020-01-17 11:42:42 +0100

    axios version

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './components/Navbar'
-import CountryDetail from './components/CountryDetail'
-import ShowCountry from './components/ShowCountry'
-import countries from './countries.json'
+import Navbar from './components/Navbar';
+import CountryDetail from './components/CountryDetail';
+import ShowCountry from './components/ShowCountry';
+// import countries from './countries.json';
+import axios from "axios";
 import {Switch, Route} from 'react-router-dom';
 
 
@@ -11,10 +12,15 @@ export default class App extends Component {
 constructor (props) {
   super(props)
   this.state = {
-    allCountries:countries,
-    Borders: [countries.borders]
-  }
+    countries: []
+    }
 }
+  componentDidMount() {
+    axios.get("https://countries.tech-savvy.tech/countries")
+    .then(response =>{
+      this.setState({countries:response.data})
+    })
+  }
 render() {
   return (
     <div className = "App">
@@ -24,7 +30,7 @@ render() {
         <div className="row ">
           <div className="col-5 countries scrollbar scrollbar-info">
             <div className="list-group force-overflow">
-            {this.state.allCountries.map(country =>(
+            {this.state.countries.map(country =>(
             <CountryDetail
               key={country.name.common}
               code={country.cca3}
