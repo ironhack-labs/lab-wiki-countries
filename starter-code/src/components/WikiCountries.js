c603a9eceb52802d8b7438d2f7647cf5d1925a90
Marciano Vilela Rodrigues <mvr@br.ibm.com>
2020-01-21 21:45:16 -0300

    backend

--- starter-code/src/components/WikiCountries.js
+++ starter-code/src/components/WikiCountries.js
@@ -2,18 +2,36 @@ import React, { Component } from 'react';
 import CountriesList from './CountriesList';
 import CountryDetail from './CountryDetail';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Countries from '../countries.json';
-
+// import Countries from '../countries.json';
+import axios from 'axios';
 
 class WikiCountries extends Component{
+
+    constructor(){
+        super()
+        this.state = {
+            Countries: []
+        }
+    }
+
+    componentDidMount() {
+        console.log('>>>>>>>>componentDidMount')
+        axios.get("http://localhost:4000/countries", {headers: {'Access-Control-Allow-Credentials' : true}})
+        .then(response => {
+            console.log('>>>>>>>>>>>>>>RESPONSE', response);
+            this.setState({Countries: response.data})
+        }).catch((err)=>{
+            console.log('>>>>>>>>>>>>', err)
+        })
+    }
     
     render(){
 
         const findDetails = props => {
 
-            console.log(Countries.find(c => c.cca3 === props.match.params.id))
+            console.log(this.state.Countries.find(c => c.cca3 === props.match.params.id))
           
-            return(Countries.find(c => c.cca3 === props.match.params.id))
+            return(this.state.Countries.find(c => c.cca3 === props.match.params.id))
           
           }
 
@@ -33,7 +51,7 @@ class WikiCountries extends Component{
                     <div class="container">
 
                         <div class="row">                            
-                            <CountriesList countries={Countries}/>
+                            <CountriesList countries={this.state.Countries}/>
                             <Switch>
                                 <Route exact path='/:id' component={(props) => <CountryDetail country={findDetails(props)} />} />  
                             </Switch>
