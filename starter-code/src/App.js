7c36121a00310aca1ec94fe11423a4daff544bc3
Sergio Iturzaeta <gios@MacBook-Pro-de-Sergio.local>
2020-02-06 17:44:07 +0100

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Countries from './data/countries.json'
+import CountryList from './components/CountryList';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import CountryDetails from './components/CountryDetails';
+
+const findDetails = props => {
+  console.log(Countries.find(c => c.cca3 === props.match.params.id))
+  return(Countries.find(c => c.cca3 === props.match.params.id))
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="countries">
+        {Countries.map((country, i) => (
+            <CountryList name={country.name.official} id={country.cca3} flag={country.flag} key={i} />
+        ))}
+      </div>
+
+      <div className="countries-detail">
+        <Switch>
+          <Route exact path='/:id' component={(props) => <CountryDetails country={findDetails(props)} />} />
+          {/* IN CASE IT THROWS ERROR */}
+          <Redirect to="/" />
+
+        </Switch>
+
+      </div>
     </div>
   );
 }
