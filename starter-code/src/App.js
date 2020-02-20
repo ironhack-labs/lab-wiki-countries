104b658fcb4ffb95d9fff220e6f1f2c2d13dd46f
israSH <isra-03p@hotmail.com>
2020-02-20 00:15:42 -0600

    muestra el listado

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom'
+import countries from './countries.json'
+import CountryDetail from './CountryDetail'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+      <div >
+        <h1 className="h1">WikiCountries</h1>
+        <div>
+            <div>
+            {countries.map((country, index) => 
+              <div key={index}>
+                <Link  to={`/${country.cca3}`}>{country.flag} {country.name.common}</Link>
+              </div>
+            )}
+        </div>
+            <div>
+            <Route exact path="/:id" component={CountryDetail} />
+            </div>  
+          </div>
+          
+        </div>
+      
+    );
+  }
 }
 
 export default App;
---
+++ starter-code/src/CountryDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import countries from './countries.json'
+
+
+const CountryDetail = (props) => {
+  const country = (cca3) => {}
+
+  const { params } = props.match
+ 
+ }
+
+export default CountryDetail;
