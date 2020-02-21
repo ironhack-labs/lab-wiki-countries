eca6d1a012ab3fc8153ba8cca0eed8aa91ef7d87
abrebion <abrebion@gmail.com>
2020-02-21 02:10:57 +0100

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import countries from "./countries.json";
+import CountryDetails from "./CountryDetails";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark bg-primary mb-3">
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              WikiCountries
+            </a>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col-5" style={{ maxHeight: "90vh", overflow: "scroll" }}>
+              <div className="list-group">
+                {countries.map((el, i) => (
+                  <Link to={el.cca3} className="list-group-item list-group-item-action" key={i}>
+                    {el.flag} {el.name.common}
+                  </Link>
+                ))}
+              </div>
+            </div>
+            <div className="col-7">
+              <Switch>
+                <Route exact path="/:code" render={routeProps => <CountryDetails {...routeProps} />}></Route>
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ starter-code/src/CountryDetails.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import countries from "./countries.json";
+
+export default function CountryDetails(props) {
+  const {
+    params: { code }
+  } = props.match;
+
+  const getCountryObject = countryCode => {
+    return countries.find(e => countryCode === e.cca3);
+  };
+
+  const getCountryName = countryCode => {
+    return getCountryObject(countryCode).name.common;
+  };
+
+  const {
+    name: { common },
+    capital,
+    area,
+    borders
+  } = getCountryObject(code);
+
+  return (
+    <div>
+      <h1>{common}</h1>
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style={{ width: "30%" }}>Capital</td>
+            <td>{capital}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {area} km
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                {borders.map((el, i) => (
+                  <li key={i}>
+                    <Link to={el}>{getCountryName(el)}</Link>
+                  </li>
+                ))}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
