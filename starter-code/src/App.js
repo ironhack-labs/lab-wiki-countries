cfb32fa09115e44a11c629134f9e866ceff7f5a8
armetegithub <armomomete@protonmail.com>
2020-02-27 01:36:24 +0100

    Aparecen los paises

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import countries from "./countries";
+import CountryDetail from "./CountryDetail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="root">
+        <div>
+          <nav class="navbar navbar-dark bg-primary mb-3">
+            <div class="container">
+              <a class="navbar-brand" href="/">
+                WikiCountries
+              </a>
+            </div>
+          </nav>
+          <div class="container">
+            <div class="row">
+              <div class="col-5" styles="max-height: 90vh; overflow: scroll;">
+                <div class="list-group">
+                  <a class="list-group-item list-group-item-action" href="/ABW">
+                    🇦🇼 Aruba
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/AFG">
+                    🇦🇫 Afghanistan
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/AGO">
+                    🇦🇴 Angola
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/AIA">
+                    🇦🇮 Anguilla
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/ALA">
+                    🇦🇽 Åland Islands
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/ALB">
+                    🇦🇱 Albania
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/AND">
+                    🇦🇩 Andorra
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/ARE">
+                    🇦🇪 United Arab Emirates
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/ARG">
+                    🇦🇷 Argentina
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/ARM">
+                    🇦🇲 Armenia
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/ASM">
+                    🇦🇸 American Samoa
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/ATA">
+                    🇦🇶 Antarctica
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/FLK">
+                    🇫🇰 Falkland Islands
+                  </a>
+                  <a
+                    class="list-group-item list-group-item-action active"
+                    href="/FRA"
+                  >
+                    🇫🇷 France
+                  </a>
+                  <a class="list-group-item list-group-item-action" href="/ZWE">
+                    🇿🇼 Zimbabwe
+                  </a>
+                </div>
+              </div>
+              <div class="col-7">
+                <h1>France</h1>
+                <table class="table">
+                  <thead></thead>
+                  <tbody>
+                    <tr>
+                      <td styles="width: 30%;">Capital</td>
+                      <td>Paris</td>
+                    </tr>
+                    <tr>
+                      <td>Area</td>
+                      <td>
+                        551695 km
+                        <sup>2</sup>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Borders</td>
+                      <td>
+                        <ul>
+                          <li>
+                            <a href="/AND">Andorra</a>
+                          </li>
+                          <li>
+                            <a href="/BEL">Belgium</a>
+                          </li>
+                          <li>
+                            <a href="/DEU">Germany</a>
+                          </li>
+                          <li>
+                            <a href="/ITA">Italy</a>
+                          </li>
+                          <li>
+                            <a href="/LUX">Luxembourg</a>
+                          </li>
+                          <li>
+                            <a href="/MCO">Monaco</a>
+                          </li>
+                          <li>
+                            <a href="/ESP">Spain</a>
+                          </li>
+                          <li>
+                            <a href="/CHE">Switzerland</a>
+                          </li>
+                        </ul>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ starter-code/src/CountryDetail.js
@@ -0,0 +1,3 @@
+import React from "react";
+import countries from "./countries";
+import { Link } from "react-router-dom";
