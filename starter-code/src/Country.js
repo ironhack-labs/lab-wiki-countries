08df57a530b0e51cb01d06711de651c591438fb6
CrisSugar <aguacero18@hotmail.com>
2020-02-27 09:24:44 +0100

    done

---
+++ starter-code/src/Country.js
@@ -0,0 +1,30 @@
+// import React from 'react';
+
+
+// export default class IronContact extends React.Component {
+//   state = {
+//     name: 'name.official',
+//     capital: String,
+//     area: Number,
+//     borders: []
+//   };
+//   render() {
+//     return (
+//         <div className="">
+//       <h2>Work Experience:</h2>
+//       {this.state.borders.map((countries) => {
+//         return (
+//           <div className="experience-content" key={index}>
+//             <h3>{eachPlace.name}</h3>
+//             <p>{eachPlace.city}, {eachPlace.country}</p>
+//             <h4>{eachPlace.jobTitle}</h4>
+//             <p>{eachPlace.jobDescription}</p>
+//             <p>{eachPlace.jobStarted} - {checkJobEnded(eachPlace.jobEnded)}</p>
+//           </div>
+//           )
+//       })}
+//     </div>
+//       );
+//     }
+//   }
+
