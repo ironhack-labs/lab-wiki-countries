9f9ad226feb38ee2c45a3eb9c1b3bddc3c2d6a4d
Laura <lauratp02@gmail.com>
2020-02-27 09:05:29 +0100

    No Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import CountryDetail from "./CountryDetail";
+import countries from "./countries.json";
+import { Switch, Route} from "react-router-dom";
+import {Link} from "react-router-dom";
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+  <div id="root"></div>
+     
+
+   
+
+   <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">WikiCountries</a>
+        </div>
+      </nav>
+
+      <div className="row">
+    <Switch>
+      <Route exact path="/country" render={()=> <CountryDetail/>}/>
+    </Switch>
+  <div class="col-5"></div>
+  <div class="col-7"></div>
+
+
+<div class="list-group">
+   <Link to ="./country"class="list-group-item list-group-item-action">{CountryDetail.cca3}{CountryDetail.name.common}</Link>
+  <a href="#" class="list-group-item list-group-item-action">Hola soy un país😬</a>
+  <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+  <a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
+</div>
+ 
     </div>
+    </div>
+   
+    
   );
+
 }
 
 export default App;
---
+++ starter-code/src/CountryDetail.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {link} from "react-router-dom";
+
+
+export default class CountryDetail extends React.Component{
+
+    //const country = [...countries]
+
+    render(){
+        return(
+            <tr>
+                
+                {/* {countries.map(eachCountry,index) => {
+                    return(
+                         <td key={eachCountry.cca3} ></td> 
+                    )
+                }} */}
+            </tr>
+        )
+    }
+
+
+}
