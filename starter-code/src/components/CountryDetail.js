2e9cc1f4be9ff409d1187541a477b9b7ebe8f283
Tonalli <tonalli@xanum.uam.mx>
2020-03-02 17:49:56 -0600

    Update CountryDetail component

--- starter-code/src/components/CountryDetail.js
+++ starter-code/src/components/CountryDetail.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const CountryDetail = (props) =>  {
-
-    return(
-        <div>
-        <h1>{props.name}</h1>
-        <p> Capital  {props.capital}</p>
-        <p> Area {props.area}</p>
-        <p> Borders </p>
-        </div>
-    );
+  const { name,  capital, area, borders } = props;
+  return(
+    <Fragment>
+      <h1>{name}</h1>
+      <table className="table">
+      <thead></thead>
+        <tbody>
+            <tr>
+              <td style={{"width": "30%"}}>Capital</td>
+              <td>{capital}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>{area} km <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                  <ul>
+                  {borders.map((item, index) => {
+                    return <li key={index}><a href={Object.keys(item)[0]}>{Object.values(item)[0]}</a></li>
+                  })}
+                  </ul>
+              </td>
+            </tr>
+        </tbody>
+      </table>
+    </Fragment>
+  );
 }
-
 
 
 export default CountryDetail;
