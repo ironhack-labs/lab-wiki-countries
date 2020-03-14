d109099b0cc5ca64816f7808679a6a0b19254320
carballo <carballet@gmail.com>
2020-03-14 19:58:14 +0100

    Iteration 1 Done & Iteration 2 Done except cool styles

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,12 +1,42 @@
 import React from "react";
+import { Link, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import { CountryDetail } from "./component/CountryDetail";
+//import CountryDetail from "./component/CountryDetail";
+import Countries from "./countries.json";
+import CountryDetail from "./component/CountryDetail";
 
 function App() {
   return (
     <div className="App">
-      <CountryDetail />
+      <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            WikiCountries
+          </a>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="row">
+          <div className="col-5" style={{ height: "90vh", overflow: "scroll" }}>
+            <div className="list-group">
+              {Countries.map(country => {
+                return (
+                  <Link
+                    to={country.cca3}
+                    className="list-group-item list-group-item-action"
+                  >
+                    {country.flag} {country.name.common}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+          <div className="col-7">
+            <Route exact path="/:countryCode" component={CountryDetail} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- starter-code/src/component/CountryDetail.js
+++ starter-code/src/component/CountryDetail.js
@@ -1,16 +1,63 @@
 import React from "react";
 import Countries from "../countries.json";
+import { Link, Route } from "react-router-dom";
 
-export const CountryDetail = () => {
-  const countryList = Countries.map(e => e.name);
-  console.log(oneCountry);
+const CountryDetail = props => {
+  // Recibo los parámetros que hay en la URL desde App.js vía el <ROUTE>
+  const { countryCode } = props.match.params;
+  console.log({ countryCode });
 
+  // Funcion para encontrar el CCA3 según el CountryCode que recibimos
+  const handeldCountry = countryCCA => {
+    const result = Countries.find(country => country.cca3 === countryCCA);
+    return result;
+  };
+
+  // LLamamos a la función que hay justo encima.
+  const countryToDisplay = handeldCountry(countryCode);
+
+  // Return y nos podemos meter en CountryToDisplah porque hemos guardado todos los datos ahí
   return (
-    <div>
-      <ul>
-      { oneCountry = countryList.map(e => e.common);       
-        <li>{oneCountry}</li>
-      </ul>}
+    <div className="col-7">
+      <h1>{countryToDisplay.name.common}</h1>
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td className="capital">Capital</td>
+            <td>{countryToDisplay.capital}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {countryToDisplay.area} km
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                {countryToDisplay.borders.map(countryId => {
+                  return (
+                    <li>
+                      <Link to={countryId}>
+                        {Countries.map(country => {
+                          if (country.cca3 === countryId) {
+                            return <p> {country.name.common} </p>;
+                          }
+                        })}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
+
+export default CountryDetail;
