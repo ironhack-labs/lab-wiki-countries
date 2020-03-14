726351bd78f1246440d4cac86996c25071442333
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-03-14 19:40:48 +0100

    Give basic structure to the page

--- starter-code/src/components/CountryDetail.js
+++ starter-code/src/components/CountryDetail.js
@@ -1,29 +1,56 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import { Route } from "react-router-dom";
 
 export const CountryDetail = () => {
   return (
     <>
       <div className="row">
         <div className="col-5">Column 5/12</div>
-        <div className="col-7">Column 7/12</div>
-      </div>
-      <div className="list-group">
-        <a href="#" className="list-group-item list-group-item-action active">
+        <div className="list-group">
+          {/*map para los paises
+          <a href="#" className="list-group-item list-group-item-action active">
           Cras justo odio (active)
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          Dapibus ac facilisis in
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          Morbi leo risus
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          Porta ac consectetur ac
-        </a>
-        <a href="#" className="list-group-item list-group-item-action disabled">
-          Vestibulum at eros
-        </a>
+          </a>
+          */}
+        </div>
+      </div>
+      <div className="col-7">
+        Column 7/12
+        {/*
+           <h1>France</h1>
+           <table class="table">
+             <thead></thead>
+             <tbody>
+               <tr>
+                 <td style="width: 30%;">Capital</td>
+                 <td>Paris</td>
+               </tr>
+               <tr>
+                 <td>Area</td>
+                 <td>
+                   551695 km
+                   <sup>2</sup>
+                 </td>
+               </tr>
+               <tr>
+                 <td>Borders</td>
+                 <td>
+                   <ul>
+                     <li><a href="/AND">Andorra</a></li>
+                     <li><a href="/BEL">Belgium</a></li>
+                     <li><a href="/DEU">Germany</a></li>
+                     <li><a href="/ITA">Italy</a></li>
+                     <li><a href="/LUX">Luxembourg</a></li>
+                     <li><a href="/MCO">Monaco</a></li>
+                     <li><a href="/ESP">Spain</a></li>
+                     <li><a href="/CHE">Switzerland</a></li>
+                   </ul>
+                 </td>
+               </tr>
+             </tbody>
+           </table>
+           */}
       </div>
     </>
   );
--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./components/App.js";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
 document.addEventListener("DOMContentLoaded", () => {
