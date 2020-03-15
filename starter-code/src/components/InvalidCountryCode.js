0273d5f2f9d42600041b9fee3086a5b2f7f74785
Carlos Mateo <cmc2k5@gmail.com>
2020-03-15 20:59:55 +0100

    Adds InvalidCountryCode component

---
+++ starter-code/src/components/InvalidCountryCode.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const InvalidCountry = () => {
+    return <div className="alert alert-warning" role="alert">
+        Invalid country code. Please, try clicking on a country from the list.
+    </div>
+}
