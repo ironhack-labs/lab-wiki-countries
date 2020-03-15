6cc3d045d02c0144da4cab38da9bd03e60200e91
Fran <fmjerez@icloud.com>
2020-03-15 13:13:34 +0100

    Added Styles

--- starter-code/src/components/App.js
+++ starter-code/src/components/App.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import '../App.css';
-import { CountryDetail } from './CountryDetail';
-import { List } from './List';
-import Countries from '../countries.json';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import "../App.css";
+import { CountryDetail } from "./CountryDetail";
+import { List } from "./List";
+import Countries from "../countries.json";
+import { Route, Switch } from "react-router-dom";
 
 export const App = () => {
-	return (
-		<div>
-			<nav className="navbar navbar-dark bg-primary">
-				<span className="navbar-brand mb-0 h1">WikiCountries</span>
-			</nav>
-			<div className="row">
-				<List countries={Countries} />
-				<Switch>
-					<Route path="/" exact />
-					{Countries.map((country, i) => {
-						return (
-							<Route
-								key={i}
-								path={`/${country.cca3}`}
-								component={() => (
-									<div className="col-7">
-										<CountryDetail country={country} />
-									</div>
-								)}
-							/>
-						);
-					})}
-				</Switch>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <nav className="navbar">
+        <span className="navbar-brand mb-0">WikiCountries</span>
+      </nav>
+      <div className="container row">
+        <List countries={Countries} />
+        <Switch>
+          <Route path="/" exact />
+          {Countries.map((country, i) => {
+            return (
+              <Route
+                key={i}
+                path={`/${country.cca3}`}
+                component={() => (
+                  <div className="col-7">
+                    <CountryDetail country={country} />
+                  </div>
+                )}
+              />
+            );
+          })}
+        </Switch>
+      </div>
+    </div>
+  );
 };
