c1001b7f556f525f016b399c69c08d408b348201
Amalia Glez <glezgutierrez95@gmail.com>
2020-04-02 17:33:51 +0200

    first part working

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import { CountryDetail } from './components/CountryDetail';
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import { CountryDetail } from "./components/CountryDetail";
 import { Link } from "react-router-dom";
-import countries from './countries.json';
+import countries from "./countries.json";
+
 
 function App() {
 
@@ -11,12 +12,16 @@ function App() {
 
   return (
     <div className="App">
-      <div class="list-group">
-        {countriesList.map( e => <Link to={`/${e.cca3}`} className="list-group-item list-group-item-action">{e.name.common}</Link>)}
-      </div>
-      <Switch>
-        <Route path='/:cca3' component={CountryDetail} />
-      </Switch>
+      <header className="App-header">
+        <h1>Countries Wiki</h1>{" "}
+        <div className="list-group">
+        {countriesList.map( e => <Link to={`/${e.cca3}`} key={e.cca3} className="list-group-item list-group-item-action"> {e.name.common} </Link> )}
+        </div>
+        <CountryDetail></CountryDetail>
+        <Switch>
+          <Route path="/:cca3" component={CountryDetail} />
+        </Switch>
+      </header>
     </div>
   );
 }
