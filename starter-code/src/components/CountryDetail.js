2943a83fd8ca77f0755e61f860ef7a9b45acb89b
Pablolo <pablo_olondriz@hotmail.com>
2020-04-22 20:25:16 +0200

    setting up

--- starter-code/src/components/CountryDetail.js
+++ starter-code/src/components/CountryDetail.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 
-class App extends Component {
+class CountryDetail extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         
       </div>
     );
   }
 }
 
-export default App;
+export default CountryDetail;
