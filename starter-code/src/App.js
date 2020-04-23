71954c8c8181cdb99e3c339e25f29aab6999e6af
Pablolo <pablo_olondriz@hotmail.com>
2020-04-23 18:47:02 +0200

    solve issue!

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import CountryDetail from './components/CountryDetail';
-import Countries from './countries.json';
+import countries from './countries.json';
+import { Route, Link } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
+
+  showCountries = () => {
+    return countries.map((item, index) => {
+      return <Link key={index} className="list-group-item list-group-item-action" to={`${item.cca3}`}><img className='flag' src={`https://www.countryflags.io/${item.cca2}/flat/64.png`} alt="flag"/> {item.name.common}</Link>
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <CountryDetail countries={ Countries } />
+      <div id="root">
+      <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">WikiCountries</a>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="row">
+          <div className="col-5">
+            <div className="list-group">
+              {this.showCountries()}
+            </div>
+          </div>
+          <Route exact path='/:id' render={(props) => <CountryDetail {...props} countries={countries} />} /> 
+        </div>
       </div>
+    </div>
     );
   }
 }
--- starter-code/src/components/CountryDetail.js
+++ starter-code/src/components/CountryDetail.js
@@ -1,33 +1,13 @@
 import React, { Component } from 'react';
+// import countries from './countries.json';
 
 class CountryDetail extends Component {
 
-  showCountries = () => {
-    const { countries } = this.props;
-    // console.log(countries)
-    // const flag = `https://www.countryflags.io/${item.cca2}/flat/64.png`
-    return countries.map(item => {
-      return <a className="list-group-item list-group-item-action" href="/ABW"><img className='flag' src={`https://www.countryflags.io/${item.cca2}/flat/64.png`} alt="flag"/> {item.name.common}</a>
-    })
-  }
-
   render() {
+    console.log(this.props.countries);
     return (
-      <div id="root">
-      <nav className="navbar navbar-dark bg-primary mb-3">
-        <div className="container">
-          <a className="navbar-brand" href="/">WikiCountries</a>
-        </div>
-      </nav>
-      <div className="container">
-        <div className="row">
-          <div className="col-5">
-            <div className="list-group">
-              {this.showCountries()}
-            </div>
-          </div>
-          <div className="col-7">
-            <h1>Country</h1>
+      <div className="col-7">
+            <h1>Country Name</h1>
             <table className="table">
               <thead></thead>
               <tbody>
@@ -52,9 +32,6 @@ class CountryDetail extends Component {
               </tbody>
             </table>
           </div>
-        </div>
-      </div>
-    </div>
     );
   }
 }
