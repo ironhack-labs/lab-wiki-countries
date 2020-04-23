47d99a40aa7e8324fcfcf63410c270c215263ebe
Pablolo <pablo_olondriz@hotmail.com>
2020-04-23 12:20:18 +0200

    countries listed with their flags

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import CountryDetail from './components/CountryDetail';
+import Countries from './countries.json';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <CountryDetail countries={ Countries } />
       </div>
     );
   }
--- starter-code/src/components/CountryDetail.js
+++ starter-code/src/components/CountryDetail.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
 
 class CountryDetail extends Component {
+
+  showCountries = () => {
+    const { countries } = this.props;
+    // console.log(countries)
+    // const flag = `https://www.countryflags.io/${item.cca2}/flat/64.png`
+    return countries.map(item => {
+      return <a className="list-group-item list-group-item-action" href="/ABW"><img className='flag' src={`https://www.countryflags.io/${item.cca2}/flat/64.png`} alt="flag"/> {item.name.common}</a>
+    })
+  }
+
   render() {
     return (
-      <div>
-        
+      <div id="root">
+      <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">WikiCountries</a>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="row">
+          <div className="col-5">
+            <div className="list-group">
+              {this.showCountries()}
+            </div>
+          </div>
+          <div className="col-7">
+            <h1>Country</h1>
+            <table className="table">
+              <thead></thead>
+              <tbody>
+                <tr>
+                  <td>Capital</td>
+                  <td>Paris</td>
+                </tr>
+                <tr>
+                  <td>Area</td>
+                  <td>551695 km
+                    <sup>2</sup>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Borders</td>
+                  <td>
+                    <ul>
+                      {/* <li><a href="/AND">Andorra</a></li> */}
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
+    </div>
     );
   }
 }
