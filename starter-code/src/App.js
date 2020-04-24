0227ccd982acb63219e22ae9c4fa997fbf78f901
Angel Baro <angelbaro44@gmail.com>
2020-04-24 10:49:29 -0400

    test

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -30,14 +30,14 @@ class App extends Component {
         <div style={{width:'40%'}}>
 
         <nav style={{maxWidth:'606px', justifyContent:'center', fontSize:'20px', border:'outset', }} className="title-bar"  >
-          <Link style={{ color: 'white', maxWidth:'100%'}} to='/home'>Wiki Countrys'</Link>
+          <Link style={{ color: 'white', maxWidth:'100%'}} to='/home'>Wiki_Countries</Link>
           <br />
           <br />
         </nav>
         <div className="row" style={{placeContent: 'center', flex:'12'}}>
 
           <div className="window" style={{ maxHeight: '80vh', overflow: 'scroll' ,margin:'10px', flex:'7'}}>
-            <h1 style={{ color: 'white' }} className="title-bar" >Countries</h1>
+            <h1 style={{ color: 'white',fontSize:'20px' }} className="title-bar" >Countries</h1>
             <div className="window"><ul className="tree-view"><ul style={{ paddingBottom: '10px' }} >{this.displaycountrys()}</ul></ul></div>
           </div>
           <div className="window" style={{ flex:'5' ,margin:'10px'}} >
