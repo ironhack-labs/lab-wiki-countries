7c9b4c6c6a48da49edc7db2a1e53ea17f84d35d0
elucserr <elenalucasserrano@gmail.com>
2020-04-24 23:31:21 +0200

    set up and doing iteration1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import countries from './countries.json';
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listCountries: countries,
+    }
+  }
+
+  render() {
+    return (
+      <div className="Wikipedia">
+        <nav className="navbar">
+          <h2 className=""></h2>
+        </nav>
+        <div className="list">
+         <div className="list-each-country">
+          {this.state.listCountries.map((country, index) => <Link className="" to={country.cca3} key={index}> {country.flag} {country.name.official}</Link> )}
+         </div>
+        </div>
+      </div>
+
+    )
+  }
 }
 
 export default App;
--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
