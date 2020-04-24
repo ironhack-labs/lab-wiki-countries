1e63d2739c3646ecf10f638a9afcd934885fec86
Carles Galí Bou <carles_ga@hotmail.com>
2020-04-24 20:56:33 +0200

    Rendered all countries of .json with <Link /> method

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CountryDetails from './components/CountryDetail';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import countries from './countries.json';
+import { Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+
+  state = {
+      listCountries: countries,
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="root">
+            <div>
+              <nav class="navbar navbar-dark bg-primary mb-3">
+                <div class="container">
+                  <Link class="navbar-brand" to="/">WikiCountries</Link>
+                </div>
+              </nav>
+              <div class="container">
+                <div class="row">
+                  <div class="col-5" style={{ maxHeight: '90vh', overflow: 'scroll' }}>
+                    <div class="list-group">
+                      { this.state.listCountries.map((info, i) => 
+                        <Link to={info.cca3} key={i} className="list-group-item list-group-item-action">
+                          {info.flag} {info.name.common}
+                        </Link>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
