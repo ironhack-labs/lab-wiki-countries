df24e266da352414f240f487ceec7a14e29f4ccd
unknown <surge0116@gmail.com>
2020-04-23 20:58:40 -0400

    test

--- starter-code/src/CountryDetail.js
+++ starter-code/src/CountryDetail.js
@@ -1,78 +1,76 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class CountryDetail extends Component {
-    
-    
-    
-    
-    componentDidMount=()=>{ //window.onload happens once when the components first mounts}
-        // console.log(this.props.match.params.id)
-        //find method ... js find 
-        /**ENTER CODE HERE **/
+  componentDidMount = () => {
+    //window.onload happens once when the components first mounts}
+    // console.log(this.props.match.params.id)
+    //find method ... js find
+    /**ENTER CODE HERE **/
+  };
 
-    }
-    
-    // Child = ({ match }) => (
-        //   match.params.id
-        //    )
-        
-        // showDetails = () => {
-            
-            // }
+  // Child = ({ match }) => (
+  //   match.params.id
+  //    )
 
-    
-            
-    render() {
-        // console.log(this.Child)
-        // console.log(this.state.country.name.common)
-        let id = this.props.match.params.id
-        let country = this.props.countries.find((eachCountry)=>{
-            return (
-                eachCountry.cca3===id
-            )
-        })
-            
-        
-        
-            return(
-            <div>
-                Country Details <br/>
-                
-                <h1>{country.name.common}</h1>
-                <table class="table">
-                <thead></thead>
-                <tbody>
-                    <tr>
-                    <td style={{width: "30%;"}}>Capital</td>
-                    <td>{country.capital}</td>
-                    </tr>
-                    <tr>
-                    <td>Area</td>
-                    <td>{country.area} km
-                        <sup>2</sup>
-                    </td>
-                    </tr>
-                    <tr>
-                    <td>Borders</td>
-                    <td>
-                        <ul>
-                        {country.borders.map(eachBorder => {
-                            let country = this.props.country.find(c=>c.cca3===eachBorder)
-                            return (
-                                <li><Link to={"/country/"+country.cca3}>{country.name.common}</Link></li>
-                            )
-                         
-                        })}
-                        </ul>
-                    </td>
-                    </tr>
-                </tbody>
-                </table>
-            </div>
-            )
-    }
+  // showDetails = () => {
+
+  // }
+
+  render() {
+    // console.log(this.Child)
+    // console.log(this.state.country.name.common)
+    let id = this.props.match.params.id;
+    let country = this.props.countries.find((eachCountry) => {
+      return eachCountry.cca3 === id;
+    });
+
+    return (
+      <div>
+        Country Details <br />
+        <h1>{country.name.common}</h1>
+        <table class="table">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td style={{ width: "30%;" }}>Capital</td>
+              <td>{country.capital}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>
+                {country.area} km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                <ul>
+                  {country.borders.map((eachBorder) => {
+                    let country = this.props.country.find(
+                      (country) => country.cca3 === eachBorder
+                    );
+                    return (
+                      <li>
+                        <Link to={"/country/" + country.cca3}>
+                          {country.name.common}
+                        </Link>
+                      </li>
+                    );
+                    //{/* return (<li key={cca3}>
+                    //    <Link to={"/country/"+cca3}>
+                    //    {this.props.country.find(country=> country.flag === cca3).name.common}</Link>
+                    //  </li>) */}
+                  })}
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default CountryDetail;
