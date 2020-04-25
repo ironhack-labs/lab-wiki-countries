848d24ad0f4a1e084dc5f00eec4fa1f35c520d8d
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-25 19:49:32 -0400

    content has displayed, app closing tab in index was missing

--- starter-code/src/Home.js
+++ starter-code/src/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
 
+// Home is wrapped in Switch, thus props may be executed
 class Home extends Component {
     render() {
         // console.log(this.props);
         return (
             <div>
-                <p>Home is where my mind is</p>
+                Home is where my mind is
             </div>
         );
     }
