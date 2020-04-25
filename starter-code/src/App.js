54d5b15a0adf18791e9a3617dc8f591ecd5b4f59
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-25 19:21:06 -0400

    navbar

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,28 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 // COMPONENTS 
+import Navbar from './Navbar';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 
 
 class App extends Component {
-  state = { }
-  componentDidMount(){ // window.onload happens on intial component mounts
-
-  }
-
+ 
   render() {
   return (
     <div>
-        <h1> Hello ? Are you human ? </h1>
+        <h1> Hello ? Are you human ? helllllllooo why arent you showing homie </h1>
          <nav>
-         <Link to='/'> Home </Link> {/* to === attribute has to match the path given*/}
-         <Link to='/About'> About </Link>
-         <Link to ='/Contact'> Contact Us </Link>
+         <Link to='/'>Home</Link> {/* to === attribute has to match the path given*/}
+         <Link to='/About'>About</Link>
+         <Link to ='/Contact'>Contact Us</Link>
          </nav>
          {/* Link to === a href, but with Switc component */}
-
+      <Navbar />
       <Switch>
       <Route exact path='/' component={(props) => <Home{...props} /> }></Route>
       <Route exact path='/About' component={(props) => <About{...props} /> }></Route>
--- starter-code/src/Home.js
+++ starter-code/src/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Home extends Component {
     render() {
-        console.log(this.props);
+        // console.log(this.props);
         return (
             <div>
                 <p>Home is where my mind is</p>
