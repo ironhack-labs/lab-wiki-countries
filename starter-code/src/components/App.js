e5aeab993d707da405dc5cbf71ba28257dacd2f3
Julian Kizeno <julianquiceno20@gmail.com>
2020-05-07 00:48:38 +0200

    done

---
+++ starter-code/src/components/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import countries from '../countries.json'
+import { Switch, Route, Link } from 'react-router-dom'
+import CountriesDetails from '../components/countries/CountriesDetails'
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.countriesCopy = [...countries]
+        this.state = {
+            countries: this.countriesCopy
+        }
+    } 
+
+    render() {
+        return (
+            <>
+            <div id="root">
+                <div>
+                    <nav className="navbar navbar-dark bg-primary mb-3">
+                        <div className="container">
+                            <a className="navbar-brand" href="/">WikiCountries</a>
+                        </div>
+                    </nav>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-5" >
+                                <div className="list-group">
+                                    {
+                                        this.state.countries.map((elm, idx) =>{
+                                            return (
+                                                <Link className="list-group-item list-group-item-action" key={idx} to={elm.cca3} > {elm.flag} {elm.name.common} </Link>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                            <Switch>
+                                <Route path="/:cca3" exact render={(props) => <CountriesDetails {...props} />}/>} />
+                            </Switch>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </>
+        )  
+    }
+}
+export default App
---
+++ starter-code/src/components/countries/CountriesDetails.js
@@ -0,0 +1,79 @@
+import React from "react"
+import countries from '../../countries.json'
+import { Link } from 'react-router-dom'
+
+const CountriesDatails = (props) => {
+    const foundCountry = countries.find(elm => props.match.params.cca3 === elm.cca3)
+    return (
+        <div className="col-7">
+            <h1>{foundCountry.name.common} {foundCountry.flag}</h1>
+            <table className="table">
+                <thead></thead>
+                <tbody>
+                    <tr>
+                        <td> 
+                            <h3>Capital</h3> 
+                        </td>
+                        <td><strong>{foundCountry.capital}</strong></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h3>Area</h3>
+                        </td>
+                        <td>
+                            {foundCountry.area}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h3>Borders</h3>
+                        </td>
+                        <td>
+                            <ul>
+                                {foundCountry.borders.map((elm, idx) => <li key={idx}><Link key={idx} to={elm}> {countries.find((country) => country.cca3 === elm).name.common}</Link> </li>)}
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default CountriesDatails;
+
+// import React from "react"
+// import countries from '../../countries.json'
+// import { Link } from 'react-router-dom'
+
+// const Country = props => {
+//     const foundCountry = countries.find(elm => props.match.params.cca3 === elm.cca3)
+//     return (
+//         <>
+//             <div className="col-7" >
+//                 <table className="table">
+//                     <thead></thead>
+//                     <tbody>
+//                         <tr>
+//                             <td>Pais: {foundCountry.name.common}</td>
+//                             <td>Capital: {foundCountry.capital}</td>
+//                         </tr>
+//                         <tr>
+//                             <td>Area</td>
+//                             <td>{foundCountry.area}</td>
+//                         </tr>
+//                         <tr>
+//                             <td>Borders</td>
+//                             <td>
+//                                 <ul>
+//                                     {foundCountry.borders.map((elm, idx) => <li key={idx}><Link className="list-group-item list-group-item-action" key={idx} to={elm}> {countries.find((country) => country.cca3 === elm).name.common}</Link> </li>)}
+//                                 </ul>
+//                             </td>
+//                         </tr>
+//                     </tbody>
+//                 </table>
+//             </div >
+//         </>
+//     )
+// }
+// export default Country
