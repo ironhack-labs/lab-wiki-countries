0ad78d9c95992c9d8cb614965af207ef3a94c271
Gabriela Gallango <ga.gallango@gmail.com>
2020-05-06 23:17:04 +0200

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import countries from './countries.json'
+import { Switch, Route, Link } from 'react-router-dom'
+import CountryDetail from './CountryDetail/CountryDetail'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+    this.countriesCopy = [...countries]
+    this.state = {
+      countries: this.countriesCopy
+    }
+  }
+  render() {
+    return (
+      <>
+        <div id="root">
+          <div>
+            <nav className="navbar navbar-dark bg-primary mb-3">
+              <div className="container">
+                <a className="navbar-brand" href="/">WikiCountries</a>
+              </div>
+            </nav>
+            <div className="container">
+              <div className="row">
+                <div className="col-5">
+                  <div className="list-group">
+                    {
+                      this.state.countries.map((elm, idx) => {
+                        return (
+                          <Link className="list-group-item list-group-item-action" key={idx} to={elm.cca3}>{elm.flag} {elm.name.common}</Link>
+                        )
+                      })
+                    }
+                  </div>
+                </div>
+                <Switch>
+                  <Route path="/:cca3" exact render={(props) => <CountryDetail {...props} />} />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
 }
-
-export default App;
+export default App
