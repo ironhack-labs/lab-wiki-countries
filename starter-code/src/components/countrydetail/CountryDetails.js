4e5df41414a42208c50b0017dfadbac9c60759ac
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2020-05-06 23:30:16 +0200

    add files work done

---
+++ starter-code/src/components/countrydetail/CountryDetails.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import countries from '../../countries.json'
+
+import { Link } from 'react-router-dom'
+
+const CountryDetails = props => {
+    const foundCountry = countries.find(elm => props.match.params.cca3 === elm.cca3)
+    return (
+        <>
+        <div className="col-7">
+                <h1>{foundCountry.name.common}</h1>
+                <hr/>
+                <h4>Capital: {foundCountry.capital}</h4>
+                <hr/>
+                <h4>Area: {foundCountry.area} Km2</h4>
+                <hr/>
+                <ul>Borders: {foundCountry.borders.map((elm, idx) =>
+                    <li key={idx}> <Link className="list-group-item list-group-item-action" key={idx} to={elm}> {countries.find((country) => country.cca3 === elm).name.common} </Link> </li>)}
+                </ul>
+          
+            </div>
+
+            </>
+    )
+}
+
+export default CountryDetails
