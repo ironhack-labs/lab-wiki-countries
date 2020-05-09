f18caf6f372ff59557bc8011a278709b976a5885
Alex Panko <alexander.panko@gmail.com>
2020-05-09 15:18:56 +0200

    done

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import countries from '../countries.json'
+import { Route, Link, Switch } from 'react-router-dom'
+
+export default function CountriesList() {
+    return (
+        countries.map((country, index) => (
+            <div key={index}>
+                <Link className="list-group-item list-group-item-action text-left" to={"/" + country.cca3}>
+                        {country.flag} 
+                        {" "}
+                        {country.name.common}
+                </Link>
+            </div>
+        ))
+    )
+}
+
+
+
+
