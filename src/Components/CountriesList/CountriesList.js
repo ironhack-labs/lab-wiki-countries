4b5759bf6fdbb90db118297bbc0af87622b8956c
Andre <andre.talpo@gmail.com>
2020-05-21 04:03:19 -0300

    Finalizado sem estilizacao

--- src/Components/CountriesList/CountriesList.js
+++ src/Components/CountriesList/CountriesList.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './CountriesList.css'
 
 const CountriesList = ({ countries }) => {
     return (
         <div className="list-group">
             {
                 countries.map((e, index) =>
-                    <a href={`/country/${e.cca2}`} className="list-group-item list-group-item-action" key={index}>
-                        <p>{e.cca2}</p>
-                        <p>{e.name.official}</p>
+                    <a href={`/country/${e.cca3}`} className="list-group-item list-group-item-action" key={index}>
+                        <img src={`https://www.countryflags.io/${e.cca2}/flat/64.png`} alt={`${e.name} flag`} />
+                        <p>{e.name.common}</p>
                     </a>
                 )
             }
--- src/Components/CountryDetail/CountryDetail.js
+++ src/Components/CountryDetail/CountryDetail.js
@@ -2,10 +2,25 @@ import React from 'react';
 import countries from '../../countries.json';
 
 const CountryDetail = (props) => {
-    const country = countries.find((e) => e.cca2 === props.match.params.id);
+    const country = countries.find((e) => e.cca3 === props.match.params.id);
     return (
-        <h2>{country.name.official}</h2>
+        <div>
+            <h2>{country.name.common}</h2>
+            <p>Capital: {country.capital[0]}</p>
+            <p>Area: {country.area} Km2</p>
+            <p>Borders:</p>
+            <ul>
+                {fillBorders(country.borders)}
+            </ul>
+        </div>
     );
+}
+
+const fillBorders = (borders) => {
+    return borders.map((e, index) => {
+        const border = countries.find(country => e === country.cca3);
+        return <li key={index}><a className="listitem" href={`/country/${border.cca3}`}>{border.name.common}</a></li>
+    });
 }
 
 export default CountryDetail;
