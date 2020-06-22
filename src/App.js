6e748364042e0c6dd0651289eff75ec282892bc8
Nardo Kraaijeveld <nardo.kra@gmail.com>
2020-06-22 20:43:58 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import CountriesList from './components/CountriesList.jsx';
+import CountryDetail from './components/CountryDetail.jsx';
+import { Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <nav className="navbar navbar-dark bg-primary mb-3">
+                    <div className="container">
+                        <a className="navbar-brand" href="/">WikiCountries</a>
+                    </div>
+               </nav>
+               <div className="container">
+                    <div className="row">
+                        <div className="col-5 countries-styling">
+                            <Route path="/" component={CountriesList}/>
+                        </div>
+                        <div className="col-7">
+                            <Route path="/country/:id" component={CountryDetail}/>
+                        </div>
+                    </div>
+                </div>        
+            </div>
+        )
+    }
 }
 
 export default App;
+
