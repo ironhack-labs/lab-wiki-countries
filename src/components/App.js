855c63113cc4b99a55cb41503e636b8b1fd6252f
Míriam Sánchez Alcón <msanchezalcon.msa@gmail.com>
2020-07-15 22:40:39 +0200

    getting countries

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import countries from "../countries.json"
+
+import { Switch, Route } from "react-router-dom"
+
+import CountriesList from "./CountriesList/CountriesList"
+import CountryDetail from "./CountryDetail/CountryDetail"
+
+function App() {
+  return (
+
+    <h1>hi</h1>
+  )
+}
+
+export default App
