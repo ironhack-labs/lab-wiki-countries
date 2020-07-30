8b570d8aaaa90d230577ea255a72c4be554f75e9
Alberto Correia <alberto_cpv@hotmail.com>
2020-07-31 01:58:01 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import Navbar from "./components/Navbar/Navbar"
+import CountryList from "./components/CountryList/CountryList"
+import CountriesDetails from "./components/CountriesDetails/CountriesDetails"
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <CountryList></CountryList>
+      <Switch>
+              <Route exact path="/:id" render={(props) => <CountriesDetails  {...props} />} />
+            </Switch>
+
     </div>
   );
 }
