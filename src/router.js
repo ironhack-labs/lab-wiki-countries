8f6272904ebefb6ef96d20159c6f62527e5d596a
Yvan Menuet <yvan.menuet@gmail.com>
2020-09-16 19:17:49 -0500

    restart

---
+++ src/router.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Router, Route, Switch, Link } from "react-router-dom"
+// import Navbar from './components/Navbar'
+
+const router = () => {
+    return (
+        <Router>
+            {/* <Navbar /> */}
+        </Router>
+    )
+}
+
+export default router
