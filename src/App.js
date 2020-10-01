0d56cdae470f810d72e172492c05e9221c80f1a9
Maria Pardo <pardo.genis@gmail.com>
2020-10-01 20:22:45 +0200

    iterations 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           <Navbar />
+           <div className="container">
+        <div className="row">
+          <CountriesList />
+          <Switch>
+            <Route exact path="/:id" component={CountryDetails} />
+          </Switch>
+        </div>
+      </div>
+
     </div>
   );
 }
