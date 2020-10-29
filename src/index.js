dd31b325bac429fe8841cc4e68f3bf5efd989af1
anna dodo <anna.dorenskaya@gmail.com>
2020-10-29 20:16:27 +0100

    1.2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+       <App />
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
 
