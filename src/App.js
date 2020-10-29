e7478919423bdcaab21eaacd9d54065c3d333346
anna dodo <anna.dorenskaya@gmail.com>
2020-10-29 23:32:44 +0100

    smth

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import './App.css';
 import countries from './countries.json';
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails'
+import { Route, Link } from 'react-router-dom';
 
 function App() {
     
   return (
     <div className="App">
+    <Link to='/yo'>Link</Link>
       <Navbar />
       <CountriesList data={countries} />
+      <Route path='/yo' component={CountryDetails}/>
     </div>
   );
 }
---
+++ src/components/CountryDetails.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route, Link } from 'react-router-dom';
+
+export default function CountryDetails() {
+    return (
+        <div>
+           smth
+        </div>
+    )
+}
