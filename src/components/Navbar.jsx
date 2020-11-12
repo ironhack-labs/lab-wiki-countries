47e7ed16d84646daada80f604cbb346da3013bec
Adrián Paniagua <paniaguasanchezadrian@gmail.com>
2020-11-12 17:45:10 +0100

    Button to go back XD

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
       <div className="navbar navbar-light bg-primary text-white">
-        <h2 className="p-1">WikiCountries</h2>
+        <h2 className="p-1">
+          <Link to={'/'}>WikiCountries</Link>
+        </h2>
       </div>
     </>
   );
