09d3f30e4fbf5434e26d72dc35867cd38bf62720
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-25 17:30:42 -0600

    3 copoonentts

---
+++ src/components/CountryDetails.js
@@ -0,0 +1,3 @@
+// Is the component that we will render via the react-router-dom's Route and will be receiving the country code (cca3) via the URL.
+
+//This is actually the id of the country (example: /ESP for Spain, /FRA for France).
