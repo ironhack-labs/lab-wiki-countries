b467a5a5e4f00067323b5ee9d073ac3c52190e72
Isabel Ferrara <ifm95@hotmail.com>
2020-11-26 09:33:10 -0600

    corregido

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,26 +1,29 @@
 import React, {useState} from 'react'
 import countries from '../countries.json'
-
+import {Link} from 'react-router-dom'
 
 const CountryDetails = (props) => {
-    const [ctry, setCountries]=useState(countries)
-    const c=[...ctry]
-    
+    const c=[...countries]
     let cca3Props= props.match.params.cca3
-    let info= c.filter(country=> country.cca3 === cca3Props)
+    let info= c.find(({cca3}) => cca3 === cca3Props)
     console.log(info)
     // setCountries(info)
   
 
     return (
         <div>
-        <h1> CountryDetails</h1>
- 
-          <h1></h1>
-          <h1>{props.match.params.cca3} </h1>
-          <h2>{ctry.area}</h2>
-          <h2>{ctry.capital}</h2>
-          {/* <h1>{props.match.params} </h1>  */}
+        <h1> CountryDetails:</h1>
+          <h1>Name:{info.name.common}</h1>
+          <p>Cca3: {props.match.params.cca3} </p>
+          <p>Capital:{info.capital}</p>
+          <p>Area: {info.area}</p>
+          {info.borders.map(border=> (
+              <li key={border}>
+                  <Link to={`/${border}`}>
+                  {(c.find(({cca3})=>cca3 === border)).name.common}
+                 </Link>
+              </li>
+          ))}
         </div>
     )
 }
