8804330cbe7b0caf38b8c071d4b516a0356de477
Juan Manuel <jramirezm942@gmail.com>
2020-12-02 19:59:09 -0600

    primeras iteraciones

---
+++ src/components/CountryDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CountryDetails extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
