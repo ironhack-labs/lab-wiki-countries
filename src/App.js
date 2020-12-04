2b32625fab84df59a6dee166b2c39a1829eb899e
Marc Gotzens <marcgotzens@hotmail.com>
2020-12-04 14:36:03 +0100

    finished 1.4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import CountriesList from './Components/CountriesList';
+import CountryDetails from './Components/CountryDetails';
+import 'bootstrap/dist/css/bootstrap.css';
+import countries from './countries';
 
 function App() {
+  console.log('Countries LIst in App.js', countries);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <CountriesList Countries={countries} />
+          <CountryDetails />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/CountriesList.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import CountryDetails from './CountryDetails'
+import { Route, Link, Switch } from "react-router-dom";
+
+export default function CountriesList(props) {
+
+    console.log(props.countries)
+    return (
+        <div className="col-5" styles={"max-height: 90vh; overflow: scroll;"}>
+            <div className="list-group">
+            
+               
+            {/* <a className="list-group-item list-group-item-action" href="/AFG">🇦🇫 Afghanistan</a> */}
+                {props.Countries.map((el) => (
+                    <Link key={el.name.common} className="list-group-item list-group-item-action" to={`/${el.cca3}`}>{el.cca2} {el.name.common}</Link>
+                    
+                ))}
+                <Switch>
+                    <Route exact path="/:cca3" render={(reactRouterProps) => (<CountryDetails {...reactRouterProps} countries={props.Countries}/>)}/>
+                </Switch>
+          </div>
+        </div>
+    )
+}
