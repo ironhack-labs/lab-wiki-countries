4ed27bf9440ea11941c7cc2513cebf0fd5aa1a20
Hector Lima <hectoraugusto12@yahoo.ie>
2020-12-09 21:42:56 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <div className="container">
+          <div className="row">
+            <CountriesList />
+            <Switch>
+              <Route path="/:country" component={CountryDetails} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <span className="navbar-brand mb-0 h1">WikiCountries</span>
+    </nav>
+  );
+}
+
+export default Navbar;
