686ea0567db7044731d8b306734304da0637b6b7
srtagalicia <srtagalicia@gmail.com>
2020-12-10 01:19:57 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import Navbar from './components/Navbar';
+//import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    CountriesList: []
+  };
+
+  componentDidMount() {
+    axios.get('https://countries.tech-savvy.tech/countries')
+      .then((res) => {
+        this.setState({
+          CountriesList: res.data
+        })
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <CountriesList countries={this.state.countriesList} />
+            <Route path="/:cca3" render={(routeProps) => {
+              return( <CountryDetails countriesList={this.state.countriesList} {...routeProps} /> );
+            }}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="navcontainer">
+                <a className="navbar-brand text-white ml-3" href="/">WikiCountries</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
