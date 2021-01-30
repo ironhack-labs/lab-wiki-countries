31c2094d8fd264e73aff9b2a7581740c91f58e77
Daina Slezaite <daina.slezaite@gmail.com>
2021-01-30 16:34:44 +0100

    Done without bonus

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React from 'react';
 import './App.css';
-import countries from './countries.json';
 import {Navbar} from './components/Navbar';
 import {CountriesList} from './components/CountriesList';
 import {CountryDetails} from './components/CountryDetails';
 import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <div className="row">
-          <CountriesList countries={countries} />
-          <Switch>
-            <Route path='/:cca3' render={(props) => {
-                return <CountryDetails {...props} countries={countries} />
-              }}   
-            />
-          </Switch>
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = { 
+      countries: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/countries.json')
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          countries: result
+        })
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          {this.state.countries.length > 0 ? 
+          <div className="row">
+            <CountriesList countries={this.state.countries} />
+              <Switch>
+                <Route path='/:cca3' render={(props) => {
+                    return <CountryDetails {...props} countries={this.state.countries} />
+                  }}   
+                />
+              </Switch>
+          </div> : 'There are no countries to display' }
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
