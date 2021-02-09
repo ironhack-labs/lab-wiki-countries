f3daebdd39c184452411f50a73bf664fb0c82f4c
ZuzanaAK <kubatovazuzana@gmail.com>
2021-02-09 10:13:46 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import countries from './countries.json'
+import { Link, Switch, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+    state = {
+      countries:countries
+    }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+            <div className="row">
+              <CountriesList countriesList={this.state.countries} />
+              <Switch>
+                <Route path="/:countryCode" render={ props => {
+                  return <CountryDetails {...props} countriesList={this.state.countries} />
+                }}/>
+              </Switch>
+            </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
+  <Router>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
