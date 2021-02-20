b3c3e2e2cca018ccec8b1321e70fde89c6f614ac
shtesh <vshtefanuka@gmail.com>
2021-02-20 12:12:02 +0100

    Done

---
+++ src/components/CountryDetails.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+
+function CountryDetails(props) {
+  
+    function getCountryDetails(countryCode) {
+    return props.countriesList.find((elm) => {
+      return elm.cca3 === countryCode;
+    });
+  }
+  
+  const foundCountry = getCountryDetails(props.match.params.countryCode);
+  const { capital, area, name } = foundCountry;
+
+  let borderList = '';
+  if (foundCountry.borders.length > 0) {
+    borderList = foundCountry.borders.map((countryCode) => {
+      const foundBorder = getCountryDetails(countryCode);
+      return (
+        <li key={countryCode}>
+          <Link to={`/${countryCode}`}>{foundBorder.name.common}</Link>
+        </li>
+      );
+    });
+  } else {
+    borderList = 'This country has no borders';
+  }
+
+  return (
+    <div className="col-7">
+      <h1>{name.common}</h1>
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style={{ width: '30%' }}>Capital</td>
+            <td>{capital}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {area} km
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>{borderList}</ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default CountryDetails;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
