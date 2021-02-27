cc72befcad9166e43ff58ee887e75a7b35fce2cf
Daniela Alonzo <monsterjunior@outlook.es>
2021-02-27 12:20:03 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from './components/Navbar'
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Navbar />
+      <Switch>
+        <Route exact path="/countries" component={CountriesList} />
+      </Switch>
+      <Switch>
+        <Route exact path="/countries/:country" component={CountryDetails}/>
+      </Switch>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <Link className="navbar-brand" to="/countries">
+          WikiCountries
+        </Link>
+      </nav>
+    </div>
+    )
+}
