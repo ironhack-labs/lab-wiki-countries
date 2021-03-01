c16aec6abfde0577af54883b7d13f178db7f60b5
Diego Barrera <di_barrera@hotmail.com>
2021-03-01 09:50:15 -0600

    second commit

---
+++ src/components/CountriesList.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import React, {useEffect,useState} from 'react'
+import { 
+    Link
+ } from 'react-router-dom'
+import CountryDetails from './CountryDetails'
+export default function CountriesList() {
+    const [countriesList, setCountriesList] = useState()
+    useEffect(()=>{
+        const  responseAPI = async () =>{
+             const listCountries = await axios.get(`https://restcountries.eu/rest/v2/all`)
+             setCountriesList(listCountries.data)
+            }
+            responseAPI()
+    },[])
+    return (
+        <>
+        <div className="container">
+        <div className="row">
+          <div className="col-5" style={{maxHeight: "70vh" , overflow: "scroll"}}>
+            <div className="list-group">
+                {
+                countriesList === undefined ? (<p>Cargando</p>) :
+                (
+                    countriesList.map((e,id)=>{
+                        return( 
+                            <div key={id} className="list-group-item list-group-item-action">
+                                <img alt={e.name} width="20" className="d-flex justify-content-left" src={e.flag}/>
+                                 <Link to={e.name}>
+                                    {e.name}
+                                 </Link>
+                            </div>
+                        )
+                    })
+                )
+                }
+            </div>
+          </div>
+            <CountryDetails/>
+        </div>
+      </div>
+        </>
+    )
+}
