e3a0dbcb48fde54c66a6eae75560a5f75db41490
fmmyriam <benhmyriam@gmail.com>
2021-03-18 23:37:05 +0100

    forgot to add the country information

--- src/Components/CountryDetails.jsx
+++ src/Components/CountryDetails.jsx
@@ -6,11 +6,27 @@ const CountryDetails = (props) => {
     const countryID = props.match.params.cca3name;
 
     const foundCountry = JSONcountries.find((country) => country.cca3 === countryID);
+    console.log(foundCountry)
+
+    // const paysFrontaliers = 
+    // .forEach((pays) => { pays.borders !== undefined})
     console.log(foundCountry);
     return (
         <div>
-            <h1>This country is {foundCountry.name.common} poupou</h1>
-            
+            <h1>This country is {foundCountry.name.common}</h1>
+            <p>
+                What informations do we have on {foundCountry.name.common}? <br />
+                We know that {foundCountry.name.common}'s flag is: {foundCountry.flag} <br />
+                {foundCountry.name.common} shares frontiers with these countries: <br />
+                
+                    {/* {foundCountry.map((country) => {
+                        return (
+                        <div key={country.borders}>
+                            test
+                        </div>)
+                    })} */}
+            </p>
+
         </div>
     )
 }
