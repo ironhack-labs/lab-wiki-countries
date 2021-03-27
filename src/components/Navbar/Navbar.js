dcc605c109a025ef63a23a25f73f1ec9c243cd9f
Ángela Herrador <angelaherrador1991@gmail.com>
2021-03-27 14:59:41 +0100

    It 1.1 and 1.2 done. Work on 1.3

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <nav className="navbar navbar-dark bg-primary mb-3">
+                <div className="container">
+                <a className="navbar-brand" href="/">WikiCountries</a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
