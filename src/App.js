7bbacd2c5485926979e09f55a4c8abdf4a7714b8
Manuel Carrillo Almoguera <manucaralmo@gmail.com>
2021-03-30 18:11:12 +0200

    Lab done with bonus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from './components/Navbar/Navbar';
+import CountriesList from './components/CountriesList/CountriesList';
+import { Route, Switch } from 'react-router';
+import CountryDetails from './components/CountryDetails/CountryDetails';
+
 
 function App() {
+
+  const [countries, setcountries] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json()).then(data => {
+        setcountries(data)
+        setLoading(false)
+    })
+  }, [countries])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
+      <div className="container mt-4">
+
+        { loading
+            ? 'Loading content...'
+            : <div className="row">
+                <div className="col-sm-4">
+                  <CountriesList countries={countries} />
+                </div>
+                <div className="col-sm-8">
+                  <Switch>
+                    <Route path="/:code" render={ (historyProps) => <CountryDetails {...historyProps} countries={countries} />} />
+                    <Route path="/"><h1>Select a country</h1></Route>
+                  </Switch>
+                </div>
+              </div>
+        }
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-dark bg-blue">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">WikiCountries</span>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
