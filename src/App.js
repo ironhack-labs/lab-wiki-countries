992cbaf424936d8273bc7a054b8ed9de0cf54b60
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-03-31 11:07:14 +0200

    Add iteration 0, 1.1, 1.2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+//components
+
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar/>
       </header>
+      <main>
+      </main>
+      <footer>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+export default function Navbar() {
+    return(
+        <nav class="navbar navbar-light navbar__container">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/"><h2>WikiCountries</h2></a>
+            </div>
+        </nav>
+    )
+}
+
