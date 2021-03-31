cde6525dd7646b5d35708fa9036e00bde2d8c8dd
Ángela Herrador <angelaherrador1991@gmail.com>
2021-03-31 17:56:24 +0200

    Work on it 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import CountriesList from './components/CountriesList/CountriesList'
+import CountryDetails from './components/CountryDetails/CountryDetails'
+import { Component, Fragment } from 'react';
+import { Route } from 'react-router-dom'
+import data from './countries.json'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <div className="row">
-          <CountriesList/>
+class App extends Component {
+  state = {
+        countries: [...data]
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <div className="col-5" style={{ maxHeight: "90vh", overflow: "scroll"}}>
+              <div className="list-group">
+                {
+                  this.state.countries.map((country) => 
+                    <Fragment key={country.cca3}>
+                      <CountriesList {...country} />
+                    </Fragment>
+                  )
+                }
+                <CountryDetails countries={this.state.countries} />
+                <Route path="/:code" component={CountryDetails} />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+
+    ) 
+  }
 }
 
 export default App;
---
+++ src/components/CountryDetails/CountryDetails.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CountryDetails = (props) => {
+    console.log(props.match)
+    //console.log(props.countries)
+    const countries = props.countries
+    //console.log(props.match.params.cca3)
+    return (
+        <div className="CountryDetails">
+            {
+                
+            }
+        </div>
+    )
+}
+
+export default CountryDetails
