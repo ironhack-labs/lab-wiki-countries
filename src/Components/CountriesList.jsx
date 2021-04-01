f0db70deb709a142aa547e24f523bc7b31417142
luisga32 <luisga32@hotmail.com>
2021-04-01 13:03:38 +0200

    CountriesList change

--- src/Components/CountriesList.jsx
+++ src/Components/CountriesList.jsx
@@ -2,16 +2,20 @@
 import { Link } from 'react-router-dom';
 
 
-const CountriesList = ({countries}) => {
+const CountriesList = ({ countries }) => {
 
   return (
-      <>
-        {countries.map((country) =>(
-            
-            <Link to={`/${country.cca3}`} key={country.cca3} className='list-group-item list-group-item-action'>{country.flag} {country.name.common}</Link>
-        ))}
-      </>
-   
+    <>
+      {countries.map((country) => {
+
+        return (
+
+          <Link to={`/${country.cca3}`} key={country.cca3} className='list-group-item list-group-item-action'>{country.flag} {country.name.common}</Link>
+        )
+      }
+      )}
+    </>
+
 
   )
 }
