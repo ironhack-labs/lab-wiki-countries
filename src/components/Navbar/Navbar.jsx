7fe8d31516a883380f7a9cd7dc1bb88642e641d9
Alicia Parra <alicia@redparra.com>
2021-04-01 11:58:15 +0200

    Do iteration 1.1 and 1.2

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='NavBar'>
+            <nav className="navbar navbar-dark bg-primary mb-3">
+                <div className="container">
+                <a className="navbar-brand" href="/">WikiCountries</a>
+                </div>
+            </nav> 
+        </div>
+    );
+};
+
+export default Navbar;
