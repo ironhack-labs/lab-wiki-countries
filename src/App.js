a16b02c8e0f37329eec16443401a8c4352ae3971
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-04-02 20:52:56 +0200

    Add iteration 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Route, Switch } from "react-router-dom";
 import {Component} from 'react'
+import Loader from "react-loader-spinner";
 
 //data
-import dataCountry from './countries.json'
+import dataCountries from './countries.json'
 
 //components
 import Navbar from './components/Navbar/Navbar'
@@ -14,7 +15,22 @@ import './App.css';
 
 class App extends Component {
   state = {
-    data: dataCountry
+    data: null,
+    loading: true
+  }
+
+  componentDidMount() {  
+    console.log('DOM renderizado')
+    
+    this.setState({
+      data: dataCountries,
+      loading: false
+    })
+
+  }
+
+  componentWillUnmount() {  
+
   }
 
   render(){
@@ -23,22 +39,27 @@ class App extends Component {
         <header>
           <Navbar/> 
         </header>
-        <main>
-          <div className="container">
-            <div className="row">
-              <div className="col-4" style={{maxHeight: 700, overflow: "scroll"}}>
-                <CountriesList data={this.state.data}/>
-              </div>
-              <div className="col-8" >
-                <Switch>
-                  <Route 
-                    path="/:cca3" 
-                    render={ props =>  <CountryDetails props={props} data={this.state.data}/> } /> 
-                </Switch>
+        
+        {this.state.loading ? 
+          (<Loader type="TailSpin" color="#00BFFF" height={80} width={50} /> )
+          :
+          (<main>
+            <div className="container">
+              <div className="row">
+                <div className="col-4" style={{maxHeight: 700, overflow: "scroll"}}>
+                  <CountriesList data={this.state.data}/>
+                </div>
+                <div className="col-8" >
+                  <Switch>
+                    <Route 
+                      path="/:cca3" 
+                      render={ props =>  <CountryDetails props={props} data={this.state.data}/> } /> 
+                  </Switch>
+                </div>
               </div>
             </div>
-          </div>
-        </main>
+          </main>)
+        }
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.css'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 ReactDOM.render(
   <BrowserRouter>
