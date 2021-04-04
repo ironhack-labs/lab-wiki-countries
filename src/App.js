3e16f34d39022bd14b43b8b2f07eb486b1211956
cisnerosjj <jorgecisnerosgavin@gmail.com>
2021-04-04 23:48:33 +0200

    Iteration 1.1 | Create components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div class="container">
+          <div className="row">
+            <div className="col-5 countriesColu">
+            </div>
+            <div className="col-7 infoCountry">
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-reportWebVitals();
