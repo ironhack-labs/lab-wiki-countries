edf5d9d8fcdc5a59d6010b15d1f2504d22b71e03
David Bueno <davidbuenox8@gmail.com>
2021-05-06 17:55:40 +0200

    done without  bonus

---
+++ src/components/CountriesList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import countries from '../countries.json';
+import { Link } from 'react-router-dom'
+
+export default function CountriesList(props) {
+
+    const countriesList = props.countries.map(country => {
+        return (
+            <div key={country.cca3}>
+                <Link to={`/countries/${country.cca3}`}>{country.name.official}</Link>
+            </div>
+        )
+    })
+    return (
+        <div>
+            {countriesList}
+        </div>
+    )
+}
