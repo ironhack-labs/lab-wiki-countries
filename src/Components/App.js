ace9849a473b3044fe0e169fa1749b85725d0313
Willy <willyman99@gmail.com>
2021-05-06 09:16:07 +0200

    First files

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './NavBar'
+
+function App() {
+  return (
+    <main>
+      <NavBar />
+      
+      
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h2>WikiCountries</h2>
+        </nav>
+    )
+}
+
+export default NavBar
