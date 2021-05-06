3a8132f817535033ea6fc25a754c387a0a07fffd
Alkin Maystorov <alkin971@gmail.com>
2021-05-06 17:57:57 +0200

    iteration 1-3 need bug fix when reloading the page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import { Route } from 'react-router-dom';
+import countries_data from './countries.json';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      countries: countries_data,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <div className="col-5">
+              <CountriesList countries={this.state.countries} />
+            </div>
+            <div className="col-7">
+              <Route
+                exact
+                path="/countries/:id"
+                render={(props) => (
+                  <CountryDetails {...props} countries={this.state.countries} />
+                )}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div>
+//       <Navbar />
+//       <div className="container">
+//         <div className="row">
+//           <div className="col-5">
+//             <CountriesList />
+//           </div>
+//           <div className="col-7">
+//             <Route exact path="/countries/:id" component={CountryDetails} />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
