0a3566cfa56b3e5e80f336f7eabaf5c5bb2b2c0b
Marcia Faria <mfndnc@gmail.com>
2021-05-07 09:01:45 +0200

    adding componentDidMount logic - preparing for future api load

--- src/App.js
+++ src/App.js
@@ -4,31 +4,50 @@ import { Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
+import React from 'react';
 
-import countries from './countries.json';
+import countries_data from './countries.json';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <div className="container">
-        <div className="row">
-          <div className="col-5">
-            <CountriesList countries={countries} />
-          </div>
-          <div className="col-7">
-            <Route
-              exact
-              path="/countries/:id"
-              render={(props) => (
-                <CountryDetails {...props} countries={countries} />
-              )}
-            />
+export default class App extends React.Component {
+  constructor(props) {
+    console.log('constructor');
+    super(props);
+    this.state = {
+      countries: [],
+    };
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    this.setState({
+      countries: countries_data,
+    });
+  }
+
+  render() {
+    if (this.state.countries.length === 0) return <h3>Loading...</h3>;
+    console.log('render');
+
+    return (
+      <div>
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <div className="col-5">
+              <CountriesList countries={this.state.countries} />
+            </div>
+            <div className="col-7">
+              <Route
+                exact
+                path="/countries/:id"
+                render={(props) => (
+                  <CountryDetails {...props} countries={this.state.countries} />
+                )}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
