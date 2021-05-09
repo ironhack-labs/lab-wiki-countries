8505096ec8b1083e8b45d5b0402e8d7949180e38
Silinde87 <paurodriguezmolina@gmail.com>
2021-05-09 21:20:47 +0200

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import './App.css';
+import React, { Component } from 'react';
 import CountriesList from './components/CountriesList/CountriesList';
 import CountryDetails from './components/CountryDetails/CountryDetails';
 import Navbar from './components/Navbar/Navbar';
 import json from './countries.json';
 import { Route, Switch } from 'react-router';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />      
-      <div className="container">
-        <div className="row">
-          <CountriesList countries={json}/>
-          <Switch>
-            <Route exact path="/:cca3" component={CountryDetails} countries={json} />
-          </Switch>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: [],
+    };
+  }
+  componentDidMount() {
+    this.setState({ countries: json });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <CountriesList countries={this.state.countries} />
+            <Switch>
+              <Route
+                exact
+                path="/:cca3"
+                component={CountryDetails}                
+              />
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
