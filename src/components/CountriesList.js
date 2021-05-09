82a814b80d79b5776235df55d541ce38cde070b7
Sisco Falcó <siscofalcop@gmail.com>
2021-05-09 11:33:31 +0200

    iteration 1, 1.2, 2.3, 1.4 and 2 done

---
+++ src/components/CountriesList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import countriesArr from '../countries.json';
+
+class CountriesList extends Component{
+    constructor(){
+        super();
+        this.state = {
+            countries: countriesArr,
+        }
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-5" style={{ maxHeight: '90vh', overflow: 'scroll' }}>
+                        <div className="list-group">
+                            {this.state.countries.map((item) => {
+                                return(
+                                    <a className="list-group-item list-group-item-action" href={`/${item.cca3}`}>{item.flag} {item.name.common}</a>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CountriesList;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
