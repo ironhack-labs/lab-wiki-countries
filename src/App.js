a36719fe90de5e6accdf4e78028a87d29737e946
triana <trianaheinz@gmail.com>
2021-05-11 12:22:13 +0200

    almost done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import Countries from './countries.json';
+import { Switch, Route, Link, useParams } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    countries: [],
+  };
+
+  componentDidMount() {
+    this.setState({
+      countries: Countries,
+    });
+  }
+  getCountryByCca3(cca3) {
+    return Countries.find((country) => {
+      return country.cca3 === cca3;
+    });
+  }
+
+  render() {
+    return (
+      <div className="main-div">
+        <Navbar />
+
+        <div className="row">
+          <div className="col-5">
+            <CountriesList countries={this.state.countries} />
+          </div>
+
+          <Switch>
+            <div className="col-7">
+              <Route
+                path="/:cca3"
+                children={<CountryDetails getCountry={this.getCountryByCca3} />}
+              />
+            </div>
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbarStyle">
+      <h1 className="h1-navbar">Wiki Countries</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
