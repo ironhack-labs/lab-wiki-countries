1a9efa8f35d5f42e2089dd0bbf4d974f368364a2
SvjetlanaR <svjetlanarados@gmail.com>
2021-05-11 10:08:01 +0200

    done without css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import CountriesList from './components/CountriesList'
+import CountryDetails from './components/CountryDetails'
+import { Route } from 'react-router-dom'
+import React from 'react';
+import countries_data from './countries.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: []
+    }
+  };
+  componentDidMount() {
+    this.setState({
+      countries: countries_data
+    })
+  };
+
+  render() {
+    return (
+    <div>
+<Navbar/>
+     {/* <div><CountriesList /></div> */}
+     <div className='container'>
+     <CountriesList countries={this.state.countries} />
+     </div>
+     <Route exact path="/countries/:id" render={(props) => 
+     <CountryDetails {...props} countries={this.state.countries} />} />
+   </div>
+  )
 }
-
-export default App;
+};
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CountriesList from './CountriesList'
+
+export default function Navbar () {
+    return (
+        <div>
+            {CountriesList}
+        
+        </div>
+        )
+};
