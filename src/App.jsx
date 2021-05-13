3f77724b355c73cea977a4ec0d8320fcda57d167
Aleksandra Pietraszek <ola.pietraszek@gmail.com>
2021-05-13 20:39:10 +0200

    iterations 1.1 & 1.2

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+function CountriesList() {
+  return (
+    <div class="container">
+      <div class="row"></div>
+    </div>
+  );
+}
+
+export default CountriesList;
