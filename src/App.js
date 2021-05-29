234b4ae00086f0628d8f13988972349ff80bc607
Jordan Caley <jordancaley10@gmail.com>
2021-05-29 15:56:28 +0200

    done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import { Route, Switch } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import CountriesList from './Components/CountriesList';
 import CountryDetails from './Components/CountryDetails';
+import countries from './countries.json'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+      {/* <div className="container">
+        <div className="row">
+          <CountriesList countries={countries} />
+            <Route exact path="/:cca3" component={CountryDetails} />
+        </div>
+      </div> */}
 
       <Switch>
         <Route exact path="/" component={CountriesList} />
--- src/Components/CountriesList.jsx
+++ src/Components/CountriesList.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import countries from "../countries.json";
 import { Link } from "react-router-dom";
+import countries from './../countries.json'
 
 const CountriesList = () => {
   return (
     <div>
       {countries.map((country) => {
+        const flag = `https://www.countryflags.io/${country.cca2}/flat/64.png`
         return (
           <div key={country.cca3}>
-            <Link to={`/${country.cca3}`} className="list-group-item list-group-item-action">{country.name.common}</Link>
+            <Link to={`/${country.cca3}`} key={country.cca3} className="list-group-item list-group-item-action"><img src={flag} alt={country.name.common} /> {country.name.common}</Link>
           </div>
         );
       })}
