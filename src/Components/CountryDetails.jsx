c8fa935d2ab9552f1dbde731675a695c27daee68
Jordan Caley <jordancaley10@gmail.com>
2021-05-29 15:32:42 +0200

    Finish iteration 1.1

--- src/Components/CountryDetails.jsx
+++ src/Components/CountryDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import countries from "../countries.json";
+import { Link } from "react-router-dom";
 
 const CountryDetails = (props) => {
 
@@ -7,8 +8,6 @@ const CountryDetails = (props) => {
     const foundCountry = countries.find((country) => {
       return country.cca3 === props.match.params.cca3;
     });
-
-    console.log(foundCountry.name.common)
   
     return (
       <div>
@@ -34,8 +33,15 @@ const CountryDetails = (props) => {
                   <td>
                     <ul>
                       {foundCountry.borders.map((border) => {
+                        
+                        const borderCountry = countries.find((country) => {
+                          return country.cca3 === border
+                        })
+                       
                         return (
-                          <li><a>{border}</a></li>
+                          <li>
+                            <Link to={`/${borderCountry.cca3}`}>{borderCountry.name.common}</Link>
+                          </li>
                         );
                       })}
                     </ul>
