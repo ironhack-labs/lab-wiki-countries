2b6d77cde4a92c3b9668dce572baba57016404c1
andres_rog <andres_rog@outlook.com>
2021-06-24 11:19:50 -0500

    lab completado

---
+++ src/Components/CountriesList.js
@@ -0,0 +1,23 @@
+import {Link} from 'react-router-dom';
+
+const CountriesList = ({countries = [0,1,2,3]}) => (
+    <ul className="list-group countryList"> {
+            countries.map((country, index_c)=> (
+                    <li key={index_c} className="list-group-item">
+                        <Link
+                            to={{
+                                pathname:"/details",
+                                state:country
+                            }}
+                        >
+                            {country.flag}
+                            {country.name.official}
+                        </Link>
+                    </li>
+                )
+            )
+        }
+    </ul>
+);
+
+export default CountriesList;
