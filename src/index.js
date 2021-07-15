180b59130ea1a0697c4895f27c9223369525eda2
elyzx <ellenchildren@icloud.com>
2021-07-15 19:36:06 +0200

    unsure how i removed strictmode...added back in

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
