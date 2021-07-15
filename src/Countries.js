5d51539ff99774143947e1a9afbe0f5df7d15a86
Irina <ifalko@protonmail.com>
2021-07-15 17:43:56 +0200

    List of countries displayed

--- src/Countries.js
+++ src/Countries.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import data from './countries.json';
 
 const Countries = () => {
   return (
-    <div style={{ display: 'flex' }}>
+    <div>
       <h1>Here is the list of countries</h1>
+      {data.map((country) => {
+        return (
+          <div key={country.id}>
+            <p>{country.name.official}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
