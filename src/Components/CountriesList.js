b679c37d3a488988b05b257623bddbd9af72ecc5
Joao Fonseca <83319665+joaof85@users.noreply.github.com>
2021-07-16 01:02:48 +0100

    done

---
+++ src/Components/CountriesList.js
@@ -0,0 +1,59 @@
+import React from "react";
+import axios from "axios";
+class CountriesList extends React.Component {
+  state = {
+    countries: [],
+  };
+ 
+  async componentDidMount() {
+   
+    const response = await axios.get("https://restcountries.eu/rest/v2/all");
+    console.log(response.data);
+    this.setState({
+      countries: response.data,
+    });
+  }
+
+  
+
+  render() {
+    const { countries } = this.state;
+    return (
+      <>
+        <h1>Country List</h1>
+
+          {countries && countries.length > 0 ? (
+            this.state.countries.map((country) => {
+                return (
+                  <>
+                    <div key={country.alpha3Code} class="card mb-3" >
+                      <div class="row g-0">
+                        <div class="col-md-4">
+                          <img className="flag"
+                            src={country.flag}
+                            class="img-fluid rounded-start"
+                            alt="..."
+                          ></img>
+                        </div>
+                        <div class="col-md-8">
+                          <div class="card-body">
+                            <h5 class="card-title">{country.name}</h5>
+
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                );
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
+       
+      </>
+    );
+  }
+}
+
+
+export default CountriesList;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// comment skipped to stay organized
 reportWebVitals();
