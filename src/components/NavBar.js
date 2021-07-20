5e5943071c2c139fa4a33c72ed7b3101547ee05c
doris hutter <doris.hutter@gmx.de>
2021-07-20 11:31:27 +0200

    iteration 1 Nav-bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+
+    render() {
+        return (
+            <nav className="navbar navbar-light bg-primary">
+                <a className="link-nav" href="/">WikiCountries</a>
+            </nav>
+        );
+    }
+}
+
+export default Navbar; 
