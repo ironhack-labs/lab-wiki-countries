511c33eeb75f6889b3773b88d7a5f93fcb4b770a
Andrei <andrei_1002@hotmail.com>
2021-09-11 10:28:34 +0200

    feat: all components

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return(
+    <div className='Navbar'>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">LAB - WikiCountries</a>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
