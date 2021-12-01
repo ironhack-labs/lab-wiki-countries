697f459a9555f7e375209313ebff7cfd7d77aec8
Fernando Cardona <fernandocardona@MacBook-Pro-de-Fernando.local>
2021-12-01 17:50:03 +0100

    creating components

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom'; 
+
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
