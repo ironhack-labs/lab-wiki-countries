e74c9ecd870258857c142b638bcfa91296f0c5c8
Carlos <carlos_dic1985@hotmail.com>
2021-12-01 23:43:08 +0100

    navbar lista

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+
+    return (
+        <nav>
+            <h1>WikiCountries</h1>
+        </nav>
+    )
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(
+
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
+)
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );
+
+// comment skipped to stay organized
 reportWebVitals();
+
