0c722382ca818264915f427057e79d3283968b68
Hector Biminchumo <hectorbiminchumo@gmail.com>
2021-12-10 14:34:34 -0500

    lab-wiki-countries done

---
+++ src/Router.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Countries from './pages/Countries';
+
+import data from './countries.json';
+import CountryDetails from './pages/CountryDetails';
+
+const Router = () => {
+  const Pokemons = () => <h1>Pokemons</h1>;
+  const Home = () => <h1>Home</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route path="*" element={<h1>404</h1>}></Route>
+            <Route path="pokemons" element={<Pokemons />}></Route>
+            <Route index element={<Home />}></Route>
+            <Route path="countries" element={<Countries allCountries={data} />}>
+              <Route
+                path=":id"
+                element={<CountryDetails allCountries={data} />}
+              ></Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default Router;
