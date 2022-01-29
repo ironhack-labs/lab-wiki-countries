3e9bee1a32f1cb4ec7615ef96f457146a0ec8ed0
Michael Seitz <michael.s.seitz@gmail.com>
2022-01-29 09:44:34 +0100

    adds Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hi</h1>
+        <NavBar/>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="#">
+            LAB - WikiCountries
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
