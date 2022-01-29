5e4d2d7ee5fd96b0bdc7d13b071baa928d66297a
Michael Seitz <michael.s.seitz@gmail.com>
2022-01-29 13:20:59 +0100

    done with error

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import CountriesList from './components/CountriesList';
 import NavBar from './components/NavBar';
 import countriesJSON from './countries.json';
+import axios from 'axios';
+import React from 'react';
+
+const api = axios.create({
+  baseURL: 'https://ih-countries-api.herokuapp.com/countries',
+});
 
 function App() {
+  async function getCountries() {
+    try {
+      const { data } = await api.get('/');
+      setCountries(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   const [countries, setCountries] = useState(countriesJSON);
+  const [loading, setLoading] = useState(true);
+
+  React.useEffect(() => {
+    getCountries();
+    setLoading(false);
+  }, [countries]);
+
   return (
     <div className="App">
       <NavBar />
-      
+      {loading && <h1>Loading...</h1>}
       <CountriesList data={countries} />
     </div>
   );
