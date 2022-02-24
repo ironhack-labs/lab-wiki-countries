f9710534931eb7953e0c8cc07edfb1f06b8d58dd
denisesenguel <denisesenguel@gmail.com>
2022-02-24 20:20:18 +0100

    fix className typo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
     return(
         <nav className="navbar navbar-light bg-light">
             <div className="container-fluid">
-                <span class="navbar brand mb-0 h1">LAB - WikiCountries</span>
+                <span className="navbar brand mb-0 h1">LAB - WikiCountries</span>
             </div>
         </nav>
     )
