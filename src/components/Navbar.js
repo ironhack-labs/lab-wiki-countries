fc9942c4c2eb4a87ae97beada37635dd4cb6ef35
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-02-24 13:45:59 +0100

    Iteration 0 & 1.1

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link , NavLink} from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-dark bg-primary mb-3">
+      <div class="container">
+        <h3 class="navbar-brand"> WikiCountries </h3>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
