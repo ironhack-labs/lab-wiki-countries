043e947a18969ea39374f0f3789ea59cf7f9d6e9
denisesenguel <denisesenguel@gmail.com>
2022-02-24 20:08:45 +0100

    1.2 added navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+    return(
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <span class="navbar brand mb-0 h1">LAB - WikiCountries</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
