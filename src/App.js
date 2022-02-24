ecdbe940d376024ffd37212f59724ed0da508e4b
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-02-24 19:26:51 +0100

    Iteration 2 and 3

--- src/App.js
+++ src/App.js
@@ -6,9 +6,22 @@ import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 import countriesJSON from './countries.json';
 import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 function App() {
   const [countries, setCountries] = useState(countriesJSON);
+  const { countryId } = useParams();
+
+  useEffect(() => {
+    //get countries from the api server
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => {
+        setCountries(response.data);
+      });
+  }, [countryId]);
 
   return (
     <div className="App">
@@ -16,12 +29,11 @@ function App() {
 
       <div className="container ">
         <div className="row">
-          <CountriesList  countries={countries}/>
-        <Routes>
-          <Route path="/countries/:countryId" element={<CountryDetails />} />
-        </Routes>
+          <CountriesList countries={countries} />
+          <Routes>
+            <Route path="/countries/:countryId" element={<CountryDetails />} />
+          </Routes>
         </div>
-        
       </div>
     </div>
   );
