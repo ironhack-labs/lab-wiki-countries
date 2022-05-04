44d84bc1960849c1910711fd66ea2db8a49be2fe
Arturo Echanique <arturoechanique@gmail.com>
2022-05-04 22:52:46 +0200

    lab solved

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+import './CountriesList.css'
+
+
+function CountriesList({ countries, fetching }) {
+
+    return (
+
+        <div className="col-5" style={{maxHeight: "90vh", overflow: "scroll"}}>
+            <div className="list-group">
+                {fetching ? <h1>Cargando</h1> : countries.map((country, idx) => {
+                    return (
+                            <Link key={idx} className="list-group-item list-group-item-action countryButton" to={"/" + country.alpha3Code}>
+                                <img src={"https://flagpedia.net/data/flags/icon/72x54/" + country.alpha2Code?.toLowerCase() + ".png"} alt="" />
+                                <h3>{country.name.common}</h3>
+                                </Link>
+                    )
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default CountriesList;
