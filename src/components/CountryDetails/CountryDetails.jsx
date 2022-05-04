6c090231e0c7612ab13599c560caddf921ac9aad
Gabriela <s.gabriela.bv96@gmail.com>
2022-05-04 23:30:27 +0200

    bonus no terminado

--- src/components/CountryDetails/CountryDetails.jsx
+++ src/components/CountryDetails/CountryDetails.jsx
@@ -4,34 +4,33 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function CountryDetails({ countriesData }) {
- 
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const [country, setCountry] = useState({});
+  const [country, setCountry] = useState({});
 
-    useEffect(() => {
-      axios
-        .get(`https://ih-countries-api.herokuapp.com/countries/${id}`)
-          .then(({ data }) => {
-            console.log (data)
-          setCountry(data);
-        })
-        .catch((err) => console.log(err));
-    }, []);
-    
+  useEffect(() => {
+    axios
+      .get(`https://ih-countries-api.herokuapp.com/countries/${id}`)
+      .then(({ data }) => {
+        console.log(data);
+        setCountry(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
+  //     countriesData = { countriesData }
 
-
-//     countriesData = { countriesData }
-
-//   const [detailedCountry] = countriesData.filter(
-//     (country) => country.alpha3Code === id
-//   );
+  //   const [detailedCountry] = countriesData.filter(
+  //     (country) => country.alpha3Code === id
+  //   );
 
   return (
     <div>
       <h1>Detalles of {country.name.common}</h1>
-      <h3>{id}</h3>
+
+      <h3>capital:{country.capital} </h3>
+          <p>{country.area}</p>
+          
     </div>
   );
 }
