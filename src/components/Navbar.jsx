0297c72af577513335526fa8612422b85ba379ec
aBoswell <A.B0ZZW3LL@gmail.com>
2022-05-05 12:01:34 -0400

    iteration 1.1 added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar(){
+
+  return(
+    <nav className="navbar navbar-dark bg-primary md-3">
+      <div className="container">
+        <a className="navbar-brand" href="/">WikiCountries</a>
+      </div>
+    </nav>
+  )
+
+}
+
+export default Navbar;
+
