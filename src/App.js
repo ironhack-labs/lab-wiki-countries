40b7e18d8a0021a68861f5fadf81c6aa91987061
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-05-12 23:11:01 +0200

    solve iteration 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import countriesArr from './countries.json'
 import { Route, Routes } from 'react-router-dom';
 import CountryDetails from './components/CountryDetails';
+import axios from 'axios';
+
 
 function App() {
   const [countries, setCountries] = useState(countriesArr);
+  useEffect(() => {
+    axios.get("https://ih-countries-api.herokuapp.com/countries")
+    .then(response => {
+      setCountries(response.data)
+      console.log("Countries from array: ", response.data);
+    })
+    .catch( (e) => console.log("error getting elements from API.", e))
+
+  }, [])
 
   return (
     <div className="App">
--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 function CountriesList(props) {
     // console.log(props.countries[0]);
 
-    return(
+    return (
         <div>
             <h2>List of countries:</h2>
             {props.countries.map((country) => {
-                return(
+                return (
                     <div key={country.alpha3Code}>
-                    <h3>{country.name.official}</h3>
-                    <Link to={`/country/${country.alpha3Code}`}> + More Detais </Link>
+                        <h3>{country.name.official}</h3>
+                        <Link to={`/country/${country.alpha3Code}`}> + More Detais </Link>
                     </div>
                 )
             })}
