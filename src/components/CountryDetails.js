a49d87ab3804140b2a993fc48c6c1822296b955c
Sepideh Attar <sepideh.att@gmail.com>
2022-05-12 22:34:35 +0200

    solved issues

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,23 +1,36 @@
 
 import { NavLink, useParams } from "react-router-dom";
+import { useState,useEffect } from 'react'; 
 
 
 const CountryDetails = ({ countries }) => {
+  const [countrydet, setCountrydet] = useState(null)
   const { countryId } = useParams()
+useEffect(() => {
   const countryDetail = countries.find(country => country.alpha3Code == countryId)
+  countryDetail && setCountrydet(countryDetail);
+}, [countryId])
+
+
+  
 
 
   return (
     <div className="CountryDetails">
-    <br/>
-      <img src={`https://flagpedia.net/data/flags/icon/72x54/${countryDetail.alpha2Code.toLowerCase()}.png`} />
-      <h2>{countryDetail.name.common}</h2>
-      <h6>Capital : {countryDetail.capital[0]}</h6>
-      <p>Area : {countryDetail.area} km<sup>2</sup> </p>
-      <ul>Border : {countryDetail.borders.length > 0 ?
-        countryDetail.borders.map(bord => <p><NavLink to={`/${bord}`}>{bord}</NavLink></p>) :
+    {countrydet && 
+    <>
+      <br/>
+      <img src={`https://flagpedia.net/data/flags/icon/72x54/${countrydet.alpha2Code.toLowerCase()}.png`} />
+      <h2>{countrydet.name.common}</h2>
+      <h6>Capital : {countrydet.capital[0]}</h6>
+      <p>Area : {countrydet.area} km<sup>2</sup> </p>
+      <ul>Border : {countrydet.borders.length > 0 ?
+        countrydet.borders.map(bord => <p><NavLink to={`/${bord}`}>{bord}</NavLink></p>) :
         ''
       } </ul>
+      </>
+    }
+    
 
 
     </div>
