806541ed559872590227bc5dc3f006381f026ec4
jidekickpush <jeandenisdrane@gmail.com>
2022-05-13 07:17:01 +0200

    Fix CSS and style

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -6,27 +6,32 @@ import CountryDetails from "./CountryDetails"
 function CountriesList() {
     const [countriesList, setCountriesList] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         setCountriesList(countriesArr)
     }, [])
 
     return (
         <div className='container'>
-            <div className="col-5" >
-                {countriesList.map((country) => {
-                    return (
-                        <div key={country.alpha3Code} className="list-group">
-                            <NavLink to={`/${country.alpha3Code}`}>{country.name.official}</NavLink>
-                        </div>
-                    )
-                })}
+            <div className="row">
+                <div className="col-5" style={{ maxHeight: "90vh", overflow: "scroll" }} >
+                    <div className="list-group">
+                        {countriesList.map((country) => {
+                            return (
+                                <div key={country.alpha3Code} className="list-group-item list-group-item-action">
+                                    <img style={{ maxHeight: "5vh", objectFit: 'contain' }} src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={country.name.official} />
+                                    <br></br>
+                                    <NavLink to={`/${country.alpha3Code}`}>{country.name.official}</NavLink>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className="col-7">
+                    <Routes>
+                        <Route path="/:id" element={<CountryDetails countries={countriesArr} />} />
+                    </Routes>
+                </div>
             </div>
-            <div className="col-8">
-                <Routes>
-                    <Route path="/:id" element={<CountryDetails countries={countriesArr} />} />
-                </Routes>
-            </div>
-
         </div>
     )
 }
