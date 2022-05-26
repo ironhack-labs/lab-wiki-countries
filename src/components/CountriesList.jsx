2d8b0dd7d41d56f68db54734dcfe93edbcb26a49
Mariana Oliveira <marianalucas018@gmail.com>
2022-05-26 18:18:29 +0100

    lab done

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+function CountriesList() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => {
+        setCountries(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+
+  return (
+    <div className="row">
+    <div className="list-group">
+    
+      {countries.map((element) => {
+        return (
+          <div key={element.alpha3Code} style={{width:"500px"}} className="list-group-item list-group-item-action">
+         
+          
+
+            <Link to={`/${element.alpha3Code}`} >
+            <img src={`https://flagpedia.net/data/flags/icon/72x54/${element.alpha2Code.toLowerCase()}.png`} alt=""/>
+              <h2>{element.name.common}</h2>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+    </div>
+  );
+}
+
+export default CountriesList;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+     <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">WikiCountries</a>
+        </div>
+      </nav></div>
+  )
+}
+
+export default Navbar
