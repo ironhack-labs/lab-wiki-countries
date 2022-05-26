3e33f62dace9b7b6c750990fb4932c88f7dd87cc
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-05-26 20:03:51 +0100

    adds borders to detailspage

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -3,25 +3,30 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function CountriesList(props) {
-  const {countries} = props;
-
-  // const [countriesList, setCountriesList] = useState([]);
-
+  const { countries } = props;
+  
+  // const [sortedData, setSortedData] = useState([]);
   // useEffect(() => {
-  //   setCountriesList(props.countries);
-  // }, [props.countries]);
+  //   const sorted = [...countries].sort((a, b) => a.name.official - b.name.official);
+  //   console.log(sorted)
+  //   setSortedData(sorted);
+  // }, [countries]); 
+ 
 
+  
   return (
     <div className="container">
       <div className="row">
         <div className="col-5">
           <div className="list-group"></div>
-          <h2>Country List</h2>
+          <h1 className="py-3" >Country List</h1>
           {countries.map((country) => {
             return (
               <div key={country.alpha3Code}>
-                <Link to={`/${country.alpha3Code}`}>
-                  {country.name.official}
+                <Link to={`/${country.alpha3Code}`} className="py-4 list-group-item list-group-item-action">
+                  <div> <img width="150" src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} />
+                  </div>
+                  {country.name.common}
                 </Link>
               </div>
             );
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -2,58 +2,67 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function CountryDetails() {
- 
-
   const { countryId } = useParams();
-
   const [country, setCountry] = useState(null);
 
   useEffect(() => {
+    axios
+      .get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+      .then(
+        (response) => {
+          setCountry(response.data);
+        })
+       },
+        [countryId]
+      );
 
-    axios.get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
-      .then((response) => {
-        console.log(response.data)
-        setCountry(response.data);
-      }, [countryId])
-  })
-
-  
-  
   return (
-
     <div class="col-7">
       {!country && <h2>No country found!</h2>}
       {country && (
         <>
-            <h1>{country.name.common}</h1>
-            <table class="table">
-              <thead></thead>
-              <tbody>
-                <tr>
-                  <td>Capital</td>
-                  <td>{country.capital}</td>
-                </tr>
-                <tr>
-                  <td>Area</td>
+        <div> <img width="200" src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} />
+                  </div>
+          <h1 className="py-3">{country.name.common}</h1>
+          <table class="table">
+            <thead></thead>
+            <tbody>
+              <tr>
+                <td>Capital</td>
+                <td>{country.capital}</td>
+              </tr>
+              <tr>
+                <td>Area</td>
+                <td>
+                  {country.area} km2
+                </td>
+              </tr>
+              <tr>
+                <td>Borders</td>
+                {!country.borders && <h2>No borders</h2>}
+                {country.borders && (
                   <td>
-                  {country.area}
-                    <sup>2</sup>
+                    {country.borders.map((border) => {
+                      return (
+                        <div>
+
+                        <Link  to={`/${border}`}>{border}</Link>
+                        </div>
+                      );
+                    })}
                   </td>
-                </tr>
-                <tr>
-                  <td>Borders</td>
-                  <td>
-          
-                  </td>
-                </tr>
-              </tbody>
+                )}
+              </tr>
+            </tbody>
           </table>
-          </>
-      )};
+        </>
+      )}
+      ;
     </div>
-  )
+  );
 }
 
 export default CountryDetails;
