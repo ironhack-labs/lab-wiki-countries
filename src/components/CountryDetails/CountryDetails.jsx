3f046661a02e9976b5635d511fe5adb5e17a06a9
monica <monicasastudy@gmail.com>
2022-07-13 22:10:23 +0200

    [MAD DE 0522]Monica Sanchez

---
+++ src/components/CountryDetails/CountryDetails.jsx
@@ -0,0 +1,47 @@
+import { Link, useParams } from "react-router-dom"
+
+
+
+function CountryDetails() {
+
+    const { CountryDetails } = useParams()
+
+    return (
+        <div class="col-7">
+            <img src="https://restcountries.eu/data/fra.svg" alt="country flag" style="width: 300px" />
+            <h1>France</h1>
+            <table class="table">
+                <thead></thead>
+                <tbody>
+                    <tr>
+                        <td style="width: 30%">Capital</td>
+                        <td>Paris</td>
+                    </tr>
+                    <tr>
+                        <td>Area</td>
+                        <td>
+                            551695 km <sup>2</sup>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Borders</td>
+                        <td>
+                            <ul>
+                                <li><a href="/AND">Andorra</a></li>
+                                <li><a href="/BEL">Belgium</a></li>
+                                <li><a href="/DEU">Germany</a></li>
+                                <li><a href="/ITA">Italy</a></li>
+                                <li><a href="/MCO">Monaco</a></li>
+                                <li><a href="/ESP">Spain</a></li>
+                                <li><a href="/CHE">Switzerland</a></li>
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+
+}
+
+export default CountryDetails
