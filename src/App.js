706a4804380aee47ca10031406defefe91669c8e
antonio-datahack <antonio.datahack@gmail.com>
2022-07-16 16:54:03 +0200

    done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Navbar from "./components/Navbar";
 import CountriesList from "./components/CountriesList";
 import CountryDetails from "./components/CountryDetails";
 import { Routes, Route } from "react-router-dom";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Countries from './countries.json';
+import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.css';
 
 // function NotFound() {
@@ -12,18 +13,41 @@ import 'bootstrap/dist/css/bootstrap.css';
 // }
 
 function App() {
-  const [countries, setCountries] = useState([...Countries])
+  // const [countries, setCountries] = useState([...Countries])
+  const [states, setStates] = useState([])
+  // const [st, setSt] = useState('')
+
+  // const handleChange = e => {
+  //   setSt(e.target.value)
+  // }
+
+  useEffect(() => {
+    // fetch the data from the api
+    axios.get('https://ih-countries-api.herokuapp.com/countries/')
+      .then(response => {
+        console.log(response.data)
+
+        setStates(response.data)
+
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+
+
   return ( <div className="App">
     <Navbar />
+    {/* <input type="text" onChange={handleChange} value={st}/> */}
+
     <div className="container col-12 ">
       <div className="row d-flex justify-content-around">
         <div className="col-6">
-          <CountriesList countries={countries} />
+          <CountriesList countries={states} />
         </div>
         <div className="col-6">
           <Routes>
-            {/* <Route path='/' element={<CountriesList countries={countries} />} /> */}
-            <Route path='/:id' element={<CountryDetails countries={countries} /> }  />  
+            <Route path='/' />
+            <Route path='/:id' element={<CountryDetails countries={states} /> }  />  
             <Route path='*' element={<h1>404- Not Found </h1>} />
           </Routes>
         </div>
--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -14,7 +14,7 @@ export default function CountryDetails({countries}) {
     if (country.borders.length !== 0) {
       for (let border of country.borders) {
       let state = countries.find(country => country.alpha3Code === border)
-      estados.push(<p>{state.name.official}</p>)
+      estados.push(<li><a href={`/${state.alpha3Code}`}>{state.name.official}</a></li>)
       }
     }
     return estados
@@ -23,51 +23,25 @@ export default function CountryDetails({countries}) {
   return (
     <>
       <div className="container col-10" key={country.alpha3Code}>
-      <div className="row"> 
-      <h1>Country Details</h1>
-        <div className="card col-12 border-0">
-          <div className="row justify-content-md-center mt-2">
-            <div className="col-12">
-              <div className="list-group">
-                <img className="mx-auto d-block mt-2" src={`https://flagcdn.com/120x90/${country.alpha2Code.toLowerCase()}.png`} alt="flag" />
-                <ul className="list-group list-group-flush col-12">
-                <li className="list-group-item"><h3>{country.name.official}</h3></li>
-                  <li className="list-group-item">Capital: {country.capital} </li>
-                  <li className="list-group-item">Area: {country.area}km² </li>
-                  <li className="list-group-item">Borders: <span>{border()}</span> </li>
-                  
-                </ul>
+        <div className="row"> 
+        <h1>Country Details</h1>
+          <div className="card col-12 border-0">
+            <div className="row justify-content-md-center mt-2">
+              <div className="col-12">
+                <div className="list-group">
+                  <img className="mx-auto d-block mt-2" src={`https://flagcdn.com/128x96/${country.alpha2Code.toLowerCase()}.png`} alt="flag" />
+                  <ul className="list-group list-group-flush col-12">
+                  <li className="list-group-item"><h3>{country.name.official}</h3></li>
+                    <li className="list-group-item">Capital: {country.capital} </li>
+                    <li className="list-group-item">Area: {country.area}km² </li>
+                    <li className="list-group-item">Borders: <ul>{border()}</ul> </li> 
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-          </div>
         </div>
       </div>
-     
-
-
-
-      {/* <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="..."/>
-        <img className="card-img-top" src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="flag" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">An item</li>
-          <li class="list-group-item">A second item</li>
-          <li class="list-group-item">A third item</li>
-        </ul>
-        <div class="card-body">
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-      </div> */}
-
-
-
-
     </>
   )
 }
