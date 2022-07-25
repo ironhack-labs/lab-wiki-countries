7473b9b0159de6aedb7ce542b0d850dcf07d7d45
Randolph Roy <randolphroy@me.com>
2022-07-25 19:43:44 -0400

    RMT-WEBDEV-JUN2022-RANDOLPH ROY

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+import Navbar from './components/Navbar'; //iteration 1
+import { Routes, Route } from 'react-router-dom';//iteration 1
+//import countries from './countries.json';
+import CountriesList from './components/CountriesList';
+import {useState,useEffect} from 'react';
+import axios from 'axios';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
+  const [CountriesData, setCountriesData] = useState(null);
+
+  useEffect(() => {
+    let config = {
+      method: 'get', url: 'https://ih-countries-api.herokuapp.com/countries',
+    };
+
+    axios(config) //The same that fetch, obtain json data
+      .then((response) => {
+        setCountriesData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!CountriesData){
+    return <p>Loading pages</p>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+      <Navbar/> {/*iteration 1*/}
+    <div className="container"></div>
+        <div className="row">
+        {<CountriesList countriesData={CountriesData} />}
+          <Routes>
+            <Route path="/:id" element={<CountryDetails countriesData={CountriesData} />} />
+          </Routes>
+        </div>
+      </div>
   );
 }
-
 export default App;
