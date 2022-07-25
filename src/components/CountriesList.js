3b47bf166c619dbffdcebd8d03fc78806d1b0c5a
hleonn <edhleon2001@hotmail.com>
2022-07-24 21:19:47 -0500

    :)

---
+++ src/components/CountriesList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import CountryDetails from './CountryDetails';
+import { Routes, Route } from 'react-router-dom';
+import {useState,useEffect} from 'react';
+import axios from 'axios';
+
+const CountriesList = ({ countriesData }) => {
+    const linkCountry = countriesData.map((countries) => (
+      <Link to={`/${countries.alpha3Code}`} className="list-group-item list-group-item-action" key={countries.name.official}>
+        <img src={`https://flagpedia.net/data/flags/icon/72x54/${countries.alpha2Code.toLowerCase()}.png`}
+          alt="flag"/> {' '} {countries.name.common}
+      </Link>
+    ));
+    return(
+        <div className="col-5" style={{maxheight:'90vh', 'overflow': 'scroll'}}>
+        <div className="list-group">{linkCountry}</div>  
+        </div>
+    )
+}
+export default CountriesList;
---
+++ src/components/CountryDetails.js
@@ -0,0 +1,59 @@
+import React from 'react'; //imr snippets
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import countries from '../countries.json';
+
+
+
+
+
+const CountryDetails = ({ countriesData }) => {
+    const params = useParams();
+    const findCountry = countriesData.find((country) => country.alpha3Code === params.id
+    );
+  const {
+    name: { official },
+    capital,
+    area,
+    borders,
+  } = findCountry;
+  console.log(params);
+
+
+  const listBorder = borders.map(border => (
+    <li><Link to={`/${border}`} key={border}>{countriesData.find(
+    (country) => country.alpha3Code === border).name.official}</Link></li>
+  ))
+
+  return (
+    <div className="col-7">
+      <h1>{official}</h1>
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style={{ width: '30%' }}>Capital</td>
+            <td>{capital}</td>
+          </tr>
+          <tr>
+            <td>area</td>
+            <td>
+              {area}
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>borders</td>
+            <td>
+              <ul>
+              {listBorder}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CountryDetails;
