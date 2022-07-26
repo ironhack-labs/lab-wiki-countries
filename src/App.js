5fc7516d5d6b33854d7f39fb1b600e7a47309847
Kristen Wegner <klwegner@gmail.com>
2022-07-26 12:23:15 -0400

    this worked for the intial refresh and then wouldn't render individual countries again. As far a I know, all I changed was adding the country flag, which did render correctly before I refreshed the page and it crshed.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,45 @@ import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 import countriesData from './countries.json';
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+
 
 function App() {
+  {/* need to make get request to get info from api */}
+// hooks have to be outside of return
+
+const [countries, setCountries ] = useState(null);
+  
+useEffect(() => {
+    axios.get('https://ih-countries-api.herokuapp.com/countries')
+    .then(response => setCountries(response.data))
+    .catch(err => console.log(err))
+    }, []);
+
   return (
     <div className="App">
+
+{/*     
+    // fetch('https://ih-countries-api.herokuapp.com/countries', {'method': 'GET'})
+    // .then(res => res.json())
+    // .then(res => { */}
+    {/* //   this.setState({ */}
+    {/* //     countries: res */}
+    {/* //   }) */}
+    {/* // }) */}
+    {/* // .catch(err => console.log(err)) */}
+    
       <Navbar />
 {/* 
       <CountriesList countries={countriesData} /> */}
 
       <Routes>
-      <Route path="/" element ={<CountriesList countries ={countriesData} />} />
+      <Route path="/" element ={<CountriesList countries={countries} />} />
         <Route
           path="/:countryId"
-          element={<CountryDetails countries ={countriesData} />}
+          element={<CountryDetails countries={countries} />}
         />
       </Routes>
     </div>
--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,75 +1,78 @@
-import countryData from "../countries.json";
+
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 
 function CountryDetails(props) {
-    // const [ countryDetails, setCountryDetails] = useState(countryData);
-    // const [countries, setCountries] = useState(countryData);
-   const { countries } = props;
-   const { countryId } = useParams();
+  const { countries } = props;
+  const { countryId } = useParams();
+  const [foundCountry, setFoundCountry] = useState(null);
 
-   const [foundCountry, setFoundCountry] = useState(null);
+  useEffect(() => {
+    console.log('useEffect triggered');
+    const country = countries.find((countryObj) => {
+      return countryObj.alpha3Code === countryId;
+    });
 
-useEffect(()=>{
-    console.log('useEffect triggered')
-const country = countries.find((countryObj)=>{
-    return countryObj.alpha3Code === countryId;
-})
+    console.log(country, countryId);
 
-console.log(country, countryId);
+    if (country) {
+      setFoundCountry(country);
+    }
+  }, [countryId, countries]);
+
+  console.log(foundCountry);
+
+  let twoDigitCode = foundCountry.alpha2Code.toLowerCase();
+  console.log(twoDigitCode)
+
+  return (
+
+    <>
+      {!foundCountry && <h3>Country not found!</h3>}
+
+      {foundCountry && (
+        <div class="col-7" >
+
+          {/* below is for flag image-- worked once on refresh and then the details crashed repeatedly */}
+
+          <img src={`https://flagpedia.net/data/flags/icon/72x54/${twoDigitCode}.png`} alt="" />
 
 
-if (country) {
-    setFoundCountry(country);
-}
-}, [countryId, countries]);
-
-
-console.log(foundCountry)
-
-    return (
-<>
-{!foundCountry && <h3>Country not found!</h3>}
-
-{foundCountry && (
-
-<div class="col-7">
-            <h1>{foundCountry.name.common}</h1>
-            <table class="table">
-              <thead></thead>
-               <tbody>
-                <tr>
-                  <td style={{width: '30%'}}>Capitol</td>
-                  <td>{foundCountry.capital}</td>
-                </tr>
+          <h1>{foundCountry.name.common}</h1>
+          <table class="table">
+            <thead></thead>
+            <tbody>
               <tr>
-                  <td>Area</td>
-                  <td>
-                    {foundCountry.area} km
-                    <sup>2</sup>
-                  </td>
-                </tr>
-                 <tr>
-                  <td>Borders</td>
-                   <td>
-                    <ul>
-        
- {foundCountry.borders.map((nation) => {
-    return (
-        <li><Link to={`/${nation}`}>{nation}</Link></li>
-        )}
-        )}
-       
-                    </ul>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-)}
-</>
-          )
-
+                <td style={{ width: '30%' }}>Capitol</td>
+                <td>{foundCountry.capital}</td>
+              </tr>
+              <tr>
+                <td>Area</td>
+                <td>
+                  {foundCountry.area} km
+                  <sup>2</sup>
+                </td>
+              </tr>
+              <tr>
+                <td>Borders</td>
+                <td>
+                  <ul>
+                    {foundCountry.borders.map((nation) => {
+                      return (
+                        <li>
+                          <Link to={`/${nation}`}>{nation}</Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default CountryDetails;
