928f27f2b1b7ed9573366558e506fbfd04d26944
Vale Quiroga <valentinqramos@outlook.com>
2022-07-31 21:52:15 -0500

    Iteration 1.2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import CountryList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className='testContainer'>
+      <div className='testList' >
+        <CountryList />
+      </div>
+
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<CountryDetails />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">Wiki-Countries</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
