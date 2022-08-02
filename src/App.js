a8986c8d80da7295dbfdd4162a270166906b21f0
Vale Quiroga <valentinqramos@outlook.com>
2022-08-01 22:51:37 -0500

    Starting over

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
-import CountryList from './components/CountriesList';
-import CountryDetails from './components/CountryDetails';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className='testContainer'>
-      <div className='testList' >
-        <CountryList />
-      </div>
-
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<CountryDetails />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/components/CountryDetails.jsx
+++
@@ -1,9 +0,0 @@
-const CountryDetails = () => {
-  return (
-    <div className="testDiv">
-      <h2>Detalles de </h2>
-    </div>
-  );
-}
-
-export default CountryDetails;
