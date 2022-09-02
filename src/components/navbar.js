21a0cbc9610db6a2ab41dcd8ac84f503c5cde0dc
cra123 <onixmohamed@yahoo.fr>
2022-09-02 08:51:10 +0300

    done

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+
+function Navbar (){
+    
+    return(
+        // Navbar
+        <nav class="navbar navbar-dark bg-primary mb-3">
+            <div class="container">
+                <a class="navbar-brand" href="/">WikiCountries</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar
