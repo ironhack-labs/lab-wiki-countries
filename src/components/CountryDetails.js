8a60cac342c81ac35d838eb0f395a9fa8fd83586
Shania <shaniaantezana@gmail.com>
2022-09-10 14:41:10 -0400

    implements correct borders country details

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,32 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import countriesData from '../countries.json'
-
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import countriesData from '../countries.json';
 
 const CountryDetails = () => {
-
   const { id } = useParams();
 
   const [countries, setCountries] = useState(countriesData);
   const [country, setCountry] = useState({});
-  const [loading, setLoading] = useState(true)
-
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if(id) {
-      const country = countries.find(country => country.alpha3Code === id)
+    if (id) {
+      const country = countries.find((country) => country.alpha3Code === id);
       setCountry(country);
-      setLoading(false)
+      setLoading(false);
     }
   }, [id]);
-  
-  return ( !loading ? <div className="col-7">
-      < h1 >{country.name.official}</h1 >
+
+  return !loading ? (
+    <div className="col-7">
+      <h1>{country.name.official}</h1>
       <table className="table">
         <thead></thead>
         <tbody>
           <tr>
-            <td style={{width: '30%'}}>Capital</td>
+            <td style={{ width: '30%' }}>Capital</td>
             <td>{country.capital[0]}</td>
           </tr>
           <tr>
@@ -40,23 +38,28 @@ const CountryDetails = () => {
             <td>Borders</td>
             <td>
               <ul>
-                <li><a href="/AND">Andorra</a></li>
-                <li><a href="/BEL">Belgium</a></li>
-                <li><a href="/DEU">Germany</a></li>
-                <li><a href="/ITA">Italy</a></li>
-                <li><a href="/LUX">Luxembourg</a></li>
-                <li><a href="/MCO">Monaco</a></li>
-                <li><a href="/ESP">Spain</a></li>
-                <li><a href="/CHE">Switzerland</a></li>
+                {country.borders.map((border, index) => {
+                  return (
+                    <li key={index}>
+                      <Link to={`/${border}`}>
+                        {
+                          countries.find(
+                            (country) => country.alpha3Code === border
+                          ).name.official
+                        }
+                      </Link>
+                    </li>
+                  );
+                })}
               </ul>
             </td>
           </tr>
         </tbody>
       </table>
-    </div > : <div>Erro</div>)
+    </div>
+  ) : (
+    <div>Erro</div>
+  );
+};
 
-
-
-}
-
-export default CountryDetails
+export default CountryDetails;
