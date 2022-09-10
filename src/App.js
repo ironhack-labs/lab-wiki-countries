8e0d813620e235530ef9f71f7ec811ccc84dd769
Anneliese Kappey <anneliesekappey@gmail.com>
2022-09-10 11:22:01 -0300

    create-component-navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+const NavBar = (props) => {
+  return (
+    <div>
+      <h1>WikiCountries</h1>
+    </div>
+  );
+};
+
+export default NavBar;
