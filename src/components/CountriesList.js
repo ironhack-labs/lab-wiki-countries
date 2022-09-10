9f46be995d1be4c218a2cece3bb3d46b056163bc
Deny Nakano <deny.nakano@gmail.com>
2022-09-10 14:01:47 -0300

    implements bootstrap on class list component

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -7,15 +7,22 @@ const CountriesList = (props) => {
   const [countries, setCountries] = useState(countriesData);
 
   return (
-    <div>
-      <ul>
-        {countries.map((country) => {
-          return (
-            <Link key={country.alpha3Code} to={`/${country.alpha3Code}`}>{country.name.common}</Link>
-          );
-        })}
-      </ul>
+    <div className="container">
+
+      <div className="row">
+
+        <div className="col-5" style={{maxHeight: "90vh", overflow: "scroll"}}>
+          <div className="list-group">
+            {countries.map((country) => {
+              return (
+                <Link className="list-group-item list-group-item-action" key={country.alpha3Code} to={`/${country.alpha3Code}`}>{country.name.common}</Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 };
 
