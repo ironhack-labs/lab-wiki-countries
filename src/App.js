c69b36094364b238e19f85b45171a860d2a561b5
unknown <Jossuha@hotmail.fr>
2022-09-11 11:56:01 +0200

    Refacto, commentaires et Iteration 3 OK

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+
 import CountriesList from './components/CountriesList';
-import NavBar from './components/NavBar';
-import { useParams,Routes,Route } from 'react-router-dom';
 import CountryDetails from './components/CountryDetails';
+import NavBar from './components/NavBar';
 
-
-
+import './App.css';
+import json from './countries.json'
 
 function App() {
-  //const {param}=useParams();
+  const [countries, setCountries] = useState(json)
+
+  useEffect(() => { // Recupère les informations des pays et ne repete pas
+    axios.get("https://ih-countries-api.herokuapp.com/countries")
+    .then(request => setCountries(request.data))
+    .catch(err => console.log("Erreur axios app.js",err))
+  }, [])
   
   return (
     <div className="App">
-    <NavBar/>
-    <CountriesList/>
+      <NavBar />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-5" style={{ maxHeight: '90vh', overflow: 'scroll' }}>
+            <div className="list-group">
+              <CountriesList countries={countries} /> {/* Affiche la liste de tous les pays */}
+            </div>
+          </div>
+          <Routes>
+            <Route path="/:id" element={ <CountryDetails /> } /> {/* Affiche les details du pays cliqué */}
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,65 +1,17 @@
-import { Link, Route, Routes} from 'react-router-dom';
-import CountryDetails from './CountryDetails';
+import { Link } from 'react-router-dom';
 
-function CountriesList() {
+function CountriesList(props) {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-5" style={{ maxHeight: '90vh', overflow: 'scroll' }}>
-          <div className="list-group">
-            <Link className="list-group-item list-group-item-action" to="/ABW">
-              🇦🇼 Aruba
+    <div>
+      {
+        props.countries.map(el => {
+          return (
+            <Link className="list-group-item list-group-item-action" to={`/${el.alpha3Code}`} key={el.alpha3Code}>
+              <img src={`https://flagpedia.net/data/flags/icon/72x54/${el.alpha2Code.toLowerCase()}.png`} /> {el.name.common}
             </Link>
-            <Link className="list-group-item list-group-item-action" to="/AFG">
-              🇦🇫 Afghanistan
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/AGO">
-              🇦🇴 Angola
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/AIA">
-              🇦🇮 Anguilla
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/ALA">
-              🇦🇽 Åland Islands
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/ALB">
-              🇦🇱 Albania
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/AND">
-              🇦🇩 Andorra
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/ARE">
-              🇦🇪 United Arab Emirates
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/ARG">
-              🇦🇷 Argentina
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/ARM">
-              🇦🇲 Armenia
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/ASM">
-              🇦🇸 American Samoa
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/ATA">
-              🇦🇶 Antarctica
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/FLK">
-              🇫🇰 Falkland Islands
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/FRA">
-              🇫🇷 France
-            </Link>
-            <Link className="list-group-item list-group-item-action" to="/ZWE">
-              🇿🇼 Zimbabwe
-            </Link>
-          </div>
-        </div>
-        
-        <Routes>
-          <Route path="/:id" element={ <CountryDetails /> } />
-        </Routes>
-       
-      </div>
+          )
+        })
+      }
     </div>
   );
 }
