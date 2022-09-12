fd0b3fe99ffc6cdf4e7b7f4162d75d64a271ce76
Chlotte <charlottealleaume99@gmail.com>
2022-09-12 11:21:01 +0200

    finished lab - forgot to push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+
+import countriesData from './countries.json';
+
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
+  const [countries, setCountries] = useState(countriesData);
+
+  useEffect(() => {
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries')
+      .then(response => {
+        if (response.data)
+          setCountries(response.data);
+      });
+  }, []);
+
+  countries.sort((a, b) => a.name.common > b.name.common);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      <Routes>
+       <Route path="/" element={<CountriesList countries={countries} />} />
+       <Route path="/countries" element={<CountriesList countries={countries} />} />
+        <Route
+          path="/countries/:id"
+          element={<CountryDetails countries={countries} />}
+        />
+      </Routes>
+      <CountriesList countries={countries} />
     </div>
   );
 }
-
 export default App;
