045d1a41380695c3676df0045b40ac3f0b5fef65
Alan ROGER <aa.roger404@gmail.com>
2022-09-17 11:43:15 +0200

    done

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,54 +1,58 @@
-import { useParams } from 'react-router-dom';
-import {useEffect,useState} from 'react'
+import { useParams, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function CountryDetails(props) {
-  const { a3 } = useParams();
-  const myCountry = props.data.find((el) => el.alpha3Code.toLowerCase() === a3);
-  const tdStyle={width:"30%"}
-  const [country, setCountry]=useState(myCountry)
+  const [country, setCountry] = useState()
 
-  useEffect(()=>{
-    axios.get(`https://ih-countries-api.herokuapp.com/countries/${myCountry.alpha3Code}`)
-    .then((response)=>{
-      console.log("response:",response)
-      setCountry(response.data)
-      console.log("country",country)
-    })
-    .catch(err=>console.log("error getting contry details :",err))
-  },[country])
+  const { a3 } = useParams();
+  
+  const tdStyle = { width: "30%" }
+  
+
+  useEffect(() => {
+    axios.get(`https://ih-countries-api.herokuapp.com/countries/${a3.toUpperCase()}`)
+      .then((response) => {
+        console.log("response:", response)
+        setCountry(response.data)
+        console.log("country", response.data)
+      })
+      .catch(err => console.log("error getting contry details :", err))
+  }, [a3])
+
+  if (!country) return 'loading...'
 
   return (
     <div className="col-7">
-            <h1>{country.name.common}</h1>
-            <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="drapeau"/>
-            <table className="table">
-              <thead></thead>
-              <tbody>
-                <tr>
-                  <td style={tdStyle}>Capital</td>
-                  <td>{country.capital[0]}</td>
-                </tr>
-                <tr>
-                  <td>Area</td>
-                  <td>
-                  {country.area}km²
-                  </td>
-                </tr>
-                <tr>
-                  <td>Borders</td>
-                  <td>
-                    <ul>
-                      {country.borders.map(borderA3=>{
-                        const borderCountry=props.data.find((el)=>{ return el.alpha3Code===borderA3})
-                        return (<li key={borderA3}><a href={`/${borderA3.toLowerCase()}`}>{borderCountry.name.common}</a></li>)
-                      })}
-                    </ul>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+      <h1>{country.name.common}</h1>
+      <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="drapeau" />
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style={tdStyle}>Capital</td>
+            <td>{country.capital[0]}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {country.area}km²
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                {country.borders.map(borderA3 => {
+                  const borderCountry = props.data.find((el) => { return el.alpha3Code === borderA3 })
+                  return (<li key={borderA3}><Link to={`/${borderA3.toLowerCase()}`}>{borderCountry.name.common}</Link></li>)
+                })}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
