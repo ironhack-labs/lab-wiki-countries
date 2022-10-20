b31d9da6789cc6665e58691ce6b0a24212039d2b
VKhodyrev <vkhodyrev1@gmail.com>
2022-10-20 23:11:09 +0300

    iteration 3 did not finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import "./App.css";
-import {Routes, Route, Link, useParams} from "react-router-dom";
+import { useState } from "react";
+import { Routes, Route, Link, useParams } from "react-router-dom";
 import CountryDetails from "./components/CountryDetails";
 import CountriesList from "./components/CountriesList";
 import Navbar from "./components/Navbar";
+import countriesJson from "./countries.json";
 
 
 
 function App() {
 
+  const [countriesArr, setCountries] = useState(countriesJson);
+  //console.log(countries);
 
   return (
-  <div className="App">
-   <Navbar/>
-    <Routes>
-        <Route path='/' element={<CountriesList/>} />
-        <Route path='/:countryCode' element={<CountryDetails/>} />
-    </Routes>
-
-  </div>
-);
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <CountriesList />
+          <Routes>
+            <Route path='/:countryCode' element={<CountryDetails country={country}/>} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default App;
--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,15 +1,46 @@
 import { useState } from "react";
-import {Routes, Route, Link, useParams} from "react-router-dom";
+import { Routes, Route, Link, useParams } from "react-router-dom";
 
-function CountryDetails({code}) {
-    let {countryCode} = useParams();
-    console.log(code);
+function CountryDetails({ country }) {
+    let { countryCode } = useParams();
+    
 
 
-    return(
-        <div>
-            <h3>Name: </h3>
-            <h3>Code: {countryCode}</h3>
+    return (
+        <div className="col-7">
+            <h1>{country.name.official}</h1>
+            <table className="table">
+                <thead></thead>
+                <tbody>
+                    <tr>
+                        {/* <td style="width: 30%">Capital</td> */}
+                        <td>Capital</td>
+                        <td>Paris</td>
+                    </tr>
+                    <tr>
+                        <td>Area</td>
+                        <td>
+                            551695 km
+                            <sup>2</sup>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Borders</td>
+                        <td>
+                            <ul>
+                                <li><a href="/AND">Andorra</a></li>
+                                <li><a href="/BEL">Belgium</a></li>
+                                <li><a href="/DEU">Germany</a></li>
+                                <li><a href="/ITA">Italy</a></li>
+                                <li><a href="/LUX">Luxembourg</a></li>
+                                <li><a href="/MCO">Monaco</a></li>
+                                <li><a href="/ESP">Spain</a></li>
+                                <li><a href="/CHE">Switzerland</a></li>
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
 
     )
