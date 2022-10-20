bece2086890cc63d32afd90035f2dc08ed968cc0
Julia Spriggs <ironhack@julia-spriggs.fr>
2022-10-20 20:21:25 +0200

    solved part of iteration 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 // src/App.js
 import "./App.css";
+//import axios from "axios";
+import {Routes, Route} from 'react-router-dom';
+import { useState } from 'react';
+import CountryDetails from "./components/CountryDetails";
+import CountriesList from "./components/CountriesList";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  const [countries, setCountries] = useState([]);
+
+  return (
+  <div className="App">
+  <Navbar />
+  <CountriesList countries={countries} />
+  {/*<CountryDetails country={countryId} />*/}
+  <Routes>
+  {/*  <Route path='/' element={<CountriesList countries={countries} />} />*/}
+    <Route path='/countries/:countryId' element={<CountryDetails />} />
+  </Routes>
+  </div>
+  )
 }
 export default App;
