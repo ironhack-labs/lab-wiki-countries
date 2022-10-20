0b9fe8360809549ff263b17c6d084447aed07991
Gabriel Gomez <ggomez.box@gmail.com>
2022-10-20 20:54:50 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import {Routes, Route, Link} from "react-router-dom";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import CountriesList from './components/CountriesList';
 import Navbar from './components/Navbar';
 
 function App() {
-  const [countriesArr, setCountriesArr]= useState([]);
+  const [countriesArr, setCountriesArr] = useState([]);
 
-  useEffect(()=> {
-    axios.get("https://ih-countries-api.herokuapp.com/countries")
-    
-    .then(response => {
-      console.clear()
-      console.log("response.data", response.data)
-      setCountriesArr(response.data)
-    })
-    .catch.catch(e => console.log("error getting countries", e));
+  useEffect(() => {
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => {
+        setCountriesArr(response.data);
+      })
+      .catch((e) => console.log('error getting countries', e));
   }, []);
 
   return (
     <div className="App">
-    <Navbar/>
-    <CountriesList countries={countriesArr}/>
-    <Routes>
-      <Route path="/"element={<CountriesList characters={countriesArr} />} />
-     </Routes>
+      <Navbar />
+      <CountriesList countries={countriesArr} />
+      {/* <Routes>
+        <Route path="/" element={<CountriesList characters={countriesArr} />} />
+      </Routes> */}
     </div>
   );
 }
--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,23 +1,21 @@
-import {Routes, Route, Link} from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom';
 
-function CountriesList (props) {
-    console.log("countries error", props)
-    return(
-        <div>
-            <h2>List of countries:</h2>
-            
-            {props.countries.map((country)=>{
-                
-                return (
-                    <div className="country">
-                        <h2>{country.name.official}</h2>
-                        <Link to={`/countries/${country.id}`}>Country details</Link>
-                        </div>
-                )
-            })}
+function CountriesList(props) {
+  console.log('countries error', props);
+  return (
+    <div>
+      <h2>List of countries:</h2>
 
-        </div>
-    )
+      {props.countries.map((country) => {
+        return (
+          <div className="country">
+            <h2>{country.name.official}</h2>
+            <Link to={`/countries/${country.id}`}>Country details</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default CountriesList;
