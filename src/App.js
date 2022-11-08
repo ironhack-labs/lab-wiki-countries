7970afde7488732fc5a0001ad67eb3009fd871cf
hvkst <bernd@hvkst.de>
2022-11-08 21:27:39 +0100

    Iteration I.II

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar navbar-expand-lg bg-secondary ">
+      <div className="container-fluid">
+        <Link className="navbar-brand text-light">LAB - WikiCountries</Link>
+      </div>
+    </nav>
+  );
 }
 export default Navbar;
