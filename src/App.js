64c510a43f518c0d3d2eca53549b6d41bbbf3319
hvkst <bernd@hvkst.de>
2022-11-08 23:52:58 +0100

    Iteration I.III

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import CountriesList from './components/CountriesList';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <CountriesList />
+    </>
   );
 }
 export default App;
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,4 +1,51 @@
+// import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 function CountriesList() {
-  return <div>CountriesList</div>;
+  const [countries, setCountries] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchSomething() {
+      const apiURL = 'https://ih-countries-api.herokuapp.com/countries';
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      console.log(data);
+      setCountries(data);
+      setLoading(false);
+    }
+    fetchSomething();
+  }, []);
+
+  return (
+    <>
+      {loading && <h1>We´re loading!</h1>}
+      {!loading && (
+        <div
+          className="col-5"
+          style={{ maxHeight: '94vh', overflow: 'scroll' }}
+        >
+          <div className="list-group countries-list">
+            {countries
+              .sort((a, b) => a.alpha2Code.localeCompare(b.alpha2Code))
+              .map((country) => {
+                return (
+                  <li key={country._id}>
+                    <Link
+                      className="list-group-item list-group-item-action "
+                      to={country.alpha3Code}
+                    >
+                      {country.alpha2Code} {country.name.official}
+                    </Link>
+                  </li>
+                );
+              })}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 export default CountriesList;
