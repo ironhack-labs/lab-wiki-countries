269f150a024fbaca1b8633b934019d2a5d37b485
hvkst <bernd@hvkst.de>
2022-11-10 18:12:07 +0100

    Iteration IV

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,45 +1,21 @@
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
-function CountryDetails({ countries, loading }) {
-  // const [foundCountry, setFoundCountry] = useState(null);
-  // const [selected, setSelected] = useState(false);
+function CountryDetails({ countries }) {
+  const [country, setCountry] = useState(null);
+  const [loading, setLoading] = useState(true);
   const { countryCode } = useParams();
 
-  // let selectedCountry;
-  // if (!loading && !selected) {
-  //   selectedCountry = countries.find((country) => {
-  //     return country.alpha3Code === countryCode;
-  //   });
-  //   setFoundCountry(selectedCountry);
-  //   setSelected(true);
-  // }
-
-  // // Have to use useEffect => if countryCode (params) changes, then find the country and set state...
-
-  // useEffect(() => {
-  //   // this has to go 3 lines up, outside of the effect, maybe...
-  //   if (!loading) {
-  //     const country = countries.find((country) => {
-  //       return country.alpha3Code === countryCode;
-  //     });
-
-  //     // if (country) {
-  //     setFoundCountry(country);
-  //     // }
-  //     console.log(foundCountry);
-  //   }
-  // }, [countries, countryCode, loading]);
-
-  // const selectedCountry = () => {
-  //   return countries.find((country) => {
-  //     return country.alpha3Code === countryCode;
-  //   });
-  // };
-
-  const selectedCountry = countries.find((country) => {
-    return country.alpha3Code === countryCode;
-  });
+  useEffect(() => {
+    async function fetchCountry() {
+      const apiURL = `https://ih-countries-api.herokuapp.com/countries/${countryCode}`;
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      setCountry(data);
+      setLoading(false);
+    }
+    fetchCountry();
+  }, [countryCode]);
 
   const giveBackCountryName = (alpha3) => {
     return countries.find((country) => {
@@ -48,45 +24,49 @@ function CountryDetails({ countries, loading }) {
   };
 
   return (
-    <div
-      className="card"
-      style={{
-        width: '20rem',
-        height: 'fit-content',
-        margin: 'auto',
-        marginTop: '20px',
-      }}
-    >
-      <img
-        src={`https://flagpedia.net/data/flags/icon/144x108/${selectedCountry.alpha2Code.toLowerCase()}.png`}
-        alt={selectedCountry.name.official}
-        style={{ width: '80%', margin: 'auto', marginTop: '20px' }}
-      />{' '}
-      <div className="card-body">
-        <h2 style={{ textAlign: 'center' }} className="card-title">
-          {selectedCountry.name.common}
-        </h2>
-        <p className="card-text">Continent {selectedCountry.region}</p>
-        <hr />
-        <p>Capital {selectedCountry.capital[0]}</p>
-        <hr />
-        <p>Area {selectedCountry.area} km²</p>
-        {selectedCountry.borders.length !== 0 && (
-          <>
+    <>
+      {!loading && (
+        <div
+          className="card"
+          style={{
+            width: '20rem',
+            height: 'fit-content',
+            margin: 'auto',
+            marginTop: '20px',
+          }}
+        >
+          <img
+            src={`https://flagpedia.net/data/flags/icon/144x108/${country.alpha2Code.toLowerCase()}.png`}
+            alt={country.name.official}
+            style={{ width: '80%', margin: 'auto', marginTop: '20px' }}
+          />
+          <div className="card-body">
+            <h2 style={{ textAlign: 'center' }} className="card-title">
+              {country.name.common}
+            </h2>
+            <p className="card-text">Continent {country.region}</p>
             <hr />
-            <p>Borders</p>
-            <ul>
-              {selectedCountry.borders.map((border) => (
-                <li key={border}>
-                  <Link to={border}>{giveBackCountryName(border)}</Link>
-                </li>
-              ))}
-            </ul>
+            <p>Capital {country.capital[0]}</p>
             <hr />
-          </>
-        )}
-      </div>
-    </div>
+            <p>Area {country.area} km²</p>
+            {country.borders.length !== 0 && (
+              <>
+                <hr />
+                <p>Borders</p>
+                <ul>
+                  {country.borders.map((border) => (
+                    <li key={border}>
+                      <Link to={border}>{giveBackCountryName(border)}</Link>
+                    </li>
+                  ))}
+                </ul>
+                <hr />
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 export default CountryDetails;
