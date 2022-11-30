f215c3f5acf8dcd9dbaf5c5e2c2cdc9be34b2093
PedroSG00 <suarezgallardopedro@gmail.com>
2022-12-01 00:19:13 +0100

    done bonus included

--- src/components/CountriesList/CountriesList.jsx
+++ src/components/CountriesList/CountriesList.jsx
@@ -4,16 +4,19 @@ import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 
 
-const CountriesList = ({ Countries }) => {
+const CountriesList = () => {
 
+    const countriesApi = 'https://ih-countries-api.herokuapp.com/countries'
 
     const [countryApi, setCountryApi] = useState([])
 
     useEffect(() => {
-        fetch('https://ih-countries-api.herokuapp.com/countries')
+        fetch(countriesApi)
             .then(res => res.json())
             .then(data => { setCountryApi(data) })
     }, [])
+
+
 
 
     return (
@@ -22,7 +25,7 @@ const CountriesList = ({ Countries }) => {
             <div className="row">
                 <ul className='col-md-6 list-group'>
                     {countryApi.map(({ name, alpha2Code, alpha3Code }, i) => {
-                        return < li key={i} > <Link to={`/${alpha3Code}`} className='list-group-item'>{`${alpha2Code} ${name.common}`}</Link> </li>
+                        return < li className='borderList' key={i} > <Link to={`/${alpha3Code}`} className='list-group-item'>{`${alpha2Code} ${name.common}`}</Link> </li>
                     })}
                 </ul>
             </div>
--- src/components/CountryDetails/CountryDetails.jsx
+++ src/components/CountryDetails/CountryDetails.jsx
@@ -1,31 +1,42 @@
 import './CountryDetails.css'
 import Countries from '../../countries.json'
 import { Link, useParams } from "react-router-dom"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const CountryDetails = () => {
 
-    const [country, setCountry] = useState(Countries)
+    // const [country, setCountry] = useState(Countries)
+    // const findCountry = country.find(elm => elm.alpha3Code === alpha3Code)
+
     const { alpha3Code } = useParams()
-    const findCountry = country.find(elm => elm.alpha3Code === alpha3Code)
+    const [countryDetails, setCountryDetails] = useState()
 
 
-    const [border, setBorder] = useState(findCountry.borders)
+
+    useEffect(() => {
+        fetch(`https://ih-countries-api.herokuapp.com/countries/${alpha3Code}`)
+            .then(res => res.json())
+            .then(data => { setCountryDetails(data) })
+    }, [`https://ih-countries-api.herokuapp.com/countries/${alpha3Code}`])
+
+    console.log(countryDetails)
+
+
 
 
     return (
         <div className='CountryDetails container'>
             <div className="row">
                 <div className="col-md-6 text-center">
-                    <h1>{findCountry.name.common}</h1>
+                    <img src={`https://flagpedia.net/data/flags/icon/72x54/${countryDetails?.alpha2Code.toLowerCase()}.png`} alt={`${countryDetails?.name.common} flag`} />
+                    <h1>{countryDetails?.name.common}</h1>
                     <ul className="list-group">
-                        <li className="list-group-item"><strong>Capital:</strong> {findCountry.capital}</li>
-                        <li className="list-group-item"><strong>Area:</strong> {findCountry.area}</li>
-                        <li className="list-group-item"><strong>Borders:</strong>
+                        <li className="list-group-item"><strong>Capital:</strong> {countryDetails?.capital}</li>
+                        <li className="list-group-item"><strong>Area:</strong> {countryDetails?.area}</li>
+                        <li className="list-group-item d-flex justify-content-center"><strong>Borders:</strong>
                             <ul>
-                                {findCountry.borders.map((elm, i) => {
-                                    return <li key={i}>{elm}</li>
-                                })}
+                                {countryDetails?.borders.length > 0 ? countryDetails?.borders.map((elm, i) => <li className='borderList' key={i}><Link to={`/${elm}`}>{elm}</Link></li>) : "There aren't borders in this country"}
+
                             </ul>
                         </li>
                     </ul>
