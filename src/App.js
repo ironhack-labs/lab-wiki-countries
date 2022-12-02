d6a20bd24523b60d760a0a899295c77e353cae31
khablo1508 <khablo.anna@gmail.com>
2022-12-02 10:44:08 +0100

    add style to countries list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,10 +1,9 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 function CountriesList({ countries }) {
   return (
-    <>
+    <Wrapper className="container">
       {countries.map((country) => {
         return (
           <div
@@ -17,17 +16,35 @@ function CountriesList({ countries }) {
             key={country.alpha3Code}
           >
             <div className="list-group">
-              <h3>
-                <Link className="list-group-item" to={`/${country.alpha3Code}`}>
-                  {country.name.common}
-                </Link>
-              </h3>
+              <div className="list-group-item   list-group-item-action">
+                <img
+                  src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                  alt="flag"
+                />
+                <h3>
+                  <Link to={`/${country.alpha3Code}`}>
+                    {country.name.common}
+                  </Link>
+                </h3>
+              </div>
             </div>
           </div>
         );
       })}
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  img {
+    width: 50%;
+    height: 85%;
+    margin: 0 auto;
+    margin-bottom: 10px;
+  }
+  .list-group-item {
+    margin-bottom: 15px;
+  }
+`;
 
 export default CountriesList;
