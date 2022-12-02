8a99cd0e3a6603897db7eac12683e84ee9828ebc
Ruyago <ruyago@gmail.com>
2022-12-02 09:40:42 +0100

    2.Iteration

---
+++ src/components/CountriesList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CountriesList = ({ countriesList }) => {
+
+  return (
+    <div>
+      <h1>Countries</h1>
+      {countriesList.map((countrie) => {
+        return (
+          <div key={countrie.alpha3Code} className="countrie">
+            <Link to={`/countries/${countrie.alpha3Code}`}>{countrie.name.official}</Link>
+        
+           
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CountriesList;
