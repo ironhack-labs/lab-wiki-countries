b83587f6eb9694910796bb22f2621db8db6561d6
Jo Huerta <santiago@alimx.mx>
2023-02-04 14:27:19 -0600

    WDFT-MEX-AGOSTO-JoseEzequielHuerta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavComp from './components/NavComp';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import { Outlet, Route, Routes } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+const apiURL = "https://ih-countries-api.herokuapp.com/countries";
+
+function Page() {
+  return (
+    <div>
+      <NavComp />
+      <div className="container">
+        <div className="row">
+          <Outlet />
+        </div> 
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get(apiURL)
+      .then((response) => {
+        setCountries(response.data);
+      }).catch((error) => {
+        console.error(error.message);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path={"/"} element={<Page />}>
+        <Route index element={<CountriesList countries={countries} />} />
+        <Route path={"/:countryId"} element={[<CountriesList countries={countries} />, <CountryDetails countries={countries} />]} />
+      </Route>
+    </Routes>
   );
 }
 
