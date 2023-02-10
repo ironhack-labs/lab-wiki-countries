1b1b335bb99a47be466b5659458506ef09f0c3a0
Cristian Gomez <gmz.mrs.cristian@gmail.com>
2023-02-10 12:42:44 -0600

    Lista la primer parte

---
+++ src/components/CountriesList/CountriesList.jsx
@@ -0,0 +1,19 @@
+import './CountriesList.css'
+import { Link } from 'react-router-dom'
+
+const CountriesList = ({name,alpha2Code,...restProps}) => {
+    return(
+        <Link to='/' className='link-card'>
+        <div className='container-card'>
+            <div id="scrollspyHeading1" className="card text-center mb-3">
+                <div className="card-body">
+                    <img src={`https://flagpedia.net/data/flags/icon/72x54/${alpha2Code.toLowerCase()}.png`} className="card-img-top" alt="flag"></img>
+                    <p>{name.official}</p>
+                </div>
+            </div>
+        </div>
+        </Link>
+    );
+};
+
+export default CountriesList;
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export {default as Navbar} from './Navbar/Navbar.jsx'
+export {default as CountriesList} from './CountriesList/CountriesList.jsx'
