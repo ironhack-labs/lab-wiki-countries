47ee735d89035a5ce02a0c4bc95d340a71708cb8
David Hatch <hatchdavidv@gmail.com>
2023-02-11 13:19:28 -0600

    2.1.2 Se hace el render la lista

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import countries from './countries.json'
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CountryList from './components/CountriesList';
+import { useState } from 'react';
 
 function App() {
+  const items = countries;
   return (
     <div className="App">
        <Navbar/>
-         <CountryList/>
+       <CountryList items = {items}/>
+        
     </div>
   );
 }
--- src/components/CountriesList/index.jsx
+++ src/components/CountriesList/index.jsx
@@ -1,16 +1,29 @@
 import './styles.css'
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function CountryList({props}){
+function CountryList({items}){
+    
     return (
-        <ul className='country-list'>
-            {props.map((prop, index)=>(
-                <li className='country-item' key ={index}>
-                    {prop}
-                </li>
-            ))}
-        </ul>
+        <div>
+            <ul>
+            <div className='country-list'>
+                {items.map((item, index)=>(
+                     <Link to={`/${item.alpha3code}`}>
+                        <img 
+                        src={`https://flagpedia.net/data/flags/icon/72x54/${item.alpha2code}.png`} 
+                        alt={item.name.official} /> {item.name.official}
+                         
+                     </Link>
+             ))}
+            </div>
+            </ul>
+        </div>
     )
 }
 
 export default CountryList;
+
+
+
+
