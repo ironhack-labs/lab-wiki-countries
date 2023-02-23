6694ab3c87e979b6e26a5a84eb37e8424deffc4d
Ulkar Isayeva <ulkar.isayeva@gmail.com>
2023-02-23 17:46:59 +0100

    completed wikicounriest

---
+++ src/components/CountryDetails.js
@@ -0,0 +1,55 @@
+import { useParams, Link } from "react-router-dom";
+
+
+function CountryDetails(props) {
+  const { code } = useParams()
+
+  
+  const oneCountry = props.countries.find(country => {
+    return code === country.alpha3Code
+  })
+
+  return (
+    <div className="col-7">
+      <h1>{oneCountry.name.common}</h1>
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style={{width: "30%"}}>Capital</td>
+            <td>{oneCountry.capital[0]}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {oneCountry.area} km
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                {oneCountry.borders.map(border => {
+                  return <li key={border} ><Link to={`/countryDetails/${border}`}>{props.countries.find(country => country.alpha3Code === border).name.common}</Link></li>
+                })}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default CountryDetails;
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-primary mb-3">
+      <div className="container">
+      <Link className="navbar-brand" to="/">WikiCountries</Link>
+      {/* <a className="navbar-brand" href="/">WikiCountries</a> */}
+    </div>
+  </nav>
+  )
+}
+
+export default Navbar
+
+
