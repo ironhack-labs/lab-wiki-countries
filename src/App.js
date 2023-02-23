22afcbec1e2150787c7fdcb6978144ea27be3369
Pauline <sisal.dynamo-0r@icloud.com>
2023-02-23 16:06:08 +0100

    iteration 2.3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import countriesJSON from "./countries.json"
 import { useState } from "react"
 import Navbar from "./components/Navbar"
-import CountriesList from "./components/CountriesList";
+import CountriesList from "./components/CountriesList"
 
 function App() {
   const [countries, setCountries] = useState(countriesJSON)
 
   return <div className="App">
     <Navbar/>
+    <CountriesList countries={countries} />
 
 
     </div>;
--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,9 +1,24 @@
 import { useState } from "react";
 import { Link } from "react-router-dom"
 
-function CountriesList() {
+function CountriesList({countries}) {
     return ( 
-        <></>
+        <div className="container">
+        <div className="row">
+          <div className="col-5" style={{maxHeight: "90vh", overflow: "scroll"}}>
+            <div className="list-group">
+              {countries.map(country => {
+                return (
+                    <Link key={country.alpha3Code} to={`/${country.alpha3Code}`} 
+                    className="list-group-item list-group-item-action">
+                    <img style={{width: "5%"}}src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="flag icon"></img>
+                    {country.name.common}</Link>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+    </div>
      );
 }
 
