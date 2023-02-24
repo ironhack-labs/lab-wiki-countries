ce70e9660062b32d7b302061d84628cd7fbefec7
Pauline <sisal.dynamo-0r@icloud.com>
2023-02-24 10:40:46 +0100

    little change in CountryDetails (nothing functional)

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -8,12 +8,7 @@ function CountriesList({countries}) {
             <div className="list-group">
               {countries.map(country => {
                 return (
-                    <Link key={country.alpha3Code} to={`/countryDetails/${country.alpha3Code}`} 
-                    className="list-group-item list-group-item-action">
-                    <img style={{width: "5%"}}
-                    src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} 
-                    alt="flag icon"></img>
-                    {country.name.common}</Link>
+                    <Link key={country.alpha3Code} to={`/countryDetails/${country.alpha3Code}`} className="list-group-item list-group-item-action"><img style={{width: "5%"}}src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="flag icon"></img>{country.name.common}</Link>
                 )
               })}
             </div>
