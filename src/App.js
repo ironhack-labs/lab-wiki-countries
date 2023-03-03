004772e7385e38b0ef239fd4de07850991e5752c
AzizaHalder <aziza.halder@sap.com>
2023-03-03 14:00:01 +0100

    begin iteration 4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import './App.css';
-import countries from '../src/countries.json';
+// import countries from '../src/countries.json';
 import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import CountriesList from './components/CountriesList';
-import CountryDetails from './components/CountryDetails'
-const apiURL = ' https://ih-countries-api.herokuapp.com/countries'
+import CountryDetails from './components/CountryDetails';
+import { useState, useEffect } from 'react';
+
+const apiURL = ' https://ih-countries-api.herokuapp.com/countries';
 
 function App() {
+  const [allCountries, setAllCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch('apiURL');
+        const data = await response.json();
+        setAllCountries(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCountries();
+  }, []);
+
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Routes>
-        <Route path='/' element={<CountriesList></CountriesList>} />
-        <Route path="/country/:countryId" element={ <CountryDetails countries = {countries} /> } />
-        {/* <Route path="/country/:countryId" element={ <CountryDetails countries = {countries} /> } /> */}
-
-
-      </Routes>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <Routes>
+            <Route path="/" element={<CountriesList></CountriesList>} />
+            <Route
+              path="/country/:countryId"
+              element={<CountryDetails countries={countries} />}
+            />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
