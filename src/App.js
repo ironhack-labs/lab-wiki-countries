a57aea27cc63d86a665ec39c9185915b8297abe7
Carlos Sosa <csosa@simpletg.com>
2023-03-10 18:12:30 -0500

    Lab Solved with Bonuses

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import countriesJson from './countries.json';
+// import countriesJson from './countries.json';
 import './App.css';
 
-import { Routes, Route, useParams, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 
-function App() {
-  const [countries, setCountries] = useState(countriesJson);
+const fetchApi = async (countryCode = '') => {
+  const ApiUrl =
+    !countryCode || countryCode === ''
+      ? 'https://ih-countries-api.herokuapp.com/countries'
+      : `https://ih-countries-api.herokuapp.com/countries/${countryCode.toUpperCase()}`;
 
-  const findCountry = (countryCode) => {
-    console.log({ countryCode });
-    return countryCode
-      ? countries.find(
-          (country) =>
-            country.alpha3Code.toLowerCase() === countryCode.toLowerCase()
-        )
-      : false;
+  try {
+    const response = await fetch(ApiUrl);
+    if (response.status !== 200) return false;
+    let data = await response.json();
+    if (data && (!countryCode || countryCode === ''))
+      data = data.sort((a, b) =>
+        a.name.official.localeCompare(b.name.official)
+      );
+    else if (!data) return false;
+
+    return data;
+  } catch (err) {
+    console.log('ERROR FETCHING COUNTRIES FROM API', err);
+    return '';
+  }
+};
+
+const getCountryData = async (countryCode) => {
+  let theCountry = await fetchApi(countryCode);
+  if (!theCountry) return false;
+  else return theCountry;
+};
+
+function App() {
+  const [countries, setCountries] = useState([]);
+
+  const getCountryName = (countryCode) => {
+    if (countries.length) {
+      const data = countries.find(
+        (country) =>
+          country.alpha3Code.toLowerCase() === countryCode.toLowerCase()
+      );
+      console.log({ data });
+      return data.name.official;
+    }
   };
+
+  useEffect(() => {
+    const getCountiesList = async () => {
+      setCountries((await fetchApi()) || []);
+    };
+
+    getCountiesList();
+  }, []);
 
   return (
     <div className="App">
@@ -31,7 +69,11 @@ function App() {
             style={{ maxHeight: '90vh', overflow: 'scroll' }}
           >
             <div className="list-group">
-              <CountriesList countries={countries} />
+              {countries && countries.length ? (
+                <CountriesList countries={countries} />
+              ) : (
+                <div>No Countries Found</div>
+              )}
             </div>
           </div>
           <div className="col-7 d-flex align-items-center justify-content-center">
@@ -39,7 +81,12 @@ function App() {
               <Route path="/" element={''} />
               <Route
                 path="/country/:countryCode"
-                element={<CountryDetails country={findCountry} />}
+                element={
+                  <CountryDetails
+                    countryData={getCountryData}
+                    countryName={getCountryName}
+                  />
+                }
               />
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,50 +1,60 @@
 import { Link, useParams, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function CountryDetails(props) {
   const { countryCode } = useParams();
-  const theCountry = props.country(countryCode);
-  if (!theCountry) return <Navigate to="/" />;
+  const [theCountry, setTheCountry] = useState('');
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await props.countryData(countryCode);
+      setTheCountry(data);
+    };
+    getData();
+  }, [countryCode, props]);
 
   return (
-    <div
-      className="d-flex flex-column countryDetails"
-      style={{ maxWidth: '500px', minWidth: '400px', gap: '30px' }}
-    >
-      <div style={{ textAlign: 'center' }}>
-        <img
-          src={`https://flagpedia.net/data/flags/w1160/${theCountry.alpha2Code.toLowerCase()}.webp`}
-          style={{ height: '150px' }}
-          alt=""
-        />
-      </div>
-      <h1 style={{ textAlign: 'center' }}>{theCountry.name.official}</h1>
-      <div className="row">
-        <div className="col-4">Capital</div>
-        <div className="col-8">{theCountry.capital[0]}</div>
-      </div>
-      <div className="row">
-        <div className="col-4">Area</div>
-        <div className="col-8">
-          {theCountry.area.toLocaleString()} km <sup>2</sup>
+    theCountry ? (
+      <div
+        className="d-flex flex-column countryDetails"
+        style={{ maxWidth: '500px', minWidth: '400px', gap: '30px' }}
+      >
+        <div style={{ textAlign: 'center' }}>
+          <img
+            src={`https://flagpedia.net/data/flags/w1160/${theCountry.alpha2Code.toLowerCase()}.webp`}
+            style={{ height: '150px' }}
+            alt=""
+          />
+        </div>
+        <h1 style={{ textAlign: 'center' }}>{theCountry.name.official}</h1>
+        <div className="row">
+          <div className="col-4">Capital</div>
+          <div className="col-8">{theCountry.capital[0]}</div>
+        </div>
+        <div className="row">
+          <div className="col-4">Area</div>
+          <div className="col-8">
+            {theCountry.area.toLocaleString()} km <sup>2</sup>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4">Borders</div>
+          <div className="col-8">
+            {theCountry.borders && theCountry.borders.length
+              ? theCountry.borders.map((border) => {
+                  return (
+                    <div key={border.toLowerCase()}>
+                      <Link to={`/country/${border.toLowerCase()}`}>
+                        {props.countryName(border)}
+                      </Link>
+                    </div>
+                  );
+                })
+              : 'None'}
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col-4">Borders</div>
-        <div className="col-8">
-          {theCountry.borders && theCountry.borders.length
-            ? theCountry.borders.map((border) => {
-                return (
-                  <div key={border.toLowerCase()}>
-                    <Link to={`/country/${border.toLowerCase()}`}>
-                      {props.country(border).name.official}
-                    </Link>
-                  </div>
-                );
-              })
-            : 'None'}
-        </div>
-      </div>
-    </div>
+    ) : <div>Country not Found</div>
   );
 }
 
