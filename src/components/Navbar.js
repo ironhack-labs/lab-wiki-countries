8a8302d3acd4071bc67e6102ffb66ea960a68028
Natalia Nossack <nvcordeiro22@gmail.com>
2023-03-25 18:23:57 +0100

    Navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
-
+import { Link } from 'react-router-dom';
 function Navbar() {
     return (
 
       <nav className="navbar navbar-dark bg-primary mb-3">
         <div className="container">
-          <a className="navbar-brand" href="/">LAB-WikiCountries</a>
+          <Link className="navbar-brand" to="/">LAB-WikiCountries</Link>
         </div>
       </nav>
 );
