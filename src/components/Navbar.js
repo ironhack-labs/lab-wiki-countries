1e015ede0d82f9209c998815d5c02084ca421652
Ander Zulaika <anderzu85@gmail.com>
2023-04-04 20:20:19 +0200

    itertation 1-2

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+    return (
+        <div>
+            <nav className = 'navbar navbar-dark bg-dark'>
+                <a class="navbar-brand" href="/">Countries List</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
