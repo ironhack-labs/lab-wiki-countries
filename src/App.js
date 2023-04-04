50fac221e2ac25c641224eb34836a9a274a6a22e
Carlos Lopez <carlopex97@gmail.com>
2023-04-04 20:08:34 +0200

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './countries.json'
+import { useState } from 'react';
+
+ 
 
 function App() {
+
+  const [countries, setCountries] = useState(data);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
