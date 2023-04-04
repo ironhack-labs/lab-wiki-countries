ce60b702cd8fde31611271818b1f54866f43f60e
Sofía J <sofiajimglez@gmail.com>
2023-04-04 22:02:53 +0200

    feat: Iterations 2.1, 2.2 & 2.3 - displays navbar and coutries list components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-const countries = require('./countries.json');
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetail from './components/CountryDetail';
+import { useState } from 'react';
+import countriesDB from './countries.json';
 
 function App() {
+  const [countries, setCountries] = useState(countriesDB);
+
   return (
     <div className="App">
+      <Navbar />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-5" style={{maxHeight: '90vh', overflow: 'scroll', overflowX:' hidden'}} >
+            <div className="list-group">
+              <CountriesList countries={countries} />
+            </div>
+            <div class="col-7">
+              <Routes>
+                <Route to="/" element={<></>} />
+                <Route to="/:code" element={<CountryDetail />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
 
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-primary mb-3 navbar-text">
+    <div className="container">
+      <a className="navbar-brand">WikiCountries</a>
+    </div>
+  </nav>
+  )
+}
