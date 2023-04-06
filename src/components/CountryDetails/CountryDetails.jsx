05f3d1da16664be89cac577531aab53df5d9d22b
Victor Lopez <victorlpz2301@gmail.com>
2023-04-06 11:31:47 +0200

    feat: Iteration 3 Done

--- src/components/CountryDetails/CountryDetails.jsx
+++ src/components/CountryDetails/CountryDetails.jsx
@@ -1,26 +1,44 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 function CountryDetails({ countries }) {
 
   const { alpha3Code } = useParams()
 
+
   return (
     <div>
-      {countries.filter(country => country.alpha3Code.includes(alpha3Code)).map(country =>
-        <div className="card m-3" key={country.alpha3Code}>
-          <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} className="card-img-top" alt="countryPicture" />
-          <div className="card-body">
-            <h5 className="card-title">{country.name.common}</h5>
-            <p className="card-text">Capital  {country.capital}</p>
+      {countries
+        .filter(country => country.alpha3Code.includes(alpha3Code))
+        .map(country =>
+          <div className="card m-3" key={country.alpha3Code}>
+            <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} className="card-img-top p-3" alt="countryPicture" />
+              <h5 className="card-title">{country.name.common}</h5>
+              <div className='d-flex flex-row border-bottom'>
+                <p className='m-2'>Capital</p>
+                <p className="list-group-item m-2 ms-5">  {country.capital}</p>
+              </div>
+            <div className='border-bottom d-flex flex-row'>
+              <p className='m-2'>Area</p>
+              <p className="list-group-item m-2 ms-5">{country.area} km2</p>
+            </div>
+            <div className='d-flex flex-row'>
+              {country.borders.length > 0 && (<>
+                <div>
+                  <p className='m-2'>Borders</p>
+                </div>
+                <div className="list-group-item border-0 ms-5 mt-2">
+                  {countries.filter(country => country.borders.includes(alpha3Code))
+                  .map(borders => {
+                    return <Link to={`/${borders.alpha3Code}`}>
+                      <div className="list-group-item border-0">{borders.name.common}</div>
+                    </Link>
+                  })}
+                </div>
+              </>)}
+            </div>
           </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">Area {country.area}</li>
-            <li className="list-group-item">Borders </li>
-          </ul>
-        </div>
-      )
-      }
+        )}
     </div>
   )
 }
