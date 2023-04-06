0092b00abe92398f7ac5d5021f084a83162ea285
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-04-06 17:32:09 +0200

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
+import countries from "./countries.json";
+
+
 
 function App() {
+
+  const[countriesArr, setCountriesArr] = useState(null)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar/> 
+
     </div>
+    
   );
 }
 
