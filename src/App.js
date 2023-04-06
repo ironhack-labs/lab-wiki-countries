84ef1fc1e88b85c8d1860f4f379ddba7139a8387
denefi <denefi@posteo.de>
2023-04-06 17:27:30 +0200

    initial setup and it.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import countriesFromDb from './countries.json';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState(countriesFromDb);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
