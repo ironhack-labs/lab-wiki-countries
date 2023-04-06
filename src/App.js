4056e5e1bb52e5c8650c7f913c26f2357557dc68
Paulius Mockevicius <pmockevicius@gmail.com>
2023-04-06 17:23:24 +0200

    complete iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import { useState } from "react";
+import "./App.css";
+
+import countries from './countries.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const [countriesArr, setCountiesArr] = useState(countries)
+
+console.log(countriesArr)
+
+  return <div className="App"></div>;
+}
 export default App;
