6699f7c9c2501db7c49cc48b77e82b169d8213ac
Garin90 <alex.garrido.baldrich@gmail.com>
2023-04-08 13:55:26 +0200

    iteration 2.3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import './App.css';
 import countriesDB from './countries.json';
 import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
 
 function App() {
   const [countries, setCountries] = useState(countriesDB);
 
   return <div className="App">
     <Navbar />
+    <CountriesList countries={countries} />
   </div>;
 }
 
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function CountriesList() {
+function CountriesList({ countries }) {
   return (
     <div>
-        
+        {countries.map((country) => 
+        <div>
+            <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt='flat img'></img>
+            
+            <h1>{country.alpha3Code}</h1>
+        </div>
+        )}
     </div>
   )
 }
