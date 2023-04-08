337c4d8366adee46063099e9f478a6c364bb1c2a
Garin90 <alex.garrido.baldrich@gmail.com>
2023-04-08 13:38:24 +0200

    iteratin 2.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import './App.css';
-import countriesDB from 'src/countries.json';
+import countriesDB from './countries.json';
+import Navbar from './components/Navbar';
 
 function App() {
   const [countries, setCountries] = useState(countriesDB);
 
-  return <div className="App"></div>;
+  return <div className="App">
+    <Navbar />
+  </div>;
 }
 
 export default App;
