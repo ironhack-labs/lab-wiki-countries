c6e65866d946f65849ee05a08af3e7f2c71ac419
michael07200 <mickmhtong@gmail.com>
2023-04-15 16:13:12 +0200

    build: Iteration 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import countries from './countries.json';
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => response.json())
+      .then((data) => {
+        setCountries(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
 
-      <div className="container">
-        <div className="row">
-          <CountriesList countries={countries} />
-          <Routes>
-            <Route
-              path="/:id"
-              element={<CountryDetails countries={countries} />}
-            />
-          </Routes>
+      {countries.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="container">
+          <div className="row">
+            <CountriesList countries={countries} />
+            <Routes>
+              <Route
+                path="/:id"
+                element={<CountryDetails countries={countries} />}
+              />
+            </Routes>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
