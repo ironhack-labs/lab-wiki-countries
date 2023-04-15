b18b788dcb5240c704c6c5df564c4c9d033bc72d
Wilkins Morales <wilkinscom@gmail.com>
2023-04-15 15:27:03 +0200

    iteration 1 import countriesData

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { useState } from 'react';
+import countriesData from './countries.json';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const [countries, setCountries] = useState(countriesData);
+
+  console.log(countries);
+  return <div className="App"></div>;
 }
 
 export default App;
