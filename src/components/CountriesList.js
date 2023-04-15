d093381c248caab74140e1d9c6037eaa9bff9cb9
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-04-15 16:00:05 +0200

    Iteration 1, ~2.2 - done

---
+++ src/components/CountriesList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const CountriesList = () => {
+  return (
+    <div>CountriesList</div>
+  )
+}
+
+export default CountriesList
