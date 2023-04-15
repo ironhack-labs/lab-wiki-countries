e426a631b57c2dd14aabd2dfd1fc8f2ba5fb63f6
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-15 17:19:19 +0200

    Solve iteration 5

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,27 +1,28 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const CountriesList = ({countries}) => {
-
-
-
-    return (
-        <div className="col-5" style={{maxHeight: '90vh', overflow: 'scroll'}}>
-        <div className="list-group">
-
-        {countries.map (country => {
-            return (
-                <Link to={`/${country.alpha3Code}`} key={country.alpha3Code} className="list-group-item list-group-item-action">
-
-             <img className=""src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="country flag" />{country.name.common}
-                </Link>
-            )
+const CountriesList = ({ countries }) => {
+  return (
+    <div className="col-5" style={{ maxHeight: '90vh', overflow: 'scroll' }} G>
+      <div className="list-group">
+        {countries.map((country) => {
+          return (
+            <Link
+              to={`/${country.alpha3Code}`}
+              key={country.alpha3Code}
+              className="list-group-item list-group-item-action"
+            >
+              <img
+                className=""
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                alt="country flag"
+              />
+              {country.name.common}
+            </Link>
+          );
         })}
-
-
-
-        </div>
-        </div>
-    );
-    }
+      </div>
+    </div>
+  );
+};
 
 export default CountriesList;
