99630cef729d64be4170444b37e5f3e06e385b58
michael07200 <mickmhtong@gmail.com>
2023-04-15 16:08:19 +0200

    style: Iteration 2.3

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -3,27 +3,20 @@ import { Link } from 'react-router-dom';
 
 const CountriesList = ({ countries }) => {
   return (
-    <div className="container">
-      <div className="row">
-        <div
-          className="col-5"
-          style={{ maxHeight: '90vh', overflow: 'scroll' }}
-        >
-          <div className="list-group">
-            {countries.map((country, index) => (
-              <Link
-                to={`/${country.alpha3Code}`}
-                key={index}
-                className="list-group-item list-group-item-action"
-              >
-                <img
-                  src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
-                />
-                {country.name.common}
-              </Link>
-            ))}
-          </div>
-        </div>
+    <div className="col-5" style={{ maxHeight: '90vh', overflow: 'scroll' }}>
+      <div className="list-group">
+        {countries.map((country, index) => (
+          <Link
+            to={`/${country.alpha3Code}`}
+            key={index}
+            className="list-group-item list-group-item-action"
+          >
+            <img
+              src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+            />
+            {country.name.common}
+          </Link>
+        ))}
       </div>
     </div>
   );
