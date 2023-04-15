cc2b34084799d65ebf4ee9e9b85eb6278ee6016c
michael07200 <mickmhtong@gmail.com>
2023-04-15 16:08:35 +0200

    build: Iteration 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import countries from './countries.json';
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <CountriesList countries={countries}/>
+
+      <div className="container">
+        <div className="row">
+          <CountriesList countries={countries} />
+          <Routes>
+            <Route
+              path="/:id"
+              element={<CountryDetails countries={countries} />}
+            />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
