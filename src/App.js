7b44110c75b2e65d2a231ccec0636bbcd9961bcb
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-04-16 18:24:08 +0200

    Iteration 5 - done, solved lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import countriesJson from './countries.json'
 import Navbar from './components/Navbar'
@@ -9,6 +9,13 @@ import CountryDetails from './components/CountryDetails'
 function App() {
 
   const [countries, setCountries] = useState(countriesJson)
+
+  useEffect(()=> {
+    fetch("https://ih-countries-api.herokuapp.com/countries")
+    .then(res => res.json())
+    .then(json => {setCountries(json)})
+  }, [])
+
   return (
     <div className="App">
       <Navbar />
--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 
 const CountryDetails = ({countries}) => {
 
   const {alpha3Code} = useParams()
 
-  const country = countries.find(country => alpha3Code === country.alpha3Code)
+  const [country, setCountry] = useState({})
+
+  // Iteration 2.4
+  /* const country = countries.find(country => alpha3Code === country.alpha3Code) */
+  
+  useEffect(() => {
+    fetch(`https://ih-countries-api.herokuapp.com/countries/${alpha3Code}`)
+    .then(res => res.json())
+    .then(json => {
+      console.log(json)
+      setCountry(json)
+    })
+  }, [alpha3Code])
 
   const { name, capital, area, borders } = country
 
   return (
-    <div className="col-7">
-        
-      <h1>{name.common}</h1>
+    <div className="col-7 text-start">
+      
+      <h1 className="font-weight-bold">{name.common}</h1>
         
       <table className="table">
         <thead></thead>
@@ -38,7 +50,7 @@ const CountryDetails = ({countries}) => {
                   borders.map(border => {
                     return (
                       <li key={border}>
-                        <Link to={`/:${border}`}>
+                        <Link to={`/:${border}`} style={{textDecoration: 'none'}}>
                           {countries.find(country => border === country.alpha3Code).name.common}
                         </Link>
                       </li>
