784e2e4524a2346351c6f175628535c9ec302365
Fabian Hurtado Volkmann <fabian04berlin@outlook.de>
2023-05-04 16:51:14 +0200

    iterations 1, 2

---
+++ src/components/CountriesList.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import {Routes, Route} from 'react-router-dom'
+import CountryDetails from '../components/CountryDetails'
+
+
+function CountriesList(props){
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    setCountries(props.countries)
+  }, [props.countries])
+
+  return(
+    <div className="container">
+      <div className="row">
+        <div className="col-5" style={{maxHeight: '90vh', overflow: 'scroll'}}>
+          <div className="list-group">
+            {countries.map(country => {
+              return(
+                <Link key={country.alpha3Code} to={`/details/${country.alpha3Code}`} className="list-group-item list-group-item-action"> <img height={40} src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}/> <br /> {country.name.common}</Link>
+              )
+            })}
+          </div>  
+        </div>
+        <Routes>
+          <Route path="/details/:id" element={<CountryDetails />}></Route>
+        </Routes>
+      </div>
+    </div>
+  )
+}
+export default CountriesList
---
+++ src/components/CountryDetails.js
@@ -0,0 +1,53 @@
+import countriesArr from '../countries.json'
+import { useParams, Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+
+function CountryDetails(){
+  const countryId = useParams().id
+  const [selectedCountry, setSelectedCountry] = useState(countriesArr.find(country => country.alpha3Code === countryId ))
+
+  useEffect(() => {
+    setSelectedCountry(countriesArr.find(country => {
+      return country.alpha3Code === countryId
+    }))
+  }, [countryId])
+
+  return(
+    <div className="col-7">
+      <img height={125} src={`https://flagpedia.net/data/flags/icon/72x54/${selectedCountry.alpha2Code.toLowerCase()}.png`}/>
+      <h1>{selectedCountry.name.common}</h1>
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style={{width: '30%'}}>Capital</td>
+            <td>{selectedCountry.capital[0]}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {selectedCountry.area} km
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul style={{listStyle: 'none'}}>
+                {selectedCountry.borders.map(borderingCountryCode => {
+                  const borderingCountry = countriesArr.find(country => country.alpha3Code === borderingCountryCode)
+                  return(
+                    <li>
+                      <Link to={`/details/${borderingCountry.alpha3Code}`}>{borderingCountry.name.common}</Link>
+                    </li>
+                  )
+                })}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+  )
+}
+export default CountryDetails
