d84742ef2be90842a890abcec78c48af06d8987b
Ricardo Navarro <ranr279@hotmail.com>
2023-05-25 09:38:47 +0200

    done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css'
+const NavBar = () => {
+    return (
+        <nav class="navbar navbar-dark bg-primary mb-3">
+            <div class="container">
+                <a class="navbar-brand" href="/">WikiCountries</a>
+            </div>
+        </nav>
+    )
+}
+export default NavBar
