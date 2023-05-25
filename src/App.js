8c46efbcbea0f48ebc24e4a8507e14178ac26eac
Lorena Lario <lorenalariotudela1999@gmail.com>
2023-05-25 20:17:00 +0200

    lab terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import CountriesList from './views/CountriesList';
+import Navbar from './components/Navbar';
+import CountryDetails from './views/CountryDetails';
+import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [countries, setCountries] = useState(null);
+
+  const getCountries = async () => {
+    let gotCountries = await axios.get(
+      'https://ih-countries-api.herokuapp.com/countries'
+    );
+    setCountries(gotCountries.data);
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="col-5" style={{maxHeight: "90vh", overflowY: "scroll"}}>
+            {countries !== null && <CountriesList countries={countries} />}
+          </div>
+          <div className="col-7">
+            <Routes>
+              <Route
+                path="/:countryId"
+                element={<CountryDetails countries={countries} />}
+              />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/views/CountryDetails.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+function CountryDetails({countries}) {
+  const [country, setCountry] = useState(null);
+  const params = useParams()
+
+  useEffect(() => {
+    let filtered = countries.find((eachCountry) => {
+      return eachCountry.alpha3Code === params.countryId;
+    });
+    setCountry(filtered);
+  }, [params.countryId, countries]);
+
+  if (country === null) {
+    return '...pensando...';
+  }
+
+  return (
+    <div>
+              <img src={`https://flagpedia.net/data/flags/h120/${country.alpha2Code.toLowerCase()}.png`} alt="foto-pais"></img>
+
+      <h1>{country.name.common}</h1>
+      <table class="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style={{ width: '30%' }}>Capital</td>
+            <td>{country.capital[0]}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {country.area + ' km'}
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                {country.borders.map((eachBorder) => {
+                  return (
+                    <li>
+                      <Link to={`/${eachBorder}`}>
+                        {
+                          countries.find((eachCountry) => {
+                            return eachCountry.alpha3Code === eachBorder;
+                          }).name.common
+                        }
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default CountryDetails;
