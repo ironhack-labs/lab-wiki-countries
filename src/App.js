034373e1c3058e46b7d89cac50530decaf855ba5
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-06-01 00:10:21 +0200

    Finished CountriesList Components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import countries from './countries.json'
 import { Navbar } from './components/Navbar';
+import { CountriesList } from './components/CountriesList';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <CountriesList countries={countries} />
     </div>
   );
 }
--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,7 +1,18 @@
-export function CountriesList(){
+import { Link } from "react-router-dom";
+
+
+export function CountriesList({countries}){
     return(
       <>
-        
+    <div className="list-group" style={{width: '500px',maxHeight: "800px", overflow: 'scroll'}}>
+        {countries.map((country) => {
+          return(
+           <div className="list-group-item list-group-item-action d-flex justify-content-start">
+            <Link className="text-decoration-none text-dark"  to={`/country`}> <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} style={{width: "25px"}} alt={country.name.common} /> {country.name.common}</Link>
+            </div>
+          );
+        })}
+        </div>
       </>
     );
 }
