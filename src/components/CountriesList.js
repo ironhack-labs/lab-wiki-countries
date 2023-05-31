aa1a4362d0ad77be2262fd78976743a6a0f0c940
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-05-31 23:52:33 +0200

    Created Components

---
+++ src/components/CountriesList.js
@@ -0,0 +1,7 @@
+export function CountriesList(){
+    return(
+      <>
+        
+      </>
+    );
+}
---
+++ src/components/CountryDetails.js
@@ -0,0 +1,7 @@
+export function CountryDetails(){
+    return(
+      <>
+        
+      </>
+    );
+}
