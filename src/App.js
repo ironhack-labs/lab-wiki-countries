6cbc18830a4ed8d5308e3591ff08b1b33f876088
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-05-31 23:57:15 +0200

    Finished Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import countries from './countries.json'
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
