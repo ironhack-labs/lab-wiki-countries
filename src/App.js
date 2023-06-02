03c21817e7eb9ab435b5c124185186dad1fca986
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-06-02 21:41:43 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import './App.css';
 import countries from './countries.json'
 import { Navbar } from './components/Navbar';
 import { CountriesList } from './components/CountriesList';
+import { Route, Routes } from 'react-router-dom';
+import { CountryDetails } from './components/CountryDetails';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const apiUrl = "https://ih-countries-api.herokuapp.com/countries"
 
 function App() {
+
+  const [countriesData, setCountriesData] = useState([]);
+
+  useEffect(() => {
+    axios.get(apiUrl)
+    .then((response) => {
+      const data = response.data;
+      setCountriesData(data)
+    })
+
+
+  },[]);
+
   return (
     <div className="App">
-      <Navbar />
-      <CountriesList countries={countries} />
+       <Navbar />
+      <div className="container">
+      <div className="row">
+      <CountriesList countries={countriesData} />
+      <Routes>
+       <Route path="/:id" element={<CountryDetails countries={countriesData} />}></Route>
+       </Routes>
+  </div>
+</div>
     </div>
   );
 }
--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import { Navbar } from "./Navbar";
 
 export function CountriesList({countries}){
     return(
@@ -7,8 +7,8 @@ export function CountriesList({countries}){
     <div className="list-group" style={{width: '500px',maxHeight: "800px", overflow: 'scroll'}}>
         {countries.map((country) => {
           return(
-           <div className="list-group-item list-group-item-action d-flex justify-content-start">
-            <Link className="text-decoration-none text-dark"  to={`/country`}> <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} style={{width: "25px"}} alt={country.name.common} /> {country.name.common}</Link>
+           <div key={country.alpha2Code} className="list-group-item list-group-item-action d-flex justify-content-start">
+            <Link className="text-decoration-none text-dark"  to={`/${country.alpha3Code}`}> <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} style={{width: "25px"}} alt={country.name.common} /> {country.name.common}</Link>
             </div>
           );
         })}
