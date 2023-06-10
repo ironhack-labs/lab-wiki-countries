adc60c1c44035102232148cc148c4ae103563e52
olgam <olgamartyniuk88@gmail.com>
2023-06-10 12:52:54 +0200

    borders working

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,30 +1,13 @@
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 function CountryDetails({ countries }) {
   const { countryCode } = useParams();
   const [countryDetail, setCountryDetail] = useState(null);
 
-  const selectedCountry = countries.find(country => country.alpha3Code === countryCode);
-
-  // const getCountryDetails = (countryCode) => {
-  //     let countriesCopy = [...countries];
-  //     const filteredCountries = countriesCopy.find((oneCountry) => {
-  //         if (oneCountry.alpha3Code === countryCode) {
-  //             return true;
-  //         }
-  //     })
-  //     setCountryDetail(filteredCountries);
-  // }
-  // getCountryDetails();
-
-//   const oneCountry = countries.filter((oneFilteredCountry) => {
-//     if (oneFilteredCountry.alpha3Code === countryCode) {
-//       return true;
-//     }
-//     // setCountryDetail(oneCountry);
-//   });
-//   console.log("THIS COUNTRY",oneCountry);
+  const selectedCountry = countries.find(
+    (country) => country.alpha3Code === countryCode
+  );
 
   return (
     <div class="col-7">
@@ -33,31 +16,31 @@ function CountryDetails({ countries }) {
         <thead></thead>
         <tbody>
           <tr>
-            <td style={{width: "30%"}}>Capital</td>
-            <td>Paris</td>
+            <td style={{ width: '30%' }}>Capital</td>
+            <td>{selectedCountry.capital}</td>
           </tr>
           <tr>
             <td>Area</td>
             <td>
-              551695 km
+              {selectedCountry.area}
               <sup>2</sup>
             </td>
           </tr>
-          {/* <tr>
-              <td>Borders</td>
-              <td>
-                <ul>
-                  <li><a href="/AND">Andorra</a></li>
-                  <li><a href="/BEL">Belgium</a></li>
-                  <li><a href="/DEU">Germany</a></li>
-                  <li><a href="/ITA">Italy</a></li>
-                  <li><a href="/LUX">Luxembourg</a></li>
-                  <li><a href="/MCO">Monaco</a></li>
-                  <li><a href="/ESP">Spain</a></li>
-                  <li><a href="/CHE">Switzerland</a></li>
-                </ul>
-              </td>
-            </tr> */}
+          <tr>
+            <td>
+              Borders:
+              {selectedCountry.borders.map((border) => {
+                const borderCountry = countries.find(
+                  (country) => country.alpha3Code === border
+                );
+                return (
+                  <Link to={`/details/${borderCountry.alpha3Code}`}>
+                    {borderCountry.name.official}
+                  </Link>
+                );
+              })}
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
