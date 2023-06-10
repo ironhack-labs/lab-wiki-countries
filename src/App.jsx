9ab2ddf1bfbf5c0771f429fac3975d5f44c9ef17
Ayoze Martin <ayozemt@gmail.com>
2023-06-10 13:18:41 +0200

    CountriesList

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { useState } from 'react';
+import data from './countries.json';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+
+function App() {
+  const [countries, setCountries] = useState(data);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <CountriesList countries={countries} />
+    </div>
+  );
+}
+
+export default App;
