90ea7d7255762658e72bf45de2c94b3f7d2f7686
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2023-06-10 15:37:18 +0200

    completed till iteration4

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,21 +1,41 @@
 import { useParams } from "react-router-dom";
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function CountryDetails({countries}) {
   const {id} = useParams()
+  const [oneCountry, setOneCountry] =useState(null);
   console.log("country id", id)
-  let country = countries.find((country) => country.alpha3Code === id )
-  console.log("this is countryDetails",country)
-  if(!country){
+  //let country = countries.find((country) => country.alpha3Code === id )
+  useEffect(() => {
+    const fetchCountryData = async () => {
+      try {
+        const response = await axios.get(`https://ih-countries-api.herokuapp.com/countries/${id}`);
+        setOneCountry(response.data);
+      } catch (error) {
+        console.log('Error fetching country data:', error);
+      }
+    };
+
+    fetchCountryData();
+  }, [id]);
+  console.log("this is countryDetails",oneCountry)
+  if(!oneCountry){
     return <div>
       Country not found!
     </div>
   }
-  console.log(country);
+  //console.log(country);
   return (
     <div>
-    <h1>{country.name.common}</h1>
-    <p>{country.name.official}</p>
+    <h1>{oneCountry.name.common}</h1>
+    <p>{oneCountry.name.official}</p>
+   <p>Region: {oneCountry.region}</p>
+      <p>Subregion: {oneCountry.subregion}</p>
+      <p>Capital: {oneCountry.capital}</p>
+      <p>Languages: {Object.values(oneCountry.languages).join(', ')}</p>
+      <p>Area: {oneCountry.area} square kilometers</p>
+
     </div>
   ) 
 }
