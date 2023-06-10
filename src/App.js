af9766d46809affc2de47a65b8e6269ec7d4fbe7
olgam <olgamartyniuk88@gmail.com>
2023-06-10 11:24:01 +0200

    countries list done but not complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import countries from "./countries.json";
 import Navbar from "./components/Navbar.jsx"
+import CountriesList from './components/CountriesList';
+import { useState } from 'react';
 
 function App() {
+  const [countriesAll, setCountriesAll] = useState(countries);
   return (
     <div className="App">
     <Navbar /> 
+    {countriesAll.map((country) => {
+        return (
+          <CountriesList
+            key={country.alpha3Code}
+            oneCountry={country}
+          />
+        );
+    })}
 
       
      
