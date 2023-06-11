7c4fc36e6d5638e2c42b06e699e0583ad3d6a9e0
Ayoze Martin <ayozemt@gmail.com>
2023-06-11 12:27:26 +0200

    iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,21 @@ import Navbar from './components/NavBar';
 import CountriesList from './components/CountriesList';
 import { Route, Routes } from 'react-router-dom';
 import CountryDetails from './components/CountryDetails';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+const API_URL = "https://ih-countries-api.herokuapp.com/countries";
 
 function App() {
-  const [countries, setCountries] = useState(data);
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(API_URL);
+      setCountries(response.data);
+    };
+    getData();
+  }, []);
 
   return (
     <div className="App">
