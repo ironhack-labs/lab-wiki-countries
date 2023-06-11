4e404c2e414952961817330988ad948ed04431f8
Bujio <javier.vivas@icloud.com>
2023-06-11 21:28:46 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import CountriesList from './components/CountriesList';
+import Navbar from './components/Navbar';
+import CountryDetails from './pages/CountryDetails';
+import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [countries, setCountries] = useState(null);
+
+  const getCountries = async () => {
+    let res = await axios.get(
+      'https://ih-countries-api.herokuapp.com/countries'
+    );
+    setCountries(res.data);
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <div
+            className="col-5"
+            style={{ maxHeight: '850px', overflowY: 'scroll' }}
+          >
+            {countries !== null && <CountriesList countries={countries} />}
+          </div>
+          <div className="col-7">
+            <Routes>
+              <Route
+                path="/:countryId"
+                element={<CountryDetails countries={countries} />}
+              />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div>
+      <h1>LAB - WikiCountries</h1>
+    </div>
+  );
+}
