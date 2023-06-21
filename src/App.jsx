ed1a715034e1b097ee98a600c35228facd5e7983
singa23 <singabe-xd@hotmail.fr>
2023-06-21 20:41:58 +0200

    iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import React from "react";
+import axios from "axios";
 import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import HomePage from "./pages/HomePage";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+     <Navbar></Navbar>
+      <HomePage></HomePage>
+      <CountryDetailsPage></CountryDetailsPage>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,13 @@
-function Navbar() {}
+import React from 'react';
+function Navbar() {
+    return (
+        <> <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">WikiCountries</a>
+        </div>
+      </nav>
+        </>
+    );
+}
 
 export default Navbar;
