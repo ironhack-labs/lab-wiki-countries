d510e2b1aa6d9c0d57854d9006f937e651ffbe5d
Cedric Aim <cedric.aim@gmail.com>
2023-06-21 20:10:33 +0200

    iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import CountryDetails from "./pages/CountryDetailsPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <HomePage />
+      <CountryDetails />
     </div>
   );
 }
