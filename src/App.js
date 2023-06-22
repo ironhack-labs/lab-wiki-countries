462b684018b10ab55f3b8da6cdf66dff10f02118
Philippe Miossec <pmiossec@gmail.com>
2023-06-22 18:12:35 +0200

    Iteration 1 | Import JSON data
    Co-authored-by: MarySueMe <MarySueMe@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import './App.css';
+import allCountries from './countries.json'
 
 function App() {
+
+  const [countries, setCountries] = useState(allCountries);
+
   return (
     <div className="App">
     </div>
