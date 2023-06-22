718bf2727dc6fc686a073eebff90050fd6e4a261
Philippe Miossec <pmiossec@gmail.com>
2023-06-22 18:33:04 +0200

    Iteration 2.1 & 2.2 | Create components & Navbar
    Co-authored-by: MarySueMe <MarySueMe@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import './App.css';
 import allCountries from './countries.json'
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountriesDetails from './components/CountryDetails';
 
 function App() {
 
@@ -8,6 +11,16 @@ function App() {
 
   return (
     <div className="App">
+      <div>
+          <Navbar />
+          <div className="container">
+            <div className="row">
+              <CountriesList />
+              <CountriesDetails />
+            </div>
+          </div>
+
+          </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
