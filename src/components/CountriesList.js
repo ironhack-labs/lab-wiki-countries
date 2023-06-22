9d5edf4a0a1f03b4bf5c7e63df649070b38f1094
Solideo Putra Zendrato <solidzendrato@gmail.com>
2023-06-22 23:02:03 +0200

    added flag for the each country

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -13,6 +13,11 @@ function CountriesList(props) {
         return(
           <div key={element.name} className="p-3">
             <h3>{element.name.common}</h3>
+            <img
+              src={`https://flagpedia.net/data/flags/icon/72x54/${element.alpha2Code.toLowerCase()}.png`}
+              alt={`Flag of ${element.name.common}`}
+              className="mr-3"
+            />
             <Link to={`/countrydetails/${element.alpha3Code}`}>More Details</Link>
           </div>  
         )
