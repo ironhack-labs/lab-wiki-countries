22565b659d3f4ee60e1117eefd9bf4b6f5907497
Fernando Ramos <fndo.ramos@gmail.com>
2023-06-22 20:42:35 +0200

    add until iteration 2.3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
-import countries from'./countries.json'
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import Navbar from './components/Navbar';
+import countries from'./countries.json';
 
 function App() {
+
+  const getCountry = (countryAlpha3Code) => {
+    return countries.filter((countryObj) => countryObj.alpha3Code === countryAlpha3Code )[0]
+  }
+
   return (
     <div className="App">
-     
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <CountriesList countriesArr={countries}></CountriesList>
+
+          {/* <CountryDetails callbackToGetCountry={getCountry}></CountryDetails> */}
+
+        </div>
+      </div>   
     </div>
   );
 }
---
+++ src/components/CountryDetails.js
@@ -0,0 +1,44 @@
+import { useParams, Link } from 'react-router-dom';
+
+const CountryDetails = (props) => {
+  const { alpha3Code } = useParams();
+
+  const countryObj = props.callbackToGetCountry(alpha3Code);
+
+  return (
+    <div className="col-7">
+      <h1>{countryObj.name.common}</h1>
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style={{ width: '30%' }}>Capital</td>
+            <td>{countryObj.capital[0]}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {countryObj.area} km
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                {countryObj.borders.map((border) => {
+                    const countryFromBorder = props.callbackToGetCountry(border)
+                  return <li key={border}>
+                    <Link href={`/${border}`}>{countryFromBorder.name.common}</Link>
+                  </li>;
+                })}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CountryDetails;
