c928c5c69d27c3bba919947753c11a4ede6f4031
gtildis <gretil_15@hotmail.com>
2023-06-23 08:55:54 +0200

    lab not done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import countriesData from './countries.json';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import { Route, Routes, NavLink, Link } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [countries, setCountries] = useState(countriesData);
+
+  return (
+    <div className="App">
+      <Navbar />
+
+      <CountriesList countries={countries} />
+
+      <Routes>
+        <Route
+          path="/:countryAlpha3Code"
+          element={<CountryDetails countries={countries} />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <>
+      <p>this is the navbar</p>
+    </>
+  );
+}
+
+export default Navbar;
