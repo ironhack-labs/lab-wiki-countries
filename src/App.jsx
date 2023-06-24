324d9b74f2976f8737c5a641d29d9a2000cd9cdb
rach <rachelpytse@gmail.com>
2023-06-24 12:02:10 +0200

    finished all iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route, Link } from "react-router-dom";
+import CountryDetails from "./pages/CountryDetailsPage";
 import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>LAB | React WikiCountries</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>LAB | React WikiCountries</h1>
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path=":countryId/" element={<CountryDetails />} />
+      </Routes>
+    </>
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,43 @@
-function HomePage() {}
+import { useState } from "react";
+import { useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+import axios from "axios";
+
+function HomePage() {
+  const [countries, setCountries] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-countries-api.herokuapp.com/countries")
+      .then(function (response) {
+        setCountries(response.data);
+        console.log(response.data);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }, []);
+
+  if (!countries) return "loading";
+
+  return (
+    <>
+      <h1>WikiCountries: Your Guide to the World</h1>
+      {countries.map(function (country) {
+        return (
+          <div key={country.alpha3Code}>
+            <Link to={`/${country.alpha3Code}`}>
+              <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+              ></img>
+              {country.name.common}
+            </Link>
+          </div>
+        );
+      })}
+    </>
+  );
+}
 
 export default HomePage;
