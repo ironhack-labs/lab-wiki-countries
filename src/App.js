115706baba90ff754d35b73db7f4224df5b24db5
Walter Bernal <wltrbm@gmail.com>
2023-08-03 02:42:34 -0700

    Iteration 2 & 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css'
 import countries from './countries.json'
+import Navbar from './components/Navbar'
+import CountriesList from './components/CountriesList'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      
+      <div className="container">
+        <div className="row">
+          <CountriesList countriesData={countries} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
