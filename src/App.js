86db9f9a73e73b2b294a5bb4902844b09f5a7140
iceisland12 <luisguile495@gmail.com>
2023-08-22 11:00:39 -0400

    Lab Done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,40 @@
 import { useEffect, useState } from 'react';
 import { Routes, Route } from "react-router-dom";
 import './App.css';
-import data from './countries.json'
+// import data from './countries.json'
 import Navbar from './components/NavBar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
-
 function App() {
-
   const [countries, setCountries] = useState(null)
   const url =  "https://ih-countries-api.herokuapp.com/countries"
-
-
   useEffect(() => {
     fetch(url)
     .then(response => {
       return response.json()
     })
-    .then(resposta => {
-      setCountries(resposta)
+    .then(response => {
+      setCountries(response)
     })
-    }, []);
-
-
-  console.log(countries)
-
+  }, []);
   return (
- <>
-
-     {!countries && <h3>Country not found!</h3>}
-     {countries && (
-      <div className="App">
-      <Navbar />
-      <div class="container">
-        <div class="row">
-      <CountriesList countries={countries} />
-      <Routes>
-        <Route path="/:id" element={ <CountryDetails  countries={countries}  /> } />
-      </Routes>
-      </div>
-      </div>
-    </div>
-     )}
-    </>
+            <>
+                {!countries && <h3>Countries not found!</h3>}
+                {countries && (
+                  <div className="App">
+                    <Navbar />
+                    <div class="container">
+                      <div class="row">
+                        <CountriesList countries={countries} />
+                          <Routes>
+                            <Route path="/:id" element={ <CountryDetails /> } />
+                            <Route path="/:id" element={ <CountryDetails countries={countries} /> } />
+                          </Routes>
+                      </div>
+                    </div>
+                  </div>
+                )}
+            </>
   );
 }
-
 export default App;
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,31 +1,25 @@
-
 import { Link } from "react-router-dom";
-
 function CountriesList({ countries }) {
-
     const divStyle = {
         maxHeight: "90vh",
         overflow: "scroll"
     };
-
     const image = {
         display: "block", 
         margin: "0 auto"
     };
-
     return (
         <div className="col-5" style={divStyle}>
             <div className="list-group">
-            {countries.map((country) => {
-
+                {countries.map((country) => {
                   return  ( 
-                    <>  
-                    <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} style={image} st />
-                    <Link className="list-group-item list-group-item-action" to={`/${country.alpha3Code}`}>{country.name.common}</Link>
-                </>
-   ) })}
-    </div>
-</div>
-);
+                        <>  
+                            <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} style={image} />
+                            <Link className="list-group-item list-group-item-action" to={`/${country.alpha3Code}`}>{country.name.common}</Link>
+                        </>
+           ) })}
+            </div>
+        </div>
+    );
 }
 export default CountriesList;
