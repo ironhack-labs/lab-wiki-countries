7e5f58f6cfecb6beab8eb637a330fda03df72fb8
Irene Buceta <irenebuzalocen@gmail.com>
2023-08-23 23:21:00 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { Route, Routes, useParams } from 'react-router-dom'
 import "./App.css";
+import Navbar from './components/Navbar'
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+import HomePage from "./pages/HomePage";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path={`/:countryId`} element={<CountryDetailsPage />} />
+      </Routes>
+
+
+
     </div>
-  );
+  )
 }
 
 export default App;
