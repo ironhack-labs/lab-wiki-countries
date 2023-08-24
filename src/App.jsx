008eac10e0f483a1ff7e9f871cc2ef03edc7ef23
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-08-24 20:16:35 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import CountryDetails from "./pages/CountryDetailsPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/countrydetails/:alpha3Code" element={<CountryDetails />} /> 
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,14 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return(
+        <nav className="navbar navbar-dark bg-primary mb-3">
+            <div class="container">
+                <a class="navbar-brand" href="/">WikiCountries</a>
+                <Link to={"/"}>Home</Link>
+            </div>
+        </nav>
+    );
+}
 
 export default Navbar;
