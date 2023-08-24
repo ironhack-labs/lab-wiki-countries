b31ba1ce99334d246b343a2843286fafbfac340b
Joao Borrega <joaoborrega18@gmail.com>
2023-08-24 22:56:55 +0100

    lab-wiki-countries-vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CountryDetails from "./pages/CountryDetailsPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<HomePage/>}/>
+
+        <Route path="/:countryId" element={<CountryDetails/>} />
+        
+      </Routes>
     </div>
   );
 }
