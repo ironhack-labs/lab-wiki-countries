bc5446b5c1a6626bf0439a6838c005481f4a3dac
Rita Saraiva <rita_saraiva@hotmail.com>
2023-08-24 19:44:21 +0100

    solved lab

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,34 @@
-function HomePage() {}
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+  const [fetching, setFetching] = useState(true);
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => {
+        setCountries(response.data);
+        setFetching(false);
+        console.log(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>WikiCountries: Your Guide to the World</h2>
+      {fetching && <p>Loading...</p>}
+      {countries.map((country) => {
+        return (
+          <div key={country._id}>
+            <Link to={`/${country.alpha3Code}`}>{country.name.common}</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
 
 export default HomePage;
