363825537dc45ee3eebd7978ce066a4d65e3ede2
HannaInIT <ann10062016@gmail.com>
2023-09-14 19:37:22 +0200

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import "./App.css";
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import { Routes, Route } from "react-router-dom";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
 
 function App() {
+
+  
+
   return (
+
     <div className="App">
+      <Navbar />
       <h1>LAB | React WikiCountries</h1>
+
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/:countryId' element={<CountryDetailsPage/>} />
+      </Routes>
+
+
+      <HomePage />
     </div>
+   
   );
 }
+
 
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,37 @@
-function HomePage() {}
+import { useState, useEffect } from 'react'
+import axios from "axios";
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://ih-countries-api.herokuapp.com/countries")
+            .then(response => {
+                console.log(response)
+                setCountries(response.data);
+            })
+            .catch((e) => {
+                console.log("error getting coutries from the API....", e);
+            });
+    }, []);
+
+    return (
+        <>
+            <h1>WikiCountries: Your Guide to the World </h1>
+            {countries.map((countriesObj, index) => {
+                return (
+                    <section key={index} className='card' >
+                        <Link to={`${countriesObj.alpha3Code}`}><h3>{countriesObj.name.common}</h3></Link>
+                    </section>
+                )
+            }
+            )}
+        </>
+    )
+}
+
+
 
 export default HomePage;
