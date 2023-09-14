d80bbf75127c146e8e0144a7d14211a0631c8999
Sandrine Manel <sandrine.manelgmail.com>
2023-09-14 19:23:43 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import CountryDetails from "./pages/CountryDetailsPage";
+import HomePage from "./pages/HomePage";
+import {Routes,Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>LAB | React WikiCountries</h1>
-    </div>
+    <>
+
+      <div className="App">
+        <h1>LAB | React WikiCountries</h1>
+        
+      </div>
+
+      <Navbar></Navbar>
+
+      <Routes>
+        <Route path="/" element= {<HomePage/>}/>
+        <Route path="/:countryId" element={<CountryDetails/>} />
+      </Routes>
+    </>
+
+
+
   );
 }
 
