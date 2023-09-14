4b2f7fb00d6fe0874c7a5a341e2f6155692a2464
Priscila Borges <priscila.borges.nl@gmail.com>
2023-09-14 22:51:22 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react'
+import axios from "axios";
+import { Route, Routes } from 'react-router-dom';
+
+import Navbar from "./components/Navbar";
+import CountryDetails from "./pages/CountryDetailsPage";
 import "./App.css";
+import HomePage from './pages/HomePage';
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>LAB | React WikiCountries</h1>
-    </div>
-  );
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/countries/:alpha3Code' element={<CountryDetails />} />
+      </Routes>
+    </>
+  )
+
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,13 @@
-function Navbar() {}
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <div>
+            <NavLink to="/">
+                WikiCountries
+            </NavLink>
+        </div>
+    );
+}
 
 export default Navbar;
