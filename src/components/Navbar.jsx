4582dd56303bed8b69ecadb678f7905de3f9c525
You-Rim Kim <yourimykim@gmail.com>
2023-09-21 23:11:32 +0200

    half done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,19 @@
-function Navbar() {}
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+          <ul>
+            
+            <NavLink to="/" className={({ isActive }) => isActive ? "selected" : ""}>
+              WikiCountries
+            </NavLink>
+            
+            
+          </ul>
+          
+        </nav>
+      );
+    }
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,51 @@
-function HomePage() {}
+import { useState, useEffect } from "react";
+import { Navigate, Link } from "react-router-dom";
+import axios from "axios";
+
+const apiURL = "https://ih-countries-api.herokuapp.com/countries";
+
+function HomePage() {
+
+  const [countries, setCountries] = useState([]);
+  const [fetching, setFetching] = useState(true);
+
+  useEffect(() => {
+    console.log("useEffect - Initial render (Mounting)");
+    axios.get(apiURL)
+      .then((response) => {
+      setCountries(response.data);
+      setFetching(false);
+    })
+    .catch((error) => {
+        console.error("Error fetching data:", error);
+        setFetching(false);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>WikiCountries: Your Guide to the World</h1>
+    
+      {fetching ? <p>Loading ...</p> : null} {/* Show "Loading ..." while fetching */}
+    
+      <ul>
+        {countries.map((country) => (
+          <li key={country.alpha3Code}>
+            <Link to={`/country/${country.alpha3Code}`}>
+              <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                alt={`${country.name} Flag`}
+              />
+              {country.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+  
+
+
 
 export default HomePage;
