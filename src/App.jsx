21fc3a45f99a1fd15c2c99064e36f6801300d0ed
etorres12345 <erik.torres1215@gmail.com>
2023-09-24 13:15:55 +0200

    contain emphassis fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:countryId" element={<CountryDetailsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,18 @@
-function Navbar() {}
+import { Link, NavLink } from "react-router-dom";
 
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      WikiCountries
+      <ul>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "selected" : "")}
+        >
+          Home
+        </NavLink>
+      </ul>
+    </nav>
+  );
+}
 export default Navbar;
