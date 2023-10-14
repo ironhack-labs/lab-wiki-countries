7186c0cae8c2143b0074c9a8f0a563a20dfbc4a6
acanest83 <133660942+acanest83@users.noreply.github.com>
2023-10-14 22:52:41 +0200

    Iteración 4 fin

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import CountryDetails from "./pages/CountryDetailsPage";
 import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
       <Navbar />
-      <CountryDetails />
       <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:countryId" element={<CountryDetails />} />
+      </Routes>
     </div>
+
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 function HomePage() {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        axios.get("https://ih-countries-api.herokuapp.com/countries ")
+            .then(response => {
+                setCountries(response.data);
+            })
+            .catch(error => {
+                console.error("Error in country data", error);
+            });
+    }, []);
     return (
-        <div className="container">
-            <p style={{ fontSize: "24px", fontWeight: "bold" }}>WikiCountries: Your Guide to the world</p>
-        </div>
+        <>
+            <section className="container">
+                <p style={{ fontSize: "24px", fontWeight: "bold" }}>WikiCountries: Your Guide to the world</p>
+            </section>
+            <div>
+                <h1>List of Countries</h1>
+                <ul>
+                    {countries.map(country => (
+                        <li key={country.alpha3Code}>
+                            <Link to={`/country/${country.alpha3Code}`}>
+                                <img
+                                    src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                                    alt={`${country.name} flag`}
+                                />
+                                {country.name}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 }
 
