67146daaa351138f9b7087a04775bc8224ad76e0
Daniel Rodrigues <danielbrodrigues25@gmail.com>
2023-11-06 16:52:04 +0000

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,13 @@
-function Navbar() {}
+import { Link } from "react-router-dom"
+
+
+function Navbar() {
+
+    return (
+        <nav>
+            <Link to ="/" id="nav-link"></Link>WikiCountries
+        </nav>
+    )
+}
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,47 @@
-function HomePage() {}
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  const [countries, setCountries] = useState(['']);
+  const [fetching, setFetching] = useState(true);
+
+  const apiUrl = "https://ih-countries-api.herokuapp.com/countries";
+
+  useEffect(() => {
+    axios.get(apiUrl)
+      .then((response) => {
+        setCountries(response.data);
+        setFetching(false);
+        console.log(response.data)
+      })
+
+    
+  }, []);
+
+  return (
+    <div className="main-div">
+      <h1>WikiCountries: Your Guide to the World.</h1>
+      {fetching ? (
+        <p>Loading countries...</p>
+      ) : (
+        <div className="container">
+            <ul>
+            {countries.map((country) => (
+            <li key={country.alpha3Code} className="list-group"> 
+              <Link to={`/${country.alpha3Code}`} className="no-link-effect">
+                            <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="" />
+                            <p>{country.name.common}</p>
+</Link>
+                    </li>
+                
+                ))}
+            </ul>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default HomePage;
