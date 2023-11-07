1267b0afe1d872bad17c5eca708916a9f5e74f58
João Silva <joaosilvapedro357@gmail.com>
2023-11-07 02:23:36 +0000

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CountryDetails from "./pages/CountryDetailsPage";
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element ={<HomePage/>} />
+        <Route path="/:countryId" element ={<CountryDetails/>} />
+      </Routes>
     </div>
   );
 }
