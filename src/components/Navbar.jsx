277768d86913ad91d53975768c58f477e934f7b4
Caroline Vallim <carolinecvallim@gmail.com>
2023-11-07 07:00:08 +0000

    final update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,19 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+
+// Create a NavBar component
+function Navbar() {
+    return(
+        <div>
+            <nav id="nav-bar">
+                <div>
+                    <p>WikiCountries</p>
+                </div>
+                <div>
+                    <Link to="/">HomePage</Link>
+                </div>
+            </nav>
+        </div>
+    )
+}
 
 export default Navbar;
