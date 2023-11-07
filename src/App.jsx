f982a850c43c2ffa65f4dc9a27729f2a8963de03
David Lucca <david.v.lucca@gmail.com>
2023-11-07 07:24:08 +0000

    done with error on loading forever on country details

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import CountryDetailsPage from "./pages/CountryDetailsPage.jsx";
+import HomePage from "./pages/HomePage";
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <h1>React WikiCountries</h1>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/:countryId" element={<CountryDetailsPage/>}/>
+      </Routes>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,46 @@
-function HomePage() {}
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Navbar from "../components/Navbar";
+
+function HomePage() {
+  const [countries, setCountries] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://ih-countries-api.herokuapp.com/countries").then(function (response){
+        setCountries(response.data);
+        console.log(response.data);
+      })
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      {!countries ? <p>loading countries</p>
+      :
+      <div
+        className="container"
+        style={{maxHeight: "90vh", overflow: "scroll"}}
+      >
+        {countries.map(function (country) {
+          return (
+            <div key={country.alpha3Code} className="list-group"> {/* alpha3Code because it represents the country with 3 capital letters instead of 2, like FRA, DEU, BRA, instead of FR, DE, BR */}
+              <Link
+                to={`/${country.alpha3Code}`}
+                className="list-group-item list-group-item-action"
+              >
+                <img
+                  src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} // Using the url as a base and then using the country.alpha2Code to change to each one of the countries represented and putting lower case because the alpha2Code requires to be lowercase
+                ></img>
+                {country.name.common} {/* Using name.common because on the api the common names are in that specific object */}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+      }
+    </>
+  );
+}
 
 export default HomePage;
