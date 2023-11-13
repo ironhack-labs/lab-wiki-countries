1200bd53ddacaecac72ba1db0a36f85a8cf00c04
Hugo Manachem <hugomanachem.pro@gmail.com>
2023-11-13 23:11:57 +0100

    Exercices done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:countryId" element={<CountryDetailsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,52 @@
-function CountryDetails() {}
+import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+function CountryDetails() {
+  const { countryId } = useParams();
+
+  const [countryDetails, setCountryDetails] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+      .then((response) => {
+        setCountryDetails(response.data);
+      })
+      .catch((error) => console.log("error: ", error));
+  });
+
+  return (
+    <>
+      <h1>Country Details</h1>
+      {countryDetails === null ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h1>{countryDetails.name.common}</h1>
+          <h2>Capital : {countryDetails.capital}</h2>
+          <h2>Area : {countryDetails.area} km²</h2>
+          {countryDetails.borders.length === 0 || countryDetails === null ? (
+            <p>No borders</p>
+          ) : (
+            <>
+              <h2>Borders :</h2>
+              {countryDetails.borders.map((border) => {
+                return (
+                  <li>
+                    <Link to={`/${border}`}>{border}</Link>
+                  </li>
+                );
+              })}
+            </>
+          )}
+
+          <Link to={`/`}>Back</Link>
+        </>
+      )}
+    </>
+  );
+}
 
 export default CountryDetails;
