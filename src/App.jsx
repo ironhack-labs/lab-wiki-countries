cb9f9141947c8a15221e70a5a768bcc94e5ec2f9
AnaRibeiroPT <anaribeiro924@gmail.com>
2023-11-13 22:02:05 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import CountryDetails from "./pages/CountryDetailsPage";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
 import "./App.css";
 
 function App() {
+  console.log("app")
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:countryId" element={<CountryDetails />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,7 @@
-function Navbar() {}
+function Navbar() {
+  return (
+    <nav className="navBar">WikiCountries</nav>
+  )
+}
 
 export default Navbar;
