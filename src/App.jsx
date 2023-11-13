d765148743b43ad83a6b59acff95cf23e23eed09
Fran Martinez <pmg.webdev@gmail.com>
2023-11-13 18:08:09 +0100

    it 2, creates routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import CountryDetails from "./pages/CountryDetailsPage"
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+      <Routes>
+        <Route path='/' element={<HomePage />} > </Route>
+        <Route path='/:countryId' element={<CountryDetails />} ></Route>
+      </Routes>
     </div>
   );
 }
