220c87204adc7e0b690496602194b5356f4a1dee
Fran Martinez <pmg.webdev@gmail.com>
2023-11-13 17:57:58 +0100

    it1, creates the components from example

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,56 @@
-function CountryDetails() {}
+import { Link } from "react-router-dom";
+
+function CountryDetails() {
+  return (
+    <>
+      <div>
+        <nav className="navbar navbar-dark bg-primary mb-3">
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              WikiCountries
+            </a>
+          </div>
+        </nav>
+
+        <div className="container">
+          <p style={{ fontSize: '24px', fontWeight: 'bold' }}>Country Details</p>
+
+          <h1>France</h1>
+
+          <table className="table">
+            <thead></thead>
+            <tbody>
+              <tr>
+                <td style={{ width: '30%' }}>Capital</td>
+                <td>Paris</td>
+              </tr>
+              <tr>
+                <td>Area</td>
+                <td>
+                  551695 km<sup>2</sup>
+                </td>
+              </tr>
+              <tr>
+                <td>Borders</td>
+                <td>
+                  <ul>
+                    <li><Link to="/AND">Andorra</Link></li>
+                    <li><Link to="/BEL">Belgium</Link></li>
+                    <li><Link to="/DEU">Germany</Link></li>
+                    <li><Link to="/ITA">Italy</Link></li>
+                    <li><Link to="/LUX">Luxembourg</Link></li>
+                    <li><Link to="/MCO">Monaco</Link></li>
+                    <li><Link to="/ESP">Spain</Link></li>
+                    <li><Link to="/CHE">Switzerland</Link></li>
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,42 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <>
+      <h1>WikiCountries: Your Guide to the World</h1>
+      <div>
+        <nav className="navbar navbar-dark bg-primary mb-3">
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              WikiCountries
+            </a>
+          </div>
+        </nav>
+
+        <div className="container" style={{ maxHeight: '90vh', overflow: 'scroll' }}>
+          <h1 style={{ fontSize: '24px' }}>WikiCountries: Your Guide to the World</h1>
+
+          <div className="list-group">
+            <Link to="/ABW" className="list-group-item list-group-item-action">🇦🇼 Aruba</Link>
+            <Link to="/AFG" className="list-group-item list-group-item-action">🇦🇫 Afghanistan</Link>
+            <Link to="/AGO" className="list-group-item list-group-item-action">🇦🇴 Angola</Link>
+            <Link to="/AIA" className="list-group-item list-group-item-action">🇦🇮 Anguilla</Link>
+            <Link to="/ALA" className="list-group-item list-group-item-action">🇦🇽 Åland Islands</Link>
+            <Link to="/ALB" className="list-group-item list-group-item-action">🇦🇱 Albania</Link>
+            <Link to="/AND" className="list-group-item list-group-item-action">🇦🇩 Andorra</Link>
+            <Link to="/ARE" className="list-group-item list-group-item-action">🇦🇪 United Arab Emirates</Link>
+            <Link to="/ARG" className="list-group-item list-group-item-action">🇦🇷 Argentina</Link>
+            <Link to="/ARM" className="list-group-item list-group-item-action">🇦🇲 Armenia</Link>
+            <Link to="/ASM" className="list-group-item list-group-item-action">🇦🇸 American Samoa</Link>
+            <Link to="/ATA" className="list-group-item list-group-item-action">🇦🇶 Antarctica</Link>
+            <Link to="/FLK" className="list-group-item list-group-item-action">🇫🇰 Falkland Islands</Link>
+            <Link to="/FRA" className="list-group-item list-group-item-action active">🇫🇷 France</Link>
+            <Link to="/ZWE" className="list-group-item list-group-item-action">🇿🇼 Zimbabwe</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default HomePage;
