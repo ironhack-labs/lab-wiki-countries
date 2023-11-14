cd5cd0c62ace85caeb421e496a842cb03054ad3f
Zohra <zohra.boukhatem1@gmail.com>
2023-11-14 01:45:46 +0100

    finished countryList (it 2.3)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import countriesJson from "./countries.json"
+import CountriesList from './components/CountriesList';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+
+  const [countries, setCountries] = useState([])
+  useEffect(() => {
+   setCountries(countriesJson)
+  }, []);
+  
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
+      
+    </>
   );
 }
 
---
+++ src/components/CountryDetails.jsx
@@ -0,0 +1,8 @@
+function CountryDetails(props) {
+  return (
+    <>
+      <h1>Details of country</h1>
+    </>
+  );
+}
+export default CountryDetails;
