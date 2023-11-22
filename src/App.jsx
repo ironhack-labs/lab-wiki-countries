56a8ac0e6955714c768bdda7a34b4769e1b13ba1
Carlos Rodriguez <carlos97sdg@gmail.com>
2023-11-22 21:05:28 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import CountryDetails from "./pages/CountryDetailsPage";
+import HomePage from "./pages/HomePage";
 import "./App.css";
+import Navbar from "./components/Navbar";
+
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+
+      <Navbar />
+
       <h1>LAB | React WikiCountries</h1>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:countryId" element={<CountryDetails />} />
+
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,17 @@
-function Navbar() {}
+import { NavLink } from "react-router-dom";
+
+NavLink
+
+function Navbar() {
+
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+                <NavLink to={"/"} style={{ textDecoration: "none", color: "black" }}> WikiCountries</NavLink>
+            </div>
+        </nav>
+
+    )
+}
 
 export default Navbar;
