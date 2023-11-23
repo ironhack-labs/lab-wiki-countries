823f12b7762bf214f4e6202210a92421b4a428ee
Ricardo Centeno <richu1094@gmail.com>
2023-11-23 09:24:29 +0100

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,10 @@
-function Navbar() {}
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-primary mb-3">
+            <div className="container">
+                <a className="navbar-brand" href="/">WikiCountries</a>
+            </div>
+        </nav>)
+}
 
 export default Navbar;
--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,57 @@
-function CountryDetails() {}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const CountryDetails = () => {
+
+    const { countryId } = useParams()
+    const [countryDetails, setCountryDetails] = useState({})
+
+    useEffect(() => {
+        axios.get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+            .then(response => setCountryDetails(response.data))
+            .catch(err => console.log(err))
+    }, [countryId])
+
+    return (
+        countryDetails.name
+            ?
+            <>
+                <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+
+                <h1>{countryDetails.name.common}</h1>
+
+                <table className="table">
+                    <thead></thead>
+                    <tbody>
+                        <tr>
+                            <td style={{ width: "30 %" }}>Capital</td>
+                            <td>{countryDetails.capital}</td>
+                        </tr>
+                        <tr>
+                            <td>Area</td>
+                            <td>
+                                {countryDetails.area} km
+                                <sup>2</sup>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Borders</td>
+                            <td>
+                                <ul>
+                                    {countryDetails.borders.map(eachBorder => {
+                                        return (<li><Link to={`/${eachBorder}`}>{eachBorder}</Link></li>)
+                                    })}
+                                </ul>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </>
+            :
+            <h1>CARGANDO....</h1>
+    )
+}
 
 export default CountryDetails;
