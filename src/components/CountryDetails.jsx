17c2e681bf9675bf95aea4f3c487fd64e086e037
zsaritek <zsaritek@gmail.com>
2023-12-19 15:05:29 +0100

    Message

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 
-const CountryDetails = (props) => {
-    const { countries } = props;
+const CountryDetails = () => {
+    // const { countries } = props;
     const { alpha3Code } = useParams();
     const [countryDetails, setCountryDetails] = useState(null);
 
@@ -26,21 +26,22 @@ const CountryDetails = (props) => {
     }
 
     //const country = countries.find((country) => country.alpha3Code === alpha3Code);
+    const { name, capital, area, borders } = countryDetails;
 
     return (
         <div className="col-7">
-            <h1>{countryDetails.name.common}</h1>
+            <h1>{name.common}</h1>
             <table className='table'>
                 <thead></thead>
                 <tbody>
                     <tr>
                         <td style={{ width: '30%' }}>Capital</td>
-                        <td>{countryDetails.capital[0]}</td>
+                        <td>{capital[0]}</td>
                     </tr>
                     <tr>
                         <td>Area</td>
                         <td>
-                            {countryDetails.area} km
+                            {area} km
                             <sup>2</sup>
                         </td>
                     </tr>
@@ -48,7 +49,7 @@ const CountryDetails = (props) => {
                         <td>Borders</td>
                         <td>
                             <ul>
-                                {
+                                {/* {
                                     countryDetails.borders.map((border) => {
                                         let borderCountry = countries.find((country) => country.alpha3Code === border);
 
@@ -56,7 +57,12 @@ const CountryDetails = (props) => {
                                             <li><Link to={"/" + border} > {borderCountry.name.common} </Link></li>
                                         );
                                     })
-                                }
+                                } */}
+                                {borders.map((border) => (
+                                    <li key={border}>
+                                        <Link to={`/${border}`}>{border}</Link>
+                                    </li>
+                                ))}
                             </ul>
                         </td>
                     </tr>
