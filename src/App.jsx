2752c1d9460da78ffcc694264e4b1bdd419352a3
Irene <amoreperdesi@gmail.com>
2024-02-02 21:07:25 +0100

    Lab done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Homepage from "./pages/HomePage";
+import CountryDetails from "./pages/CountryDetailsPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Homepage></Homepage>}></Route>
+        <Route path='/:countryId' element={<CountryDetails></CountryDetails>} />
       </Routes>
 
    </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
 import "./HomePage.css";
 import { Link } from "react-router-dom";
 
@@ -14,19 +13,30 @@ function HomePage() {
   }, []);
 
   return (
+   
     <div id="HomePage">
       <h1>WikiCountries: Your Guide to the World</h1>
       <ul>
         {countries.map((country) => {
+            console.log(`${country.name.common} flag`);
+
+            let flagCode = country.alpha2Code.toLowerCase();
+            {/* I stored it in a variable to get it easily below  */}
           return (
             <li className="country-item" key={country._id}>
-              <Link to={country._id}>{country.name.common}</Link>
+              <img src={`https://flagpedia.net/data/flags/icon/72x54/${flagCode}.png`} alt={`${country.name.common} + flag`}></img>
+              <Link to={country.alpha3Code}>
+              {/*I use alpha3Code to get the name of the Country after the slash"/" in the domain */}
+              {country.name.common}
+              </Link>
             </li>
           );
         })}
       </ul>
     </div>
   );
+
+  
 }
 
 export default HomePage;
