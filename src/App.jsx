6df51a4d63894bd738030f4aab43f66531a15d05
Eduardo Botelho <eduardo11224b@gmail.com>
2024-02-02 17:38:34 +0000

    Homepage component and axios request to countries API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Homepage from './pages/Homepage'
+import Homepage from "./pages/HomePage";
 
 function App() {
   return (
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,31 @@
-function HomePage() {}
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import "./HomePage.css";
+
+function HomePage() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-countries-api.herokuapp.com/countries ")
+      .then((result) => setCountries(result.data));
+  }, []);
+
+  return (
+    <div id="HomePage">
+      <h1>WikiCountries: Your Guide to the World</h1>
+      <ul>
+        {countries.map((country) => {
+          return (
+            <li className="country-item" key={country._id}>
+              <span>{country.name.common}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
 
 export default HomePage;
