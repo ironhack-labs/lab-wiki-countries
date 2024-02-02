1374ad96bf387ee3ec1022ec08adfff83cbb7690
Irene <amoreperdesi@gmail.com>
2024-02-02 15:31:11 +0100

    started lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+      <Routes>
+        <Route/>
+      </Routes>
     </div>
   );
 }
