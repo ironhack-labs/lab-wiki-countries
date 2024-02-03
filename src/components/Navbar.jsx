a4ab78ae40d293fb1a2c9ef1b5592df8f6a05d5e
Irene <amoreperdesi@gmail.com>
2024-02-03 16:43:53 +0100

    Lab done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-    return <nav className="Navbar">WikiCountries</nav>;
+    return <nav className="Navbar"><Link className="Link" to={"/"}>WikiCountries</Link></nav>;
   }
   
   export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,30 +13,33 @@ function HomePage() {
   }, []);
 
   return (
-   
     <div id="HomePage">
       <h1>WikiCountries: Your Guide to the World</h1>
       <ul>
+      
         {countries.map((country) => {
-            console.log(`${country.name.common} flag`);
+          {/* console.log(`${country.name.common} flag`); */}
 
-            let flagCode = country.alpha2Code.toLowerCase();
-            {/* I stored it in a variable to get it easily below  */}
+          let flagCode = country.alpha2Code.toLowerCase();
+          {
+            /* I stored it in a variable to get it easily below  */
+          }
           return (
-            <li className="country-item" key={country._id}>
-              <img src={`https://flagpedia.net/data/flags/icon/72x54/${flagCode}.png`} alt={`${country.name.common} + flag`}></img>
-              <Link to={country.alpha3Code}>
-              {/*I use alpha3Code to get the name of the Country after the slash"/" in the domain */}
-              {country.name.common}
-              </Link>
-            </li>
+            <Link className="country-name" key={country._id} to={"/" + country.alpha3Code}>
+              <li className="country-item" >
+                <img
+                  src={`https://flagpedia.net/data/flags/icon/72x54/${flagCode}.png`}
+                  alt={`${country.name.common} + flag`}
+                ></img>
+                {/*I use alpha3Code to get the name of the Country after the slash"/" in the domain */}
+                {country.name.common}
+              </li>
+            </Link>
           );
         })}
       </ul>
     </div>
   );
-
-  
 }
 
 export default HomePage;
