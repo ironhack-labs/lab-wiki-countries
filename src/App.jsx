563fe4bb200c159fa1a28c4df7fb3798cac165cc
Christian Doeller <mail@christiandoeller.de>
2024-02-03 11:26:38 +0100

    lab finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/:alpha3" element={<CountryDetailsPage />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,40 @@
-function HomePage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  const [countriesData, setCountriesData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-countries-api.herokuapp.com/countries")
+      .then((result) => {
+        console.log(result.data);
+        setCountriesData(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>WikiCountries</h1>
+      {countriesData.map((country) => {
+        return (
+          <div key={country._id}>
+            <Link to={`/${country.alpha3Code}`}>
+              <img
+                src={` https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                alt=""
+              />
+              <p>{country.name.common}</p>
+            </Link>
+          </div>
+        );
+      })}
+    </>
+  );
+}
 
 export default HomePage;
