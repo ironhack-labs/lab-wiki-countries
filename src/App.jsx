29ad72ce77c2a27fb1ae4a4993291be696378331
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-02-05 15:27:17 +0100

    iteration 1 almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Navbar from "./components/Navbar";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from "./pages/HomePage";
+import CountryDetails from "./pages/CountryDetailsPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <h1 className="mt-5">LAB | React WikiCountries</h1>
+      <HomePage />
+      <Navbar />
+      <CountryDetails />
     </div>
   );
 }
