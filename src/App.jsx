ae44c61d49b22ee1e704e551c6614c7c46e380cd
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-02-06 11:28:56 +0100

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>LAB | React WikiCountries</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:countryId" element={<CountryDetailsPage />} />
+
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,13 @@
-function Navbar() {}
+function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-primary mb-3">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          WikiCountries
+        </a>
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
