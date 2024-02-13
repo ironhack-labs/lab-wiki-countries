184b4cc9deb3c24200da0561334c588cacfede3a
Teresa <tereac1102@gmail.com>
2024-02-13 17:06:08 +0100

    work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+import HomePage from "./pages/HomePage";
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:countryId" element={<CountryDetailsPage />} />
+      </Routes>
+
     </div>
   );
 }
--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,58 @@
-function CountryDetails() {}
+import './CountryDetailsPage.css'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+
+function CountryDetails() {
+
+    const { id } = useParams()
+
+    const [countries, setCountries] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        loadCountries()
+    }, [id])
+
+    const loadCountries = () => {
+
+        axios
+            .get(`https://ih-countries-api.herokuapp.com/countries/${id}`)
+            .then(response => {
+                setCountries(response.data)
+                setIsLoading(false)
+            })
+            .catch(err => console.log(err))
+    }
+    return (
+
+        <div className="CountryDetails">
+
+            <h2>Country Details</h2>
+
+
+
+            {
+                isLoading
+                    ?
+                    <h2>Loading...</h2>
+                    :
+                    countries.map(cont => {
+
+                        return (
+
+                            <article className="CountryDetails" key={cont._id}>
+                                <Link to={`/${cont._id}`}><p>{cont.name.common} {cont.capital}</p></Link>
+
+                            </article>
+
+                        )
+                    })
+            }
+
+        </div>
+    )
+}
 
 export default CountryDetails;
