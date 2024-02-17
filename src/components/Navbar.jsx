9e385e5972d29f590f71dae47d55071c46cee402
Ensha C Latheef <enshalatheef@gmail.com>
2024-02-17 10:55:21 +0100

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,12 @@
-function Navbar() {}
+import { Link } from "react-router-dom"; 
+function Navbar() {
+    return(
+        <nav className="navbar navbar-dark bg-primary">
+        <Link to="/">
+          <h1 className="nav-wiki text-white">WikiCountries</h1>
+        </Link>
+      </nav>
+    )
+}
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,42 @@
-function HomePage() {}
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function HomePage() {
+    const [fetching, setFetching] = useState(true);
+    const [countries, setCountries] = useState([]);
+    
+    useEffect(() => {
+        
+        axios.get("https://ih-countries-api.herokuapp.com/countries")
+        .then((response) => {
+            console.log(response)
+            setCountries(response.data);
+            setFetching(false);
+        })
+        .catch(console.error);
+      }, []);
+    
+    if (fetching) {
+        return <p>Loading...</p>
+    }
+    
+    return (
+        <div className="container">
+            <h1>WikiCountries: Your Guide to the World</h1>
+            <div className="list-group">
+                {countries.map((c) => {
+                    return (    
+                    <Link key={c._id} className="list-group-item list-group-item-action" to={`/${c.alpha3Code}`}>
+                        <img src={`https://flagpedia.net/data/flags/icon/72x54/${c.alpha2Code.toLowerCase()}.png`} width={72} height={54} />
+                        <h3>{c.name.common}</h3>
+                        
+                    </Link>
+
+                    )
+                })}
+            </div>
+          </div>
+    )
+}
 
 export default HomePage;
