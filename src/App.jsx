cea03e0a51b155bb57ef7537d8baf3f1970ae1ae
Nihat Algan <burakalgan@gmail.com>
2024-02-17 14:09:12 +0100

    done iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+import CountryDetails from "./pages/CountryDetailsPage";
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:countryId" element={<CountryDetailsPage />} />
+      </Routes>
     </div>
   );
 }
