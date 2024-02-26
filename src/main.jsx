4f72072146f4af2fa005a52ba982b44a304fc023
Vitor193 <vitormiguel135@gmail.com>
2024-02-26 14:54:44 +0000

    done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
