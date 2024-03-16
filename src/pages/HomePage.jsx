9f7e14eecfaab50e40a78f4ecfad8d8b257f7873
Arnau Ramos <arnauramos21@gmail.com>
2024-03-16 11:25:32 +0100

    Done iteration 3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,33 @@
-function HomePage() {
-    return <h2>WikiCountries: Your Guide to the World</h2>
+import axios from "axios";
+import { useState, useEffect } from "react";
 
+const countriesAPI = "https://ih-countries-api.herokuapp.com/countries";
+
+function HomePage() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get(countriesAPI).then((response) => {
+      const countries = response.data;
+      setCountries(countries);
+      console.log(countries);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>WikiCountries: Your Guide to the World</h2>
+      <ul>
+        {countries.map((country) => {
+          return (
+            <li className="countries-list" key={country._id}>
+              {country.name.common}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default HomePage;
