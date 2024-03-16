b10b9504209e540599e52319d956e476ad120e99
guzegui <guzmanegui@gmail.com>
2024-03-16 10:53:21 +0100

    Done Iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CountryDetails from "./pages/CountryDetailsPage";
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+const apiURL = `https://ih-countries-api.herokuapp.com/countries`;
+
+
 
 function App() {
+
+const [countries, setCountries] = useState(apiURL)
+
+  useEffect(() => {
+    axios.get(apiURL).then((response) => {
+      const countries = response.data;
+      setCountries(countries);
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/country/:countryId" element={<CountryDetails />} />
+      </Routes>
     </div>
   );
 }
