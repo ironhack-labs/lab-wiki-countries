4e956323ef64d10d8a5082d71454619cff786db2
guzegui <guzmanegui@gmail.com>
2024-03-16 11:33:22 +0100

    Done Iteration 4

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,22 @@
-function HomePage({countries}) {
-    return(
-        <div>
-        <h1>WikiCountries: Your Guide to the World</h1>
-        {countries.map((country) =>
-            <p key={country._id}>{country.name.common}</p>
-            )}
-        </div>
-    )
+import { Link } from "react-router-dom";
+
+function HomePage({ countries }) {
+  return (
+    <div>
+      <h1>WikiCountries: Your Guide to the World</h1>
+      {countries.map((country) => (
+        <Link key={country._id} to={`/country/${country.alpha3Code}`}>
+          <h3>{country.name.common}</h3>
+          <img
+            key={country._id}
+            className="country-img"
+            src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+            alt={country.name.common}
+          />
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default HomePage;
