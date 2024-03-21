124950f3b8ab0798905722663d124b5c554c427a
Nawelchafi <chafinawel@gmail.com>
2024-03-21 02:46:56 +0100

    done lab

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -15,3 +15,38 @@ function CountryDetails() {
             .catch(err => console.log(err));
     }, [countryId]);
 
+    const flagUrl = country ? `https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code?.toLowerCase()}.png` : null;
+
+    const countryBorders = country.borders ? country.borders.map((border, index) => (
+        <p key={index}>{border}</p>
+    )) : null;
+    return (
+        <div>
+            <h1>Country Details</h1>
+            {country === "loading" ? (
+                <p>Loading...</p>
+            ) : (
+                <div>
+                    <img src={flagUrl} alt={country.name?.official} />
+                    <h1>{country.name?.official}</h1>
+
+                    <div className="capital">
+                        <p>Capital</p>
+                        <p>{country.capital}</p>
+                    </div>
+                    <div className="area">
+                        <p>Area</p>
+                        <p>{country.area}km</p>
+                    </div>
+                    <div className="borders">
+                        <p>Borders</p>
+                        
+                        <div className="countryBorders">{countryBorders}</div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default CountryDetails;
