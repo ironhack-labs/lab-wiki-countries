6bff3e7aa662c503267c1f102e5e517165d80dc2
Sowjanya Kambhampati <sowjanyaakella87@gmail.com>
2024-03-21 15:41:53 +0100

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+    <Navbar/>
+      {/* <h1>LAB | React WikiCountries</h1> */}
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/:countryId" element={<CountryDetailsPage />} />
+      </Routes>
     </div>
   );
 }
